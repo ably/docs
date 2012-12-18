481524380c4e93a3a424c3cb22fc00f29b7c5e14
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-12-18 14:15:03 +0100

    Support for link helpers

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,5 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+include Nanoc::Helpers::LinkTo
+include LinkHelper
---
+++ lib/helpers/link_helper.rb
@@ -0,0 +1,6 @@
+module LinkHelper
+  def link(text, target, attributes = {})
+    target = "/#{target}" unless target.match(/^(\w+:\/\/|\/)/)
+    link_to text, target, attributes
+  end
+end
