657bbd108eee2a30e83e2b28c9a3e5bf5074b545
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-12-24 22:21:39 +0000

    Language selection for content within body

--- content/assets/javascripts/lang-manager.js
+++ content/assets/javascripts/lang-manager.js
@@ -1,42 +1,46 @@
 $(function() {
-  $('pre:has(code)')
-    .each(function() {
-      var $first = $(this),
-          $siblings = $first.nextUntil(':not(pre)');
-      $first.addClass('prettyprint');
+  $('pre:has(code),p[lang],span[lang]').each(function() {
+    var $first = $(this),
+        tag = this.nodeName.toLowerCase(),
+        $siblings = $first.nextUntil(':not(' + tag + ')');
 
-      if (!$first.hasClass('with-lang-nav')) {
-        if ($siblings.length) {
-          var langs = [$first.attr('lang')],
-              langSelector = $('<ul class="lang-selector"></ul>');
+    if (tag == 'pre') $first.addClass('prettyprint').addClass('linenums');
 
-          // we will be building a nav so change the class so it's aware and this code block is selected
-          $first.addClass('with-lang-nav').addClass('selected');
+    if (!$first.hasClass('with-lang-nav')) {
+      if ($siblings.length) {
+        var langs = [$first.attr('lang')],
+            langSelector = $('<ul class="lang-selector"></ul>');
 
-          // find all siblings and add to the language nav, and set up with correct class
-          $siblings.each(function() { langs.push($(this).attr('lang')); }).addClass('with-lang-nav');
-          $.each(langs, function(i, n) {
-            langSelector.append('<li>' + n + '</li>');
-          });
-          langSelector.find('li:first').addClass('selected');
+        if (tag != 'pre') langSelector.addClass('inline');
 
-          // insert nav before this first code block
-          $first.before(langSelector);
+        // we will be building a nav so change the class so it's aware and this code block is selected
+        $first.addClass('with-lang-nav').addClass('selected');
 
-          langSelector.on('click', 'li', function() {
-            var selectedLang = $(this).text();
-            langSelector.find('li').removeClass('selected');
-            $(this).addClass('selected');
-            $siblings.removeClass('selected');
-            if ($first.attr('lang') == selectedLang) {
-              $first.addClass('selected');
-            } else {
-              $first.removeClass('selected');
-              $siblings.each(function() { if ($(this).attr('lang') == selectedLang) $(this).addClass('selected'); });
-            }
-          });
-        }
+        // find all siblings and add to the language nav, and set up with correct class
+        $siblings.each(function() { langs.push($(this).attr('lang')); }).addClass('with-lang-nav');
+        $.each(langs, function(i, n) {
+          langSelector.append('<li>' + n + '</li>');
+        });
+        langSelector.find('li:first').addClass('selected');
+
+        // insert nav before this first code block
+        $first.before(langSelector);
+
+        langSelector.on('click', 'li', function() {
+          var selectedLang = $(this).text();
+          langSelector.find('li').removeClass('selected');
+          $(this).addClass('selected');
+          $siblings.removeClass('selected');
+          if ($first.attr('lang') == selectedLang) {
+            $first.addClass('selected');
+          } else {
+            $first.removeClass('selected');
+            $siblings.each(function() { if ($(this).attr('lang') == selectedLang) $(this).addClass('selected'); });
+          }
+        });
       }
-    });
+    }
+  });
+
   prettyPrint();
 });
