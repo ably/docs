5f494fe5bb5bf220e4b78724f91bdd9b62e09875
Matthew O'Riordan <matthew.oriordan@gmail.com>
2012-12-27 17:22:00 +0000

    REST API documentation + navigation helpers

--- lib/helpers/nav_helper.rb
+++ lib/helpers/nav_helper.rb
@@ -6,6 +6,34 @@ module NavHelper
     end
     results.join("\n")
   end
+
+  def toc_items(context)
+    html = []
+    if context.kind_of?(String)
+      link_title = context
+      link = if context.match(/(.+)#(.+)/)
+        link_title, link_id = context.match(/(.+)#(.+)/)[1..2]
+        link_id
+      else
+        link_title.downcase
+      end
+      html << "<li><a href='##{link}'>#{link_title}</a></li>"
+    elsif context.kind_of?(Array)
+      html << "<ul>"
+      context.each do |item|
+        html << toc_items(item)
+      end
+      html << "</ul>"
+    elsif context.kind_of?(Hash)
+      context.each do |key, val|
+        html << "<li>"
+        html << key
+        html << toc_items(val)
+        html << "</li>"
+      end
+    end
+    html.join("\n")
+  end
 end
 
 include NavHelper
