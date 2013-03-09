91746f3d95087c52f462b8448067f48d68c4869d
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-03-09 18:36:51 +0000

    Centralise documentation languages so that updates only occur in one place across this application as well as the main website which will require documentation_languages.rb

---
+++ lib/sass_functions.rb
@@ -0,0 +1,15 @@
+require 'sass'
+require File.expand_path('../documentation_languages', __FILE__)
+
+module Sass::Script::Functions
+  def language_name(id)
+    assert_type id, :String
+    Sass::Script::String.new(Ably::DOCUMENTATION_LANGUAGES[id.to_s])
+  end
+  declare :language_name, :args => [:string]
+
+  def language_ids
+    keys = Ably::DOCUMENTATION_LANGUAGES.keys.map { |d| Sass::Script::String.new(d) }
+    Sass::Script::List.new(keys, :comma)
+  end
+end
