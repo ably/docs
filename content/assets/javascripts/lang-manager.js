22aea2b8e6119672c258060c2d50d55da5bf13a2
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-04-11 22:59:57 +0100

    Support standalone language blocks, and language specific definition lists

--- content/assets/javascripts/lang-manager.js
+++ content/assets/javascripts/lang-manager.js
@@ -7,7 +7,7 @@ $(function() {
   var friendlyLanguageNames = {
     <%= Ably::DOCUMENTATION_LANGUAGES.reject { |lang_d, lang| lang[:ignore_from_language_selector] }.map { |lang_id, lang| "\"#{lang_id}\": \"#{lang[:name]}\"" }.join(',') %>
   };
-  var langaugeExtensions = {
+  var languageExtensions = {
     <%= Ably::DOCUMENTATION_LANGUAGES.map { |lang_id, lang| "\"#{lang_id}\": \"#{lang[:extension]}\"" }.join(',') %>
   };
 
@@ -17,54 +17,64 @@ $(function() {
 
   // select language tab event callback for individual language element
   function selectLang() {
-    var $this = $(this),
-        selectedLang = $this.attr('lang'),
+    var $this = $(this);
+    // when tabs should not be shown and no language block should be shown, $this is set to ul
+    if ($this[0].nodeName.toLowerCase() == 'ul') {
+      var $first = $(this).next(),
+          $children = $first.nextUntil(':not(' + $first[0].nodeName.toLowerCase() + '),:not([lang])').addBack();
+      $(this).find('> li').removeClass('selected');
+      $children.removeClass('selected');
+    } else {
+      var selectedLang = $this.attr('lang'),
         langSelector = $this.parent('ul'),
         $first = langSelector.next(),
         tag = $first[0].nodeName.toLowerCase(),
-        $siblings = $first.nextUntil(':not(' + tag + '),:not([lang])');
+        $languageElements = $first.nextUntil(':not(' + tag + (tag === 'dt' ? ',dd' : '') + '),:not([lang])').addBack();
 
-    langSelector.find('li').removeClass('selected');
-    $(this).addClass('selected');
-    $siblings.removeClass('selected');
-    if ($first.attr('lang') == selectedLang) {
-      $first.addClass('selected');
-    } else {
-      $first.removeClass('selected');
-      $siblings.each(function() { if ($(this).attr('lang') == selectedLang) $(this).addClass('selected'); });
+
+      langSelector.find('li').removeClass('selected');
+      $(this).addClass('selected'); // select the navigation tab
+      $languageElements.removeClass('selected');
+      $languageElements.each(function() { if ($(this).attr('lang') == selectedLang) $(this).addClass('selected'); });
     }
   }
 
+  // transform all definitions with lang into valid language tags as Textile does not allow language to be assigned to dt or dd
+  $('dl dt > div[lang]').each(function() {
+    var lang = $(this).attr('lang');
+    $(this).parent('dt').attr('lang', lang);
+    $(this).parent('dt').next('dd').attr('lang', lang);
+    $(this).removeAttr('lang');
+  });
+
   // find all elements that have a language specified and make them language selectable
-  $('pre:has(code),p[lang],span[lang],div[lang]').each(function() {
+  $('pre[lang]:has(code),p[lang],span[lang],div[lang],h2[lang],h3[lang],h4[lang],dt[lang],dd[lang]').each(function() {
     var $first = $(this),
         tag = this.nodeName.toLowerCase(),
-        $siblings = $first.nextUntil(':not(' + tag + '),:not([lang])'),
-        dlParent = $first.parents('dl').length;
+        $siblings = $first.nextUntil(':not(' + tag + (tag === 'dt' ? ',dd' : '') + '),:not([lang])'),
+        dlParent = $first.parents('dl').length,
+        hasLanguageNav = (tag === 'pre'),
+        languageClass = hasLanguageNav ? 'with-lang-nav' : 'lang-resource';
 
     // convert all pre formatted text except those within a definition list without a language to pretty code blocks
     // pre tags within dls without language are used for alternate paths
-    if ( (tag === 'pre') && (!dlParent || (dlParent && $first.attr('lang'))) ) {
-      if ($first.attr('lang')) $first.addClass('lang-' + langaugeExtensions[$first.attr('lang')]);
+    if ( hasLanguageNav && (!dlParent || (dlParent && $first.attr('lang'))) ) {
+      if ($first.attr('lang')) $first.addClass('lang-' + languageExtensions[$first.attr('lang')]);
       $first.addClass('prettyprint').addClass('linenums');
     }
 
-    // if element has with-lang-nav class, then this element has already been converted by this script when operating on one of it's siblings
-    if (!$first.hasClass('with-lang-nav')) {
-      if ($siblings.length) {
+    // if element has class indicating it's a langauge resource, then this element has already been converted by this script when operating on one of it's siblings
+    if (!$first.hasClass(languageClass)) {
+      if ($siblings.length || !hasLanguageNav) {
         var langs = [$first.attr('lang')],
             langSelector = $('<ul class="lang-selector"></ul>'),
             uniqueLangs = [];
 
-        // pre blocks have navigation above as tabs
-        // inline language selector is shown for all p/span/div tags instead, but has different more subtle styling, apply
-        if (tag !== 'pre') langSelector.addClass('inline');
-
         // we will be building a nav so change the class so it's aware and this code block is selected
-        $first.addClass('with-lang-nav').addClass('selected');
+        $first.addClass(languageClass).addClass('selected');
 
         // find all siblings and add to the language nav, and set up with correct class
-        $siblings.each(function() { langs.push($(this).attr('lang').toLowerCase()); }).addClass('with-lang-nav');
+        $siblings.each(function() { langs.push($(this).attr('lang').toLowerCase()); }).addClass(languageClass);
         $.each(langs, function(i, el) { if($.inArray(el, uniqueLangs) === -1) uniqueLangs.push(el); });
         $.each(friendlyLanguageNames, function(key, val) {
           if (uniqueLangs.indexOf(key) !== -1) langSelector.append('<li lang="' + key + '">' + val + '</li>');
@@ -74,6 +84,7 @@ $(function() {
 
         // insert nav before this first code block
         $first.before(langSelector);
+        if (!hasLanguageNav) langSelector.hide();
 
         langSelector.on('click', 'li[lang]', selectLang);
       }
@@ -110,6 +121,7 @@ $(function() {
 
     $('ul.lang-selector').each(function() {
       var langSelector = $(this),
+          inline = langSelector.next().hasClass('lang-resource'),
           languageTab = langSelector.find('li:not(.warning):langequals("' + lang + '")'),
           allOtherLanguagesTab = langSelector.find('li:not(.warning):langequals(default)'); // special tab that allows content to be dispayed as default
       if (languageTab.length) {
@@ -119,20 +131,19 @@ $(function() {
         selectLang.apply(allOtherLanguagesTab);
         langSelector.hide();
       } else {
-        langSelector.show();
-        langSelector.find('li.warning').remove();
-        langSelector.append('<li class="warning">No ' + friendlyLang + ' example exists</li>');
+        if (!inline) {
+          langSelector.show();
+          langSelector.find('li.warning').remove();
+          langSelector.append('<li class="warning">No ' + friendlyLang + ' example exists</li>');
+        } else {
+          langSelector.hide();
+          selectLang.call(langSelector);
+        }
       }
 
       /* if language selector for this content area is not visible, and the element is a span, lets no longer show it as a block */
       var contentAreaTag = langSelector.next()[0].tagName.toLowerCase(),
           selectedSibling = langSelector.nextUntil(':not(' + contentAreaTag + '),:not([lang])').filter('.selected');
-
-      if (!langSelector.is(':visible') && contentAreaTag == 'span') {
-        selectedSibling.addClass('show-as-span');
-      } else {
-        selectedSibling.removeClass('show-as-span');
-      }
     });
   }
 
@@ -145,9 +156,9 @@ $(function() {
 
   prettyPrint();
 
-  // clean up list definition headers which should not have trailing :
+  // clean up list definition headers which should not have trailing colon (:) which cannot be easily fixed with CSS alone
   $('dl dt > strong').parent().addClass('header');
 
   // simple roll over hint to explain why inline language copy has a dotted line around it
-  $('span.with-lang-nav').attr('title', 'This inline text is highlighted to indicate that it will change based on the language selected');
+  $('.lang-resource').attr('title', 'This inline text is highlighted to indicate that it will change based on the language selected');
 });
