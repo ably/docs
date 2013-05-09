42cb9a0ac3a75d11eaccf8f11e0cf1327ee08e6c
Matthew O'Riordan <matthew.oriordan@gmail.com>
2013-05-09 17:26:44 +0100

    Jump to documentation and jump to menu in doc help

--- lib/documentation_languages.rb
+++ lib/documentation_languages.rb
@@ -13,6 +13,7 @@ module Ably
     'phonegap' => { name: 'PhoneGap', extension: 'js' },
     'html' => { name: 'HTML', extension: 'html', ignore_from_language_selector: true },
     'json' => { name: 'JSON', extension: 'json' },
-    'sh' => { name: 'Shell script', extension: 'sh', ignore_from_language_selector: true }
+    'sh' => { name: 'Shell script', extension: 'sh', ignore_from_language_selector: true },
+    'yaml' => { name: 'YAML', extension: 'yaml', ignore_from_language_selector: true }
   } unless defined?(::Ably::DOCUMENTATION_LANGUAGES)
 end
