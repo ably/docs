d5902ef2165784e737a3fead9a6470d4f0ba8b67
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-02-04 13:06:58 +0100

    Support external links using class=“external”

--- lib/ably_post_textile_filter.rb
+++ lib/ably_post_textile_filter.rb
@@ -2,7 +2,8 @@ class AblyPostTextileFilter
   class << self
     def run(content, path)
       content = convert_blang_blocks_to_html(content)
-      add_github_line_breaks(content)
+      content = add_github_line_breaks(content)
+      convert_external_links_to_blank_target(content)
     end
 
     # Line breaks when using Github notation insert <br /> tags, replace them with actual HTML <br />
@@ -19,6 +20,10 @@ class AblyPostTextileFilter
       content.gsub(blang_regex, %{\n\n<div lang="\\1"> <!-- start \\1 language block -->\n\\2\n</div> <!-- /end \\1 language block -->\n\n})
     end
 
+    def convert_external_links_to_blank_target(content)
+      content.gsub(link_external_regex, %{\\1target="_blank"\\2})
+    end
+
     private
     def blang_regex
       tag_regex = %r{(?:<[\w /]+>)*\s*}
@@ -30,6 +35,12 @@ class AblyPostTextileFilter
         #{tag_regex}
         {{/LANG_BLOCK}}
         #{tag_regex}
+      }mx
+    end
+
+    def link_external_regex
+      %r{
+        (<a[^>]*)class="external"([^>]*>)
       }mx
     end
   end
