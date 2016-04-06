eef2b2179ab9039f3688f438c352bd13625734a3
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-06 21:00:21 +0200

    Use canonical URL to ensure traffic is directed to main docs
    Fixes https://github.com/ably/docs/issues/104

--- lib/helpers/link_helper.rb
+++ lib/helpers/link_helper.rb
@@ -1,7 +1,17 @@
 module LinkHelper
+  CANONICAL_ROOT = 'https://www.ably.io/'
+
   def link(text, target, attributes = {})
     target = "/#{target}" unless target.match(/^(\w+:\/\/|\/)/)
     link_to text, target, attributes
+  end
+
+  def canonical_link
+    if item.identifier == '/'
+      CANONICAL_ROOT
+    else
+      "#{CANONICAL_ROOT}documentation#{item.identifier.gsub("/root", "")}".gsub(%r{/$}, '')
+    end
   end
 end
 
