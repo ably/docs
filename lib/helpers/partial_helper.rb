635be8999aaa8b6aeced72d3ea08c6559e90ac25
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-04-06 20:49:34 +0200

    Add authCallback example + authCallback to AuthOptions
    Fixes https://github.com/ably/docs/issues/106

--- lib/helpers/partial_helper.rb
+++ lib/helpers/partial_helper.rb
@@ -4,13 +4,13 @@ module PartialHelper
     path = File.join(content_path, "#{path}#{File.extname(item.raw_filename)}")
     raise "Partial #{path} was not found" unless File.exists?(path)
 
-    File.read(path).split(/\n/).each_with_index.map do |line, index|
+    ERB.new(File.read(path).split(/\n/).each_with_index.map do |line, index|
       if indent == 0 || (index == 0 && skip_first_indent)
         line
       else
         "#{' ' * indent}#{line}"
       end
-    end.join("\n")
+    end.join("\n")).result
   end
 end
 
