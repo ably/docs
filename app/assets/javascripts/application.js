dddd7f86d98c9aedb5c1354f1462b0508fc67ddd
Matthew O'Riordan <matthew.oriordan@gmail.com>
2016-07-22 12:39:53 +0100

    tutorials: Added inline example + QR code for mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -222,3 +222,22 @@ $(function() {
     adjustAnchor();
   });
 });
+
+/*
+  Global helper methods for tutorials
+  These methods will be replaced with string values when published to Ably
+*/
+
+function getQueryParam(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getRandomChannelName() {
+  return window.randomChannelName;
+}
