f4b9ee5b1e9f5ffe146ef4bda350d57a789cdf5b
Simon Woolf <simon@ably.io>
2016-11-01 23:56:13 +0000

    Add support for passing variables to partials

--- lib/helpers/partial_helper.rb
+++ lib/helpers/partial_helper.rb
@@ -1,5 +1,5 @@
 module PartialHelper
-  def partial(path, indent: 0, skip_first_indent: true)
+  def partial(path, indent: 0, skip_first_indent: true, vars: Nanoc::Context.new({}))
     content_path = File.expand_path('../../../content', __FILE__)
     path = File.join(content_path, "#{path}#{File.extname(item.raw_filename)}")
     raise "Partial #{path} was not found" unless File.exists?(path)
@@ -10,7 +10,11 @@ module PartialHelper
       else
         "#{' ' * indent}#{line}"
       end
-    end.join("\n")).result
+    end.join("\n")).result(vars.get_binding)
+  end
+
+  def vars(hash)
+    {vars: Nanoc::Context.new(hash)}
   end
 end
 
