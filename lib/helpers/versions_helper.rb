686d24fd60637f982a1aaf049971d356275f3c0d
tomC <tc13270@my.bristol.ac.uk>
2018-12-11 15:32:25 +0000

    Created v1.0 versioning of existing files

--- lib/helpers/versions_helper.rb
+++ lib/helpers/versions_helper.rb
@@ -1,5 +1,5 @@
 module VersionsHelper
-  CURRENT_VERSION = '1.0' unless defined?(CURRENT_VERSION)
+  CURRENT_VERSION = '1.1' unless defined?(CURRENT_VERSION)
 
   VERSIONED_FOLDERS = %w(
     client-lib-development-guide
