ced4682dbfb4f94a2f990eb445d59fa77eaca367
Srushtika <n.srushtika@gmail.com>
2019-08-12 13:02:04 +0100

    Revert "api streamer docs: conflicts resolved"
    This reverts commit c8c1bb6e172705c89e5dbd2c044cf63b07edac74, reversing
    changes made to f03171b137a0ba1afccd8d8a14b69341897c281c.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -253,10 +253,8 @@ function getQueryParam(name, url) {
 
 function insertKeyUsageWarning(code) {
   let message = "API Keys should not be shared. The key here is intended solely for this example.";
-  let currentLangRegexp = /lang="(.*?)"/g;
-  let currentLang = currentLangRegexp.exec(code);
-  if (currentLang != null) currentLang = currentLang[1];
-
+  let currentLang = code.match(/(?<=(lang=")).*?(?=")/g);
+  if (currentLang != null) currentLang = currentLang[0];
   if(["php", "ruby", "python"].indexOf(currentLang) > -1) {
     message = `# ${message}`;
   } else {
