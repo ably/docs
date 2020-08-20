c19613ddf3bc8ca3c141df4c030978cf262170d3
Kenneth Kalmer <kenneth.kalmer@ably.com>
2020-08-20 18:48:38 +0100

    fix: improved docker feedback when our Gemfile or Gemfile.lock has changed

--- docker/entry_point.sh
+++ docker/entry_point.sh
@@ -11,9 +11,19 @@ fi
 # and overwrite existing dependencies as subtle changes like installed Ruby version
 # will cause bundler to raise an warning and exit due to mismatch
 if ! cmp -s /docs/dependencies/Gemfile /docs/app/Gemfile; then
-  echo "The Gemfile built for this container does not match the current Gemfile i.e. they have changed";
-  echo "You must rebuild the container with: $ ./docker-run build"
+  echo "The Gemfile built for this container does not match the current Gemfile. You must rebuild the container.";
+  echo
+  echo "If you're using docker-compose you can run the following command:"
+  echo "docker-compose up --build"
   exit 2
+fi
+
+if ! cmp /docs/dependencies/Gemfile.lock /docs/app/Gemfile.lock; then
+    echo "The Gemfile.lock built for this container does not match the current Gemfile.lock. You must rebuild the container.";
+    echo
+    echo "If you're using docker-compose you can run the following command:"
+    echo "docker-compose up --build"
+    exit 2
 fi
 
 exec $@
