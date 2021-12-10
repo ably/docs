705c45d173cd7377debc2183cde93eaf5f4943f2
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-10 10:26:36 +0000

    Initial commit from gatsby

---
+++ gatsby-toolchain/gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Documentation | Ably Realtime",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
