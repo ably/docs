4875b4555b8b4e955661a5842e3f1460a2cde4ab
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-14 11:22:35 +0000

    Pipe document data through to pages

---
+++ gatsby-toolchain/data/types/index.js
@@ -0,0 +1,19 @@
+// Avoiding using an Enum as such, in favour of simpler type labels as there is no ordering/priority.
+// Trade-offs:
+/**
+ * + Simple
+ * + Serializable
+ * + Difficult to overwrite accidentally
+ * - Not using TypeScript to manage all typing
+ * - Not guaranteed unique (cf. Symbols)
+ * - Possible to overwrite (cf. Object.defineProperty())
+ * - Not maximally performant
+ * (cf. https://stackoverflow.com/questions/287903/how-can-i-guarantee-that-my-enums-definition-doesnt-change-in-javascript/50355530#50355530,
+ *  but this is extremely marginal & suggested solution in comment is not workable)
+ */
+const DataTypes = Object.freeze({
+    Partial: "Partial",
+    String: "String"
+});
+
+module.exports = DataTypes;
