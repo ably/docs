19eca4065e31a0af04e4c9b495e847beb3c39f5e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-15 09:32:45 +0000

    Show page data on Gatsby page

--- gatsby-toolchain/src/pages/404.js
+++ gatsby-toolchain/src/pages/404.js
@@ -35,7 +35,7 @@ const NotFoundPage = () => {
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
-        we couldn’t find what you were looking for.
+        we couldn&apos;t find what you were looking for.
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
