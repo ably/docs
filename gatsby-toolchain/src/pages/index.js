653b081b17cb4320e9283bc015599083d7d99be1
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-15 17:11:34 +0000

    Adding Ably UI (without styles) for Meganav

--- gatsby-toolchain/src/pages/index.js
+++ gatsby-toolchain/src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Meganav from "@ably/ui/core/Meganav";
 
 // markup
 const IndexPage = () => {
@@ -6,6 +7,7 @@ const IndexPage = () => {
     <main>
       <title>Home Page</title>
       <header>
+        <Meganav paths={{}}/>
         <h1 className="text-3xl font-bold underline">Home Page</h1>
       </header>
     </main>
