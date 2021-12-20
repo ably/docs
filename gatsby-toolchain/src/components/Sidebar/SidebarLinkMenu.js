194c2c69357b6aa5370e4f2b75f59372ea850570
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-20 09:56:26 +0000

    Tweak to sidebar component for reusability

--- gatsby-toolchain/src/components/Sidebar/SidebarLinkMenu.js
+++ gatsby-toolchain/src/components/Sidebar/SidebarLinkMenu.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Link } from 'gatsby';
+import React, { useMemo } from 'react';
 import { Accordion } from 'react-accessible-accordion';
 import SidebarLinkItem from './SidebarLinkItem';
 
 const SidebarLinkMenu = ({ data }) => {
-    const linkMenu = data.map(({ label, link, level, content }) => 
-        <SidebarLinkItem key={ label } label={ label } link={ link } level={ level } content={ content } />);
+    const linkMenu = useMemo(() => data.map(({ label, link, level, content }) => 
+        content ?
+        <SidebarLinkItem key={ label } label={ label } link={ link } level={ level } content={ content } /> :
+        <Link to={ link } >{ label }</Link>), [data]);
     return <Accordion>
         { linkMenu }
     </Accordion>;
