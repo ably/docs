fb9692c882ef60a0c81f1f57dd23ab26c760a0a4
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-20 17:27:22 +0000

    Define recursive types to allow easy querying of sidebar schema

--- gatsby-toolchain/gatsby-node.js
+++ gatsby-toolchain/gatsby-node.js
@@ -1,5 +1,20 @@
 const { createPages } = require('./data/createPages');
 const { onCreateNode } = require('./data/onCreateNode');
 
+const createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type PageFurnitureYaml implements Node {
+      label: String!
+      link: String!
+      level: Int
+      contentString: String
+      contentArray: [PageFurnitureYaml!]
+    }
+  `
+  createTypes(typeDefs)
+}
+
 exports.onCreateNode = onCreateNode;
 exports.createPages = createPages;
+exports.createSchemaCustomization = createSchemaCustomization;
