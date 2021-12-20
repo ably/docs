1aeda4fcb2b837cac787287677af0bdb4514458d
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-20 10:39:55 +0000

    Extract functions from gatsby-node

---
+++ gatsby-toolchain/data/createPages/index.js
@@ -0,0 +1,36 @@
+const { flattenContentOrderedList, maybeRetrievePartial } = require("../transform-utils");
+const path = require("path");
+
+const createPages = async ({ graphql, actions: { createPage } }) => {
+    const documentTemplate = path.resolve(`src/templates/document.js`)
+    const result = await graphql(`
+      query {
+        allFileHtml {
+          edges {
+            node {
+              slug
+              contentOrderedList {
+                data
+                type
+              }
+            }
+          }
+        }
+      }
+    `);
+    const retrievePartialFromGraphQL = maybeRetrievePartial(graphql);
+    await Promise.all(result.data.allFileHtml.edges.map(async edge => {
+      const contentOrderedList = flattenContentOrderedList(await Promise.all(edge.node.contentOrderedList.map(retrievePartialFromGraphQL)));
+  
+      createPage({
+        path: `/${edge.node.slug}`,
+        component: documentTemplate,
+        context: {
+          slug: edge.node.slug,
+          contentOrderedList
+        },
+      });
+    }));
+  }
+
+module.exports ={ createPages };
