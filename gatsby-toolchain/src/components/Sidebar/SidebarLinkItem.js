584de158c22fa50933bc6c72400e47494e27a235
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-20 09:56:26 +0000

    Tweak to sidebar component for reusability

--- gatsby-toolchain/src/components/Sidebar/SidebarLinkItem.js
+++ gatsby-toolchain/src/components/Sidebar/SidebarLinkItem.js
@@ -1,17 +1,16 @@
 import { Link } from 'gatsby';
-import React from 'react';
-import { AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from 'react-accessible-accordion';
+import React, { useMemo } from 'react';
+import SidebarItem from './SidebarItem';
 import SidebarLinkMenu from './SidebarLinkMenu';
 
-const SidebarLinkItem = ({ label, link, content, level}) => <AccordionItem>
-    <AccordionItemHeading aria-level={ level || 3 }>
-        <AccordionItemButton>
-            { label }
-        </AccordionItemButton>
-    </AccordionItemHeading>
-    <AccordionItemPanel>
-       { Array.isArray(content) ? <SidebarLinkMenu key={ label } data={ content } /> : <Link to={ link }>content</Link> }
-    </AccordionItemPanel>
-</AccordionItem>;
+const SidebarLinkItem = ({ label, link, content, level }) => {
+    const linkContent = useMemo(() => Array.isArray(content) ?
+        <SidebarLinkMenu key={ label } data={ content } /> :
+        <Link to={ link }>content</Link>, [content, label]);
+    return <SidebarItem
+        label={ label }
+        content={ linkContent }
+        level={ level } />;
+}
 
 export default SidebarLinkItem;
