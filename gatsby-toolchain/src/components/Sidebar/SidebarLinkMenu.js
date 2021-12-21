d3d84a75a0ea563065bb5acfce6c283af1ab991d
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-21 11:27:03 +0000

    Sidebar Yaml with fallback to document file structure

--- gatsby-toolchain/src/components/Sidebar/SidebarLinkMenu.js
+++ gatsby-toolchain/src/components/Sidebar/SidebarLinkMenu.js
@@ -1,11 +1,11 @@
 import { Link } from 'gatsby';
 import React, { useMemo } from 'react';
 import { Accordion } from 'react-accessible-accordion';
+import { ROOT_LEVEL } from './consts';
 import SidebarLinkItem from './SidebarLinkItem';
 
 const SidebarLinkMenu = ({ data }) => {
-    console.log(data);
-    const linkMenu = useMemo(() => data.map(({ label, link, level = 3, content = false }) => 
+    const linkMenu = useMemo(() => data.map(({ label, link, level = ROOT_LEVEL, content = false }) => 
         content ?
         <li key={ label }><SidebarLinkItem label={ label } link={ link } level={ level } content={ content } /></li> :
         <li key={ label }><Link to={ link } >{ label }</Link></li>), [data]);
---
+++ gatsby-toolchain/src/components/Sidebar/consts.js
@@ -0,0 +1,8 @@
+const ROOT_LEVEL = 3;
+const MAX_LEVEL = 6;
+
+// Consts should also be used by gatsby build scripts
+module.exports = {
+    ROOT_LEVEL,
+    MAX_LEVEL
+};
