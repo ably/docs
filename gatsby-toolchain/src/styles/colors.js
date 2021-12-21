334f8535eb80d58ed0f9126cce9d01bea6d951de
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-21 14:58:02 +0000

    Re-arranging and restyling sidebar

---
+++ gatsby-toolchain/src/styles/colors.js
@@ -0,0 +1,37 @@
+// https://brand.ably.com/colors
+
+export const primary = {
+  richOrange: '#ed760a',
+  brightOrange: '#f9a01b',
+  charcoal: '#333333',
+  white: '#fff',
+  black: '#161616',
+};
+
+export const secondary = {
+  actionBlue: '#50b4f3',
+  cableGrey: '#555555',
+  smokeGrey: '#999999',
+  deniedRed: '#EC465B',
+  acceptGreen: '#00C766',
+};
+
+export const containers = {
+  one: '#ebebeb',
+  two: '#f1f1f1',
+  three: '#f8f8f8',
+};
+
+export const borders = { ...containers };
+
+export const text = {
+  aux: secondary.smokeGrey,
+  main: secondary.cableGrey,
+  bold: primary.black,
+  highlight: primary.richOrange,
+  link: secondary.actionBlue,
+  linkInverse: primary.white,
+  linkHover: '#2f93d2',
+  linkHoverAlternate: primary.richOrange,
+  linkHoverAlternateMuted: primary.brightOrange,
+};
---
+++ gatsby-toolchain/src/styles/index.js
@@ -0,0 +1,6 @@
+import * as colors from './colors';
+import * as mq from './mq';
+
+export { colors, mq };
+export { default as fonts, weights as fontWeights } from './fonts';
+export { default as spacing } from './spacing';
