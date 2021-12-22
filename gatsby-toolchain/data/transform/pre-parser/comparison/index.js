5467ed8ac176064dbe7ebf038e7378b31435a925
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2021-12-22 15:01:20 +0000

    Add shell of pre textile parser

---
+++ gatsby-toolchain/data/transform/pre-parser/comparison/index.js
@@ -0,0 +1,12 @@
+const { identity } = require("lodash");
+
+const addCompareTable = identity;
+const addCompareNames = identity;
+const addCompareUrls = identity;
+const addCompareId = identity;
+module.exports = {
+    addCompareTable,
+    addCompareNames,
+    addCompareUrls,
+    addCompareId
+}
