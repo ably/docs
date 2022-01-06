51796308b57188a3a4af9e93f4b59cb5288903ed
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-06 11:57:27 +0000

    Add minimize for blocks, headings

--- gatsby-toolchain/src/components/blocks/Html/index.js
+++ gatsby-toolchain/src/components/blocks/Html/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import DOMPurify from 'dompurify';
 
-const Html = ({ html }) => <section dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(html, {
+// This is not to be used in production.
+const Html = ({ html }) => <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(html, {
     // The SVG and Math tags have been used in the past as attack vectors for mXSS,
     // but if we really need them should be safe enough to enable.
     // This is probably too cautious but we have no need for them at time of writing, so forbidding them is free.
     FORBID_TAGS: ['svg', 'math']
-}) }}></section>;
+}) }}></div>;
 
 export default Html;
