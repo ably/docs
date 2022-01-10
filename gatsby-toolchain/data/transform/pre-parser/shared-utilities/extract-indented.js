5bc2cb3d67902cd9f461379e25f2f079167e5e17
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-10 10:10:28 +0000

    Allow empty lines optionally in BLANG indentation regex

--- gatsby-toolchain/data/transform/pre-parser/shared-utilities/extract-indented.js
+++ gatsby-toolchain/data/transform/pre-parser/shared-utilities/extract-indented.js
@@ -1,6 +1,6 @@
 const INDENTATION_REGEX = /^(\s+).*$/m;
 
-const extractIndented = (text, name='Indentable') => {
+const extractIndented = (text, name='Indentable', allowEmptyLines=false) => {
     const indentationResult = text.match(INDENTATION_REGEX);
     if(!indentationResult || !indentationResult[1]) {
         throw `${name} blocks must be followed by indentation. Offending block: '${text.slice(0,127)}'\n`;
@@ -9,10 +9,14 @@ const extractIndented = (text, name='Indentable') => {
     const indentation = indentationResult[1];
     const indentationRegexString = `^${indentation}(.*)$`
     const indentationRegex = new RegExp(indentationRegexString, 'gm');
-    const negativeIndentationRegexString = `^(?!${indentation}).*$`;
+    const negativeIndentationRegexString = allowEmptyLines ? `^(?!${indentation}.*\\n)(?!\\s*\\n)` : `^(?!${indentation}.*)$`
     const negativeIndentationRegex = new RegExp(negativeIndentationRegexString, 'gm');
     
     const nonIndentedLineLocation = text.search(negativeIndentationRegex);
+
+    if(nonIndentedLineLocation === -1) {
+        throw `${name} block error, could not find ${negativeIndentationRegex.source} in ${text.slice(0,100)}`;
+    }
 
     const onlyIndentedLines = text.slice(0,nonIndentedLineLocation).replace(indentationRegex, '$1');
 
