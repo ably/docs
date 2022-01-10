38107631e0437d7c53988e03c99f64fa704cda39
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-10 13:57:46 +0000

    Convert div tokens to actual divs
    Also move /data/compare.yaml to better spot

--- gatsby-toolchain/data/transform/post-parser/index.js
+++ gatsby-toolchain/data/transform/post-parser/index.js
@@ -1,31 +1,9 @@
+const { convertBlangBlocksToHtml } = require('./blang');
+const { convertCollapsibleMarkupToHtml } = require('./collapsible');
+
 const addGithubLineBreaks = content => content.replaceAll('{{{github_br}}}', '\n');
 
-const TAG_REGEX_STRING = '(?:<[\\w /]+>)*\\s*';
-
-const LANG_BLOCK_POST_TEXTILE_REGEX_STRING = `LANG_(?:<span class="caps">)?BLOCK(?:<\\/span>)?`;
-/**
- * TODO: this will not work for complex HTML
- * The correct, but relatively involved, solution will be to use a DOMParser to extract the inner HTML from
- * a more generic inner string, and preserve it that way.
- * */ 
-const BLANG_REGEX_STRING = `${
-    TAG_REGEX_STRING
-}{{${LANG_BLOCK_POST_TEXTILE_REGEX_STRING}\\[([\\w,]+)\\]}}${
-        TAG_REGEX_STRING
-    }(.*?)${
-        TAG_REGEX_STRING 
-    }{{\\/${LANG_BLOCK_POST_TEXTILE_REGEX_STRING}}}${
-    TAG_REGEX_STRING
-}`;
-
-const BLANG_REGEX = new RegExp(BLANG_REGEX_STRING, 'gms');
-
 const LINK_EXTERNAL_REGEX = /(<a[^>]*)class="external"([^>]*>)/m
-
-const convertBlangBlocksToHtml = content => content.replace(
-    BLANG_REGEX,
-    '\n\n<div lang="$1"> <!-- start $1 language block -->\n$2\n</div> <!-- /end $1 language block -->\n\n'
-)
 
 const convertExternalLinksToBlankTarget = content => content.replace(LINK_EXTERNAL_REGEX, '$1target="_blank" rel="noopener noreferrer"$2');
 
@@ -33,6 +11,7 @@ const postParser = content => {
     let result = addGithubLineBreaks(content);
     result = convertBlangBlocksToHtml(result);
     result = convertExternalLinksToBlankTarget(result);
+    result = convertCollapsibleMarkupToHtml(result);
     return result;
 }
 
