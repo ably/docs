4c3a16173fdccb7c8abede1a827da12930f47a97
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-10 17:38:34 +0000

    Preserve and improve comments for regex etc.

--- gatsby-toolchain/data/transform/post-parser/blang.js
+++ gatsby-toolchain/data/transform/post-parser/blang.js
@@ -1,16 +1,20 @@
 const { TAG_REGEX_STRING } = require('./constants');
 
+/**
+ * textile-js wraps capital letters in <span class="caps"></span> tags, we need to check if those are there
+ */
 const LANG_BLOCK_POST_TEXTILE_REGEX_STRING = `LANG_(?:<span class="caps">)?BLOCK(?:<\\/span>)?`;
 /**
  * TODO: this may not work for complex HTML
  * The correct, but relatively involved, solution will be to use a DOMParser to extract the inner HTML from
  * a more generic inner string, and preserve it that way.
  * */ 
-const BLANG_REGEX_STRING = `${
+const BLANG_REGEX_STRING = `${ // Replace the outer tag
     TAG_REGEX_STRING
-}{{${LANG_BLOCK_POST_TEXTILE_REGEX_STRING}\\[([\\w,]+)\\]}}${
+}{{${LANG_BLOCK_POST_TEXTILE_REGEX_STRING}\\[([\\w,]+)\\]}}${ // The first capturing group, $1 or p1, as in LANG_BLOCK[myText]
+    // And replace the inner tag:
         TAG_REGEX_STRING
-    }(.*?)${
+    }(.*?)${ // The second capturing group, $2 or p1
         TAG_REGEX_STRING 
     }{{\\/${LANG_BLOCK_POST_TEXTILE_REGEX_STRING}}}${
     TAG_REGEX_STRING
--- gatsby-toolchain/data/transform/pre-parser/language/html-support.js
+++ gatsby-toolchain/data/transform/pre-parser/language/html-support.js
@@ -1,8 +1,40 @@
 const { identity } = require("lodash");
 
+/**
+ * Begins with bq(definition...)
+ * bq\(definition
+ * Finds an optional named `anchor` group like (definition#myAnchorGroup)
+ * (?<anchor>\#[^\)]+)?
+ * Followed by a mandatory period, then any number of spaces, then a newline.
+ * \.\s*\n
+ * Then lazily match any number of any character...
+ * .*?
+ * ...until we find a newline that is empty
+ * \n\s*\n
+ */
 const BLOCKQUOTE_REGEX = /^bq\(definition(?<anchor>\#[^\)]+)?\)\.\s*\n.*?\n\s*\n/mg;
+
+/**
+ * Any number of spaces
+ * \s*
+ * First capture group: at least one of any character, matched lazily
+ * (.+?)
+ * Any number of spaces, followed by a colon, followed by any number of spaces
+ * \s*:\s*
+ * Second capture group: same as the first
+ * (.+?)
+ * Any number of spaces, followed by a newline, circumflex/hat, or pipe symbol
+ * \s*[\n|^]
+ * Examples:
+ * "   (one) :  (two)|"
+ * "(first):(second)
+ * "
+ * " (primary) : (secondary) ^"
+ */
+const LANGUAGE_DEFINITIONS_REGEX = /\s*(.+?)\s*:\s*(.+?)\s*[\n|^]/;
+
 const blockQuoteReplacer = (matchText, anchor) => {
-    const langDefinitions = matchText.match(/\s*(.+?)\s*:\s*(.+?)\s*[\n|^]/).slice(1);
+    const langDefinitions = matchText.match(LANGUAGE_DEFINITIONS_REGEX).slice(1);
     const langs = langDefinitions.filter((_v, i) => i % 2 === 0);
     const definitions = langDefinitions.filter((_v, i) => i % 2 !== 0);
 
@@ -15,9 +47,23 @@ const blockQuoteReplacer = (matchText, anchor) => {
 }
 const addLanguageSupportForBlockQuotes = content => content.replace(BLOCKQUOTE_REGEX, blockQuoteReplacer);
 
+/**
+ * First capture: Starts with h1,h2,h3,h4,h5,h6
+ * ^(h[1-6])
+ * Second capture: optionally match (#someText), e.g. h1(#someText)
+ * (\(#[^\)]+\))?
+ * Third capture: optionally match (moreText), e.g. h1(#someText)(moreText) explicitly without a hash
+ * (\([^(\)|#)]+\))?
+ * A line of content that eventually terminates
+ * \.\s*\n.+?\n\s*\n
+ * Example:
+ * 
+ * h2(#channels-object).
+ *  default: Channels
+ */
 const HEADING_REGEX = /^(h[1-6])(\(#[^\)]+\))?(\([^(\)|#)]+\))?\.\s*\n.+?\n\s*\n/mg
 const headingReplacer = (matchText, hTag, anchor, _option) => {
-    const langDefinitions =matchText.match(/\s*(.+?)\s*:\s*(.+?)\s*[\n|^]/);
+    const langDefinitions = matchText.match(/\s*(.+?)\s*:\s*(.+?)\s*[\n|^]/);
     const langs = langDefinitions.filter((_v, i) => i % 2 === 0);
     const definitions = langDefinitions.filter((_v, i) => i % 2 !== 0);
     const langSpans = langs.map((lang, i) =>
@@ -25,6 +71,7 @@ const headingReplacer = (matchText, hTag, anchor, _option) => {
             `<span lang='javascript,nodejs'>${definitions[i]}</span>` :
             `<span lang='${lang}'>${definitions[i]}</span>`
     ).join('');
+    // TODO: Replace this with another token, to be parsed in post-parser
     return `${hTag}${anchor}. ${langSpans}`
 }
 const addLanguageSupportForHeadings = content => content.replace(HEADING_REGEX, headingReplacer);
