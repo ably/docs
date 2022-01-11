b1f24601d1e83f76ba9030d17a61b9f41c90de4f
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-11 11:16:57 +0000

    Round off textile parser work

--- gatsby-toolchain/data/transform/pre-parser/index.js
+++ gatsby-toolchain/data/transform/pre-parser/index.js
@@ -16,19 +16,18 @@ const {
     addSpecAnchorLinks
 } = require('../parser-enhancements/spec');
 const {
-    addCompareTable,
-    addCompareNames,
-    addCompareUrls,
-    addCompareId
-} = require('../parser-enhancements/comparison');
-const {
     addSupportForInlineCodeEditor
 } = require('../parser-enhancements/inline-code');
+
+// Newlines before closing tags inhibit textile-js' ability to correctly parse HTML
+const removeNewlinesBeforeClosingTags = content => content.replace(/\n+(<\/\w+>)/g,'$1');
+
 //TODO: Everything related to creating its own blocks/datatypes should be put into the parsing stage, which should include parseNanocPartials
 // & reduce over the array created by that function.
 const preParser = (content) => {
     // Readability/Semantic operations
     let result = stripComments(content);
+    result = removeNewlinesBeforeClosingTags(result);
     result = addMinimizeForHeadings(result);
     result = addMinimizedIndent(result);
     // Language Operations
@@ -43,13 +42,8 @@ const preParser = (content) => {
     // TODO: Move commented out functions to place where they have access to metadata/attributes/frontmatter
     // TODO: add anchor_specs to variables that must be included from frontmatter
     // result = addSpecAnchorLinks(result, attributes);
-    // Comparison operations
-    // TODO: load compare.yaml into GraphQL
-    // result = addCompareTable(result, attributes);
-    // result = addCompareNames(result, attributes);
-    // result = addCompareUrls(result, attributes);
-    // result = addCompareId(result, attributes);
     // Inline code editor operations
+    // TODO: Move commented out functions to place where they have access to JSBin YAML
     // result = addSupportForInlineCodeEditor(result, path);
     return result;
 }
