dca9620c8b00ac959076a70c7a90fa8201f08db6
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-11 11:16:57 +0000

    Round off textile parser work

--- gatsby-toolchain/data/transform/post-parser/blang.js
+++ gatsby-toolchain/data/transform/post-parser/blang.js
@@ -3,16 +3,16 @@ const { TAG_REGEX_STRING } = require('./constants');
 /**
  * textile-js wraps capital letters in <span class="caps"></span> tags, we need to check if those are there
  */
-const LANG_BLOCK_POST_TEXTILE_REGEX_STRING = `LANG_(?:<span class="caps">)?BLOCK(?:<\\/span>)?`;
+const LANG_BLOCK_POST_TEXTILE_REGEX_STRING = `LANG_(?:<span class=["']caps["']>)?BLOCK(?:<\\/span>)?`;
 /**
  * TODO: this may not work for complex HTML
  * The correct, but relatively involved, solution will be to use a DOMParser to extract the inner HTML from
  * a more generic inner string, and preserve it that way.
  * */ 
-const BLANG_REGEX_STRING = `${ // Replace the outer tag
+const BLANG_REGEX_STRING = `${ // Replace the outer tag if any
     TAG_REGEX_STRING
 }{{${LANG_BLOCK_POST_TEXTILE_REGEX_STRING}\\[([\\w,]+)\\]}}${ // The first capturing group, $1 or p1, as in LANG_BLOCK[myText]
-    // And replace the inner tag:
+    // And replace the inner tag if any:
         TAG_REGEX_STRING
     }(.*?)${ // The second capturing group, $2 or p1
         TAG_REGEX_STRING 
@@ -21,10 +21,23 @@ const BLANG_REGEX_STRING = `${ // Replace the outer tag
 }`;
 
 const BLANG_REGEX = new RegExp(BLANG_REGEX_STRING, 'gms');
-
 const convertBlangBlocksToHtml = content => content.replace(
     BLANG_REGEX,
-    '\n\n<div lang="$1"> <!-- start $1 language block -->\n$2\n</div> <!-- /end $1 language block -->\n\n'
+    (_match, p1, p2) => {
+        const trimmedLines = p2 ?
+            p2.replace(/^\s+/gm, "")
+                .replace(/^(?!<p>)(.*)<\/p>$/gm,'$1') :
+            '';
+        return `\n\n<div lang="${
+            p1
+        }"><!-- start ${
+            p1
+        } language block -->\n${
+            trimmedLines
+        }\n</div><!-- /end ${
+            p1
+        } language block -->\n\n`
+    }
 )
 
 module.exports = {
