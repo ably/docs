4d10c46c03331c25d8f124a4ea0fc9d16ca6ca2f
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-12 10:55:09 +0000

    Extract data for Gatsby, add any HTML elements it is possible we will support

---
+++ gatsby-toolchain/data/types/html.js
@@ -0,0 +1,81 @@
+const HtmlDataTypes = Object.freeze({
+    // Base types
+    Tag: "tag",
+    Text: "text",
+    // Text
+    P: "p",
+    Pre: "pre",
+    Code: "code",
+    // Semantic styles
+    Strong: "strong",
+    Em: "em",
+    Small: "small",
+    Sub: "sub",
+    Sup: "sup",
+    // Enhancements
+    Abbr: "abbr",
+    Details: "details",
+    Summary: "summary",
+    Dfn: "dfn",
+    Time: "time",
+    // Software-related
+    Kbd: "kbd",
+    Output: "output",
+    Samp: "samp",
+    Var: "var",
+    // External references
+    A: "a",
+    Iframe: "iframe",
+    Img: "img",
+    Video: "video",
+    Audio: "audio",
+    Address: "address",
+    Blockquote: "blockquote",
+    Q: "q",
+    // Headings
+    Header: "header",
+    H1: "h1",
+    H2: "h2",
+    H3: "h3",
+    H4: "h4",
+    H5: "h5",
+    H6: "h6",
+    // Tables
+    Caption: "caption",
+    Colgroup: "colgroup",
+    Col: "col",
+    Table: "table",
+    Tbody: "tbody",
+    Td: "td",
+    Tfoot: "tfoot",
+    Th: "th",
+    Thead: "thead",
+    Tr: "tr",
+    // Lists
+    Dl: "dl",
+    Dd: "dd",
+    Dt: "dt",
+    Li: "li",
+    Ol: "ol",
+    Ul: "ul",
+    // Dividers
+    Main: "main",
+    Article: "article",
+    Section: "section",
+    Aside: "aside",
+    Span: "span",
+    Div: "div",
+    // Complex functionality
+    Input: "input",
+    Button: "button",
+    Meter: "meter",
+    Noscript: "noscript",
+    Progress: "progress",
+    Select: "select",
+    Textarea: "textarea",
+    // Other/utility
+    Br: "br",
+    Wbr: "wbr"
+});
+
+module.exports = HtmlDataTypes;
