5669d56c9d9d18243080d43b3c5a3ae14909ea27
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-12 11:27:09 +0000

    Recursive extraction functional, TODO: tests

--- gatsby-toolchain/data/html-parser/index.js
+++ gatsby-toolchain/data/html-parser/index.js
@@ -3,31 +3,33 @@ const cheerio = require("cheerio");
 const DataTypes = require("../types");
 const HtmlDataTypes = require("../types/html");
 
-const cheerioParser = cheerioNode => {
-    const children = cheerioNode.children('*');
-    const data = children.map((i, { type, name = '', data = '', attribs = {} }) => {
-        if(type === HtmlDataTypes.Text) {
-            return {
-                data,
-                type: HtmlDataTypes.Text,
-                name: HtmlDataTypes.Text
-            }
-        }
-
+const cheerioNodeParser = (_i, { type = null, name = '', data = '', attribs = {}, children = [] }) => {
+    if(!type || type === HtmlDataTypes.text) {
         return {
             data,
-            type: HtmlDataTypes.Tag,
-            name,
-            attribs
-        };
-    });
+            type: HtmlDataTypes.text,
+            name: HtmlDataTypes.text
+        }
+    }
+    const nextItems = children.map((data, i) => cheerioNodeParser(i, data));
+    // console.log(nextItems);
+    return {
+        data: nextItems,
+        type: HtmlDataTypes.tag,
+        name: HtmlDataTypes[name] ?? DataTypes.Html,
+        attribs
+    };
+};
+
+const cheerioParser = cheerioNodes => {
+    const data = cheerioNodes.map(cheerioNodeParser);
     return data.toArray();
 }
 
 const htmlParser = content => {
     const loadedDom = cheerio.load(content, null);
-    const parsedNodes = cheerioParser(loadedDom('body'));
-    console.log(parsedNodes);
+    const parsedNodes = cheerioParser(loadedDom('body').children('*'));
+    // console.log(parsedNodes);
     return [{
         data: content,
         type: DataTypes.Html
