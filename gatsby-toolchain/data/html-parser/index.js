2276191c1ba936f7c1413bee44be8d651eb1f355
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-12 10:55:09 +0000

    Extract data for Gatsby, add any HTML elements it is possible we will support

--- gatsby-toolchain/data/html-parser/index.js
+++ gatsby-toolchain/data/html-parser/index.js
@@ -1,6 +1,33 @@
-const DataTypes = require("../types")
+// Essentially jQuery for parsing the DOM
+const cheerio = require("cheerio");
+const DataTypes = require("../types");
+const HtmlDataTypes = require("../types/html");
+
+const cheerioParser = cheerioNode => {
+    const children = cheerioNode.children('*');
+    const data = children.map((i, { type, name = '', data = '', attribs = {} }) => {
+        if(type === HtmlDataTypes.Text) {
+            return {
+                data,
+                type: HtmlDataTypes.Text,
+                name: HtmlDataTypes.Text
+            }
+        }
+
+        return {
+            data,
+            type: HtmlDataTypes.Tag,
+            name,
+            attribs
+        };
+    });
+    return data.toArray();
+}
 
 const htmlParser = content => {
+    const loadedDom = cheerio.load(content, null);
+    const parsedNodes = cheerioParser(loadedDom('body'));
+    console.log(parsedNodes);
     return [{
         data: content,
         type: DataTypes.Html
