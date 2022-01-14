977ea1d9267e910ac9dc982c1000976b60e85d6d
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-14 10:37:54 +0000

    Render paragraphs (only)

--- gatsby-toolchain/src/components/blocks/Html/index.js
+++ gatsby-toolchain/src/components/blocks/Html/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import DOMPurify from 'dompurify';
+import blocksFromData from '../blocks-from-data';
+import { isArray } from 'lodash';
 
 // This is not to be used in production.
-const Html = ({ html }) => <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(html, {
+const DangerousHtml = ({ data }) => <div dangerouslySetInnerHTML={{__html: DOMPurify.sanitize(data, {
     // The SVG and Math tags have been used in the past as attack vectors for mXSS,
     // but if we really need them should be safe enough to enable.
     // This is probably too cautious but we have no need for them at time of writing, so forbidding them is free.
     FORBID_TAGS: ['svg', 'math']
 }) }}></div>;
 
+const Html = ({ data, attribs }) => isArray(data) ? <>{blocksFromData(data)}</> : <>{data}</>;
+
+export {
+    DangerousHtml
+};
 export default Html;
