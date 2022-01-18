ac1c108705bfe0bdcda1b102b7bb59856a37be2a
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-01-18 13:54:42 +0000

    Style h tags like Voltaire (#2)
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- gatsby-toolchain/src/components/blocks/headings/H1.js
+++ gatsby-toolchain/src/components/blocks/headings/H1.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+import { fonts, spacing } from '../../../styles';
 import Html from '../Html';
 
-const H1 = ({ data, attribs }) => <h1 {...attribs}><Html data={ data } /></h1>;
+const StyledH1 = styled.h1`
+  ${fonts.h1}
+  margin-bottom: ${spacing.medium};
+`;
+
+const H1 = ({ data, attribs }) => <StyledH1 {...attribs}><Html data={ data } /></StyledH1>;
 
 export default H1;
