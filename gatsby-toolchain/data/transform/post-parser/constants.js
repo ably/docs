0a0819801fc524aac663fb9cf1ed523163136de1
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-19 10:55:02 +0000

    Add tests, fix bugs in support for heading languages

--- gatsby-toolchain/data/transform/post-parser/constants.js
+++ gatsby-toolchain/data/transform/post-parser/constants.js
@@ -1,9 +1,15 @@
 // Shared constants between different post-parse operations
 /**
- * Any number of opening or closing XML tags, inc. HTML tags, followed by any number of spaces
+ * Any number of opening XML tags, inc. HTML tags, followed by any number of spaces
  */
-const TAG_REGEX_STRING = '(?:<[\\w \\/]+>)*[\\s\\r\\n]*';
+ const TAG_OPEN_REGEX_STRING = '(?:<[\\w ]+>)*[\\s\\r\\n]*';
+
+/**
+ * Any number of closing XML tags, inc. HTML tags, followed by any number of spaces
+ */
+const TAG_CLOSE_REGEX_STRING = '(?:</[\\w ]+>)*[\\s\\r\\n]*';
 
 module.exports = {
-    TAG_REGEX_STRING
+    TAG_OPEN_REGEX_STRING,
+    TAG_CLOSE_REGEX_STRING
 }
