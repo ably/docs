ecfd4b69d81943ee966c10be083e1c645f5e6af1
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-20 13:39:41 +0000

    Add workaround and tests for new textile on definition lists
    also fix bugs related to DLs

--- gatsby-toolchain/src/components/blocks/list/index.js
+++ gatsby-toolchain/src/components/blocks/list/index.js
@@ -1,6 +1,6 @@
 export { default as Dd } from './Dd';
 export { default as Dl } from './Dl';
-export { default as Dt } from './Dd';
+export { default as Dt } from './Dt';
 export { default as Li } from './Li';
 export { default as Ol } from './Ol';
 export { default as Ul } from './Ul';
