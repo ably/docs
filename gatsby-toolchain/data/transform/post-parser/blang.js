907ab799dda3224289cc50dfc438ad4b377bc556
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-21 10:20:46 +0000

    Fix blang parsing in partial textile files, add test for failing case

--- gatsby-toolchain/data/transform/post-parser/blang.js
+++ gatsby-toolchain/data/transform/post-parser/blang.js
@@ -4,11 +4,7 @@ const { TAG_OPEN_REGEX_STRING, TAG_CLOSE_REGEX_STRING } = require('./constants')
  * textile-js wraps capital letters in <span class="caps"></span> tags, we need to check if those are there
  */
 const LANG_BLOCK_POST_TEXTILE_REGEX_STRING = `LANG_(?:<span class=["']caps["']>)?BLOCK(?:<\\/span>)?`;
-/**
- * TODO: this may not work for complex HTML
- * The correct, but relatively involved, solution will be to use a DOMParser to extract the inner HTML from
- * a more generic inner string, and preserve it that way.
- * */ 
+
 const BLANG_REGEX_STRING = `${ // Replace the outer tag if any
     TAG_OPEN_REGEX_STRING
 }{{${ // The first capturing group, $1 or p1, as in LANG_BLOCK[myText]
