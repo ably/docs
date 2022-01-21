8fc5d5f550e48bbb588459db218259aa767a60b3
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-21 10:20:46 +0000

    Fix blang parsing in partial textile files, add test for failing case

--- gatsby-toolchain/data/transform/pre-parser/language/html-support.test.js
+++ gatsby-toolchain/data/transform/pre-parser/language/html-support.test.js
@@ -19,7 +19,7 @@ describe('Language support for headings replaces headings of the format:' +
     'h1(#id).\ndefault: heading with a string with language-specific spans', () => {
     test('Simple strings are translated into the correct <span > format', () => {
         const result = addLanguageSupportForHeadings(headingWithLanguage);
-        expect(result).toEqual(`h3(#channel-state). <span lang='default'>ChannelState</span><span lang='objc,swift'>ARTRealtimeChannelState</span><span lang='ruby'>Channel::STATE Enum</span><span lang='java'>io.ably.lib.realtime.ChannelState Enum</span><span lang='csharp'>IO.Ably.Realtime.ChannelState Enum</span><span lang='flutter'>ably.ChannelState Enum</span>`);
+        expect(result).toEqual(`h3(#channel-state). <span lang='default'>ChannelState</span><span lang='objc,swift'>ARTRealtimeChannelState</span><span lang='ruby'>Channel::STATE Enum</span><span lang='java'>io.ably.lib.realtime.ChannelState Enum</span><span lang='csharp'>IO.Ably.Realtime.ChannelState Enum</span><span lang='flutter'>ably.ChannelState Enum</span>\n`);
     });
 });
 
