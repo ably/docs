db24d631d62991b6b2246977c8c7ae786468c36e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-21 11:31:06 +0000

    Add workaround for textile nested h[1-6] tags, add test for same

--- gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.js
+++ gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.js
@@ -1,5 +1,16 @@
+// textile-js, unlike RedCloth, cannot parse multiple new lines between list items
+// each list item will instead be wrapped in its own list collection
 const compressMultipleNewlinesInLists = content => content.replace(/^(-.*)\n{2,}(?![^-])/gm, '$1\n');
 
+// textile-js cannot parse h[1-6]. lines that are located inside another HTML tag, with leading spaces
+const manuallyReplaceHTags = content => content.replace(/^\s*h([1-6]).\s+(.*)$/gm, '<h$1>$2</h$1>');
+
+/**
+ * TODO: Of interest are:
+ * l.1207 in channels.textile
+*/
+
 module.exports = {
-    compressMultipleNewlinesInLists
+    compressMultipleNewlinesInLists,
+    manuallyReplaceHTags
 }
