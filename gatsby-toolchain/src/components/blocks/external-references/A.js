90bcd2148b430b72315c8ca8a9e71c5193cd1c03
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-21 14:20:30 +0000

    Preloading links for Gatsby

--- gatsby-toolchain/src/components/blocks/external-references/A.js
+++ gatsby-toolchain/src/components/blocks/external-references/A.js
@@ -1,6 +1,15 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import Html from '../Html';
 
-const A = ({ data, attribs }) => <a {...attribs}><Html data={ data } /></a>;
+const A = ({ data, attribs }) => {
+    if(
+        attribs.href &&
+        /^(\/|#|https?:\/\/(?:www.)?ably.com\/documentation).*/.test(attribs.href)
+    ) {
+        return <Link {...{ ...attribs, to: attribs.href }}><Html data={ data } /></Link>
+    }
+    return <a {...attribs}><Html data={ data } /></a>;
+}
 
 export default A;
