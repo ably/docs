b8e6f4a5eff331d2cf43cf099007ffa056dec6a4
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-21 11:31:06 +0000

    Add workaround for textile nested h[1-6] tags, add test for same

--- gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.test.js
+++ gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.test.js
@@ -1,5 +1,5 @@
 const textile = require("textile-js");
-const { compressMultipleNewlinesInLists } = require(".");
+const { compressMultipleNewlinesInLists, manuallyReplaceHTags } = require(".");
 
 const definitionList = `h3(#connection-states). Available connection states
 
@@ -47,4 +47,21 @@ describe('Reads a definition string correctly', () => {
     test('A definition string is rendered into a valid HTML definition list from textile', () => {
         expect(textile(compressMultipleNewlinesInLists(definitionList)).replace(/\s/g, '')).toEqual(expectedDefinitionList.replace(/\s/g, ''));
     });
+});
+
+const nestedH1_6String = `<div>
+    h1. Lorem ipsum
+vos populi vos dei
+calamitus calamitabandum
+</div>`;
+
+const nestedH1_6Html = `<div><h1>Lorem ipsum</h1>
+vos populi vos dei
+calamitus calamitabandum
+</div>`;
+
+describe('Reads an h[1-6]. string correctly', () => {
+    test('An h[1-6]. line nested inside an outer HTML tag gets read correctly in textile', () => {
+        expect(textile(manuallyReplaceHTags(nestedH1_6String))).toEqual(nestedH1_6Html);
+    })
 });
