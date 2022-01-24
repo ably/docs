b6da23cf1635e22e850f7de92fc59a1821431688
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-24 09:30:19 +0000

    Fix preParser treatment of nested divs

--- gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.test.js
+++ gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.test.js
@@ -4,8 +4,10 @@ const {
     definitionList,
     expectedDefinitionList,
     nestedH1_6String,
-    nestedH1_6Html
+    nestedH1_6Html,
+    nestedDiv
 } = require('./workarounds.raw.examples');
+const { preParser } = require('../');
 
 describe('Reads a definition string correctly', () => {
     test('A definition string is rendered into a valid HTML definition list from textile', () => {
@@ -17,5 +19,13 @@ describe('Reads a definition string correctly', () => {
 describe('Reads an h[1-6]. string correctly', () => {
     test('An h[1-6]. line nested inside an outer HTML tag gets read correctly in textile', () => {
         expect(textile(manuallyReplaceHTags(nestedH1_6String))).toEqual(nestedH1_6Html);
-    })
+    });
 });
+
+describe('Reads nested divs correctly', () => {
+    test('A nested div gets read correctly after preParse operations', () => {
+        const processedDivs = textile(preParser(nestedDiv));
+        const firstDiv = processedDivs.split('\n')[1];
+        expect(firstDiv).toEqual(`<div lang=\"default\">`);
+    });
+})
