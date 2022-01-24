c021a9fddc8b2fa954ecb4452b719bdd43b1163e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-01-24 11:42:48 +0000

    Fix nested/consecutive/list-related bug with divs and spans
    also add tests for same

--- gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.js
+++ gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/index.js
@@ -3,7 +3,7 @@
 const compressMultipleNewlinesInLists = content => content.replace(/^(-.*)\n{2,}(?![^-])/gm, '$1\n');
 
 // textile-js cannot parse h[1-6]. lines that are located inside another HTML tag, with leading spaces
-const manuallyReplaceHTags = content => content.replace(/^\s*h([1-6]).\s+(.*)$/gm, '<h$1>$2</h$1>');
+const manuallyReplaceHTags = content => content.replace(/^\s*h([1-6])\.\s+(.*)$/gm, '<h$1>$2</h$1>');
 
 
 module.exports = {
