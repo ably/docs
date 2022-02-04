4f66490368a51070708a867772a1855ccbfd9fdf
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-02-04 10:56:27 +0000

    Language selection menu (#4)
    * Create basic language context provider
    
    * Use a wrapper rather than a HoC to provide language context
    
    * Also add extra documentation
    
    * Bugfix: remove typo from jsall expansion
    
    * Horizontal menu component
    
    * Correct parsing of languages

---
+++ gatsby-toolchain/src/contexts/page-language-context.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { DEFAULT_LANGUAGE } from '../../data/createPages/createPageVariants';
+
+const PageLanguageContext = React.createContext(DEFAULT_LANGUAGE);
+
+export default PageLanguageContext;
