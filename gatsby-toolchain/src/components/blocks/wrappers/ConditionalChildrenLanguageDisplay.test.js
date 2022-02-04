c342eac49f591ad130759eb89284c1d6512a150f
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-04 15:31:04 +0000

    Improve ConditionalLanguageDisplay, add tests

---
+++ gatsby-toolchain/src/components/blocks/wrappers/ConditionalChildrenLanguageDisplay.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { DEFAULT_LANGUAGE } from '../../../../data/createPages/createPageVariants';
+import { Span } from '../dividers';
+import ConditionalChildrenLanguageDisplay from './ConditionalChildrenLanguageDisplay';
+
+const twoChildrenInstance = TestRenderer.create(<ConditionalChildrenLanguageDisplay>
+    <Span data='content' attribs={{ lang: DEFAULT_LANGUAGE }}/>
+    <Span data='alternative' attribs={{ lang: 'javascript' }}/>
+</ConditionalChildrenLanguageDisplay>).root;
+
+describe('ConditionalChildrenLanguageDisplay only displays one child of alternatives', () => {
+    test('A basic instance of two children with different lang attributes only shows the default language option', () => {
+        expect(twoChildrenInstance.findAllByType(Span).length).toBe(1);
+    });
+});
