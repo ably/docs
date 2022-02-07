183d169b6b7b9a3706894dc3b78b32fa4383f1c2
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-02-07 13:02:30 +0000

    Add Local Language Selector (#5)
    * Add stylings from Voltaire CodeBlock
    
    * Add syntax highlighting
    
    * Fix Gatsby scrolling behaviour
    
    * Implement local language selector

---
+++ gatsby-toolchain/src/components/Menu/MenuItem/MenuItemButton/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { text } from '../../../../styles/colors';
+
+const UnstyledButton = styled.button`
+    appearance: none !important;
+    background: none;
+    outline: none;
+    box-shadow: none;
+    border: 0;
+    border-radius: 0;
+`;
+
+const MenuItemButton = styled(UnstyledButton)`
+    color: ${text.linkInverse};
+
+    &:focus,
+    &:hover {
+        color: ${text.linkHoverAlternate};
+    }
+`;
+
+const SelectedMenuItemButton = styled(MenuItemButton)`
+    color: ${text.highlight};
+`;
+
+export {
+    SelectedMenuItemButton
+};
+
+export default MenuItemButton;
