7b880d016df498670f3a1f87a2df24daec047f1e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-15 14:39:44 +0000

    Small improvement to on-page navigation

--- gatsby-toolchain/src/components/Sidebar/SidebarLink.js
+++ gatsby-toolchain/src/components/Sidebar/SidebarLink.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
 import { SidebarHeadingStyle } from './styles';
 
-const SidebarLink = styled(Link)`${SidebarHeadingStyle}`;
+const onPageNav = /[#?]/.test;
+
+const SidebarGatsbyLink = styled(Link)`${SidebarHeadingStyle}`;
+const SidebarAnchor = styled.a`${SidebarHeadingStyle}`;
+
+const SidebarLink = ({ to, ...props  }, children) => onPageNav(to) ?
+    /**
+     *  Relevant page of documentation: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-link/#recommendations-for-programmatic-in-app-navigation
+     *  "If you need this (in-app navigation) behavior, you should either use an anchor tag or import the navigate helper from gatsby"
+     */
+    <SidebarAnchor to={to} {...props}>{children}</SidebarAnchor> :
+    <SidebarGatsbyLink to={to} {...props}>{children}</SidebarGatsbyLink>
 
 export default SidebarLink;
