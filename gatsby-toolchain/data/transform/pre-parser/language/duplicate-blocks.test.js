176fdd6fe09a2e5cbe813de9054e58268785a5be
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-17 14:55:17 +0000

    Fix straightforward React warnings, bring test coverage above 60% (funcs)

---
+++ gatsby-toolchain/data/transform/pre-parser/language/duplicate-blocks.test.js
@@ -0,0 +1,41 @@
+import { duplicateLanguageBlocks } from ".";
+import { duplicateLanguageBlock } from "./duplicate-blocks";
+
+const langBlockExpected = `h1[javascript](exampleClass). 
+Some content`;
+
+describe('Given the data for a single language block, constructs a valid output', () => {
+    it('Creates a valid output block', () => {
+        expect(duplicateLanguageBlock('h1', ['exampleClass'],'Some content')('javascript'))
+            .toEqual(langBlockExpected);
+    });
+})
+
+const bcTest = `bc[javascript,csharp].
+Some content
+
+`;
+const bcExpected =
+`bc[javascript]. 
+Some content
+
+bc[csharp]. 
+Some content
+
+`;
+
+const h1SingleLanguageTest = `h1[javascript].
+Some content
+
+`;
+
+describe('Given a valid language block with multiple entries ([lang1, lang2]), creates multiple language blocks ([lang1][lang2])', () => {
+    it('Works on bc p or h language blocks', () => {
+        const result = duplicateLanguageBlocks(bcTest);
+        expect(result).toEqual(bcExpected);
+    });
+    it('Leaves single blocks unchanged', () => {
+        const result = duplicateLanguageBlocks(h1SingleLanguageTest);
+        expect(result).toEqual(h1SingleLanguageTest);
+    });
+});
