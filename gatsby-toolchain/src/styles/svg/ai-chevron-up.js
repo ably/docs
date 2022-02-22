fac81c8dfe4f347992b303d3671964ca682d85ee
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-02-22 09:29:27 +0000

    Circleci lint (#10)
    * CircleCI config for lint and test

--- gatsby-toolchain/src/styles/svg/ai-chevron-up.js
+++ gatsby-toolchain/src/styles/svg/ai-chevron-up.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 // Gatsby SVG React plugin does not work with latest version of Gatsby/webpack
-const AIChevronUp = () => <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
+const AIChevronUp = () => (
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24">
     <title>ai-chevron-up</title>
     <path d="M12.074 4.926c-0.608-0.031-1.226 0.186-1.69 0.651l-9.493 9.493c-0.869 0.869-0.868 2.284 0.005 3.157 0.879 0.879 2.286 0.875 3.157 0.005l7.949-7.949 7.949 7.949c0.869 0.869 2.284 0.868 3.157-0.005 0.879-0.879 0.875-2.286 0.005-3.157l-9.493-9.493c-0.426-0.426-0.984-0.643-1.544-0.651z"></path>
-</svg>;
+  </svg>
+);
 
 export default AIChevronUp;
