dd1a51bd8120c7ae3ec17b744c27a27ff60ca801
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-02-22 09:29:27 +0000

    Circleci lint (#10)
    * CircleCI config for lint and test

--- gatsby-toolchain/src/components/blocks/headings/H4.js
+++ gatsby-toolchain/src/components/blocks/headings/H4.js
@@ -3,8 +3,8 @@ import { fonts, spacing } from '../../../styles';
 import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
 const StyledH4 = styled.h4`
-    ${fonts.h4};
-    margin: 40px 0 ${spacing.small};
+  ${fonts.h4};
+  margin: 40px 0 ${spacing.small};
 `;
 
 const H4 = GenericHtmlBlock(StyledH4);
--- gatsby-toolchain/src/components/blocks/wrappers/LocalLanguageAlternatives.js
+++ gatsby-toolchain/src/components/blocks/wrappers/LocalLanguageAlternatives.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react';
-import { DEFAULT_LANGUAGE } from "../../../../data/createPages/constants";
+import PropTypes from 'prop-types';
+import { DEFAULT_LANGUAGE } from '../../../../data/createPages/constants';
 import languageLabels from '../../../maps/language';
 import MenuItemButton, { SelectedMenuItemButton } from '../../Menu/MenuItem/MenuItemButton';
 import Html from '../Html';
 import HorizontalMenu from '../../Menu/HorizontalMenu';
+import { ChildPropTypes } from '../../../react-utilities';
 
 const LocalLanguageAlternatives = ({ language, languages, data, children }) => {
-    const [selected, setSelected] = useState(children);
-    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
+  const [selected, setSelected] = useState(children);
+  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
 
-    const onClick = ({target : { value }}) => {
-        setSelected(<Html data={data[value]} />)
-        setSelectedLanguage(value);
-    };
+  const onClick = ({ target: { value } }) => {
+    setSelected(<Html data={data[value]} />);
+    setSelectedLanguage(value);
+  };
 
-    const label = language === DEFAULT_LANGUAGE ? 
-        `Select:` :
-        `No ${languageLabels[language]} example exists, select:`;
-    const languageItems = languages.map(lang => ({
-        Component: lang === selectedLanguage ? SelectedMenuItemButton : MenuItemButton,
-        props: {
-            onClick,
-            value: lang
-        },
-        content: languageLabels[lang] ?? ''
-    }));
+  const label = language === DEFAULT_LANGUAGE ? `Select:` : `No ${languageLabels[language]} example exists, select:`;
+  const languageItems = languages.map((lang) => ({
+    Component: lang === selectedLanguage ? SelectedMenuItemButton : MenuItemButton,
+    props: {
+      onClick,
+      value: lang,
+    },
+    content: languageLabels[lang] ?? '',
+  }));
 
-    return <>
-        <HorizontalMenu label={ label } items={ languageItems } />
-        {selected}
-    </>;
+  return (
+    <>
+      <HorizontalMenu label={label} items={languageItems} />
+      {selected}
+    </>
+  );
+};
+
+LocalLanguageAlternatives.propTypes = {
+  language: PropTypes.string,
+  languages: PropTypes.array,
+  data: PropTypes.object,
+  children: ChildPropTypes,
 };
 
 export default LocalLanguageAlternatives;
