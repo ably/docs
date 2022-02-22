d9a2cf8db5af968eebea0e27c3e784eb0b153468
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-02-22 09:29:27 +0000

    Circleci lint (#10)
    * CircleCI config for lint and test

--- gatsby-toolchain/src/components/Link/LanguageLink.js
+++ gatsby-toolchain/src/components/Link/LanguageLink.js
@@ -1,42 +1,45 @@
 import { Link } from 'gatsby';
 import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { DEFAULT_LANGUAGE } from "../../../data/createPages/constants";
+import { DEFAULT_LANGUAGE } from '../../../data/createPages/constants';
 import PageLanguageContext from '../../contexts/page-language-context';
 import languageLabels from '../../maps/language';
 import { primary, text } from '../../styles/colors';
 
 const HoverLink = styled(Link)`
-    cursor: pointer;
-    display: block;
-    position: relative;
-    user-select: none;
-    color: ${text.aux};
+  cursor: pointer;
+  display: block;
+  position: relative;
+  user-select: none;
+  color: ${text.aux};
 
-    &:hover, &.active, &.is-active {
-        color: ${ primary.richOrange}
-    }
+  &:hover,
+  &.active,
+  &.is-active {
+    color: ${primary.richOrange};
+  }
 `;
 
 const LanguageLink = ({ language }) => {
-    const pageLanguage = useContext(PageLanguageContext);
+  const pageLanguage = useContext(PageLanguageContext);
 
-    const languageIsDefault = language === DEFAULT_LANGUAGE;
-    const pageIsDefault = pageLanguage === DEFAULT_LANGUAGE;
+  const languageIsDefault = language === DEFAULT_LANGUAGE;
+  const pageIsDefault = pageLanguage === DEFAULT_LANGUAGE;
 
-    const href = pageIsDefault ?
-        `./language/${language}` :
-        `../../${
-            languageIsDefault ?
-            '' :
-            `language/${language}`
-        }`;
+  const href = pageIsDefault ? `./language/${language}` : `../../${languageIsDefault ? '' : `language/${language}`}`;
 
-    const isActiveClassName = language === pageLanguage ? 'active' : 'inactive';
+  const isActiveClassName = language === pageLanguage ? 'active' : 'inactive';
 
-    return <HoverLink className={isActiveClassName} to={href}>
-        { languageLabels[language] ?? language }
-    </HoverLink>;
-}
+  return (
+    <HoverLink className={isActiveClassName} to={href}>
+      {languageLabels[language] ?? language}
+    </HoverLink>
+  );
+};
+
+LanguageLink.propTypes = {
+  language: PropTypes.string,
+};
 
 export default LanguageLink;
