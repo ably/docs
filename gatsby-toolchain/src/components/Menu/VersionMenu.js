dad2959e8b8d3f400f1de617f8b4f40fc479dc23
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-25 15:10:18 +0000

    Warning message if API version is older

--- gatsby-toolchain/src/components/Menu/VersionMenu.js
+++ gatsby-toolchain/src/components/Menu/VersionMenu.js
@@ -4,6 +4,7 @@ import Select from 'react-select';
 import { SmallMenuLabel } from './Label';
 import { LATEST_ABLY_API_VERSION_STRING } from '../../../data/transform/constants';
 import { navigate } from 'gatsby';
+import { Warning } from '../Notifications';
 
 const routeToPage = ({ value }) => {
   navigate(`/documentation/${value}`);
@@ -24,12 +25,23 @@ const VersionMenu = ({ versions, version, rootVersion }) => {
   );
   const currentValue = version ? { label: version, value: version } : latestOption;
 
-  return versions.length > 0 ? (
-    <>
-      <SmallMenuLabel htmlFor={'version-menu'}>API Version:</SmallMenuLabel>
-      <Select inputId={'version-menu'} value={currentValue} options={options} onChange={routeToPage} />
-    </>
-  ) : null;
+  return (
+    <div>
+      {versions.length > 0 && (
+        <>
+          <SmallMenuLabel htmlFor={'version-menu'}>API Version:</SmallMenuLabel>
+          <Select inputId={'version-menu'} value={currentValue} options={options} onChange={routeToPage} />
+        </>
+      )}
+      {version && version !== LATEST_ABLY_API_VERSION_STRING && (
+        <Warning
+          message={`You are viewing an old version (${version}) of this documentation. We recommend you `}
+          linkText={`view the latest version, ${LATEST_ABLY_API_VERSION_STRING}`}
+          href={`/documentation/${rootVersion}`}
+        />
+      )}
+    </div>
+  );
 };
 
 VersionMenu.propTypes = {
