d587a0d4c84a7fc5f831f2ab7cd038b5d04778af
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-25 15:10:18 +0000

    Warning message if API version is older

---
+++ gatsby-toolchain/src/components/Notifications/Notice.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import AIBarricade from '../../styles/svg/notification-levels/ai-barricade';
+import styled from 'styled-components';
+
+const MESSAGE_LEVEL = Object.freeze({
+  TIP: 'Tip:',
+  NOTICE: 'Notice:',
+  CAUTION: 'Caution:',
+  WARNING: 'Warning:',
+  ERROR: 'Sorry:',
+});
+
+const MESSAGE_ICONS = Object.freeze({
+  [MESSAGE_LEVEL.WARNING]: <AIBarricade role="presentation" />,
+});
+
+const CAlertMessage = styled.div`
+  display: flex;
+  font-size: inherit;
+  margin: 0;
+
+  a {
+    color: white;
+    font-weight: 700;
+    text-decoration: underline;
+    opacity: 0.8;
+    transition: opacity 0.2s;
+
+    &:hover {
+      color: inherit;
+      opacity: 1;
+    }
+  }
+
+  ul {
+    margin-left: 20px;
+    text-align: left;
+    list-style-type: disc;
+
+    li {
+      margin-bottom: 5px;
+    }
+  }
+
+  position: relative;
+  z-index: 2;
+  display: flex;
+  width: 100%;
+  align-content: center;
+`;
+
+const SuggestDestination = ({ message, messageLevel, href, linkText }) => (
+  <>
+    {MESSAGE_ICONS[messageLevel]}
+    <CAlertMessage className="px-4">
+      <span>
+        <strong>{messageLevel}</strong>
+        {message}
+        <strong>
+          <Link to={href}>{linkText}</Link>
+        </strong>
+        .
+      </span>
+    </CAlertMessage>
+  </>
+);
+
+SuggestDestination.propTypes = {
+  message: PropTypes.string,
+  messageLevel: PropTypes.string,
+  href: PropTypes.string,
+  linkText: PropTypes.string,
+};
+
+export { MESSAGE_LEVEL };
+export default SuggestDestination;
---
+++ gatsby-toolchain/src/styles/svg/notification-levels/ai-barricade.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AIBarricade = () => (
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <title>ai-barricade</title>
+    <path d="M23 2h-3v-1c0-0.553-0.447-1-1-1s-1 0.447-1 1v1h-12v-1c0-0.553-0.447-1-1-1s-1 0.447-1 1v1h-3c-0.553 0-1 0.447-1 1v11c0 0.553 0.447 1 1 1h3v9h2v-9h12v9h2v-9h3c0.553 0 1-0.447 1-1v-11c0-0.553-0.447-1-1-1v0zM2 4h3.186l-3.186 3.186v-3.186zM2 8.6l4.6-4.6h2.829l-7.429 7.429v-2.829zM2 13v-0.156l8.844-8.844h2.828l-9 9h-2.672zM6.086 13l9-9h2.828l-9 9h-2.828zM22 13h-3.186l3.186-3.186v3.186zM22 8.4l-4.6 4.6h-2.829l7.429-7.429v2.829zM22 4.157l-8.844 8.843h-2.828l9-9h2.672v0.157z"></path>
+  </svg>
+);
+
+export default AIBarricade;
