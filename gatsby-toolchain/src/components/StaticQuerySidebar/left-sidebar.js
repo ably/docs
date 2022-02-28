edaf2bb745eabc19a566fc5891e611c2699702a7
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-28 10:46:41 +0000

    Overhaul sidebar

--- gatsby-toolchain/src/components/StaticQuerySidebar/left-sidebar.js
+++ gatsby-toolchain/src/components/StaticQuerySidebar/left-sidebar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Sidebar from '../Sidebar';
 import { sidebarDataFromDocumentPaths, sidebarDataFromPageFurniture } from './data';
+import { EXPAND_MENU } from '../Sidebar/consts';
 
 const LeftSideBar = ({ className }) => {
   const data = useStaticQuery(graphql`
@@ -10,16 +11,19 @@ const LeftSideBar = ({ className }) => {
       label
       link
       level
-      contentString
+      text
     }
     query {
       pageFurnitureYaml(name: { eq: "LeftSidebarMenu" }) {
-        contentArray {
+        items {
           ...SubMenuFields
-          contentArray {
+          items {
             ...SubMenuFields
-            contentArray {
+            items {
               ...SubMenuFields
+              items {
+                ...SubMenuFields
+              }
             }
           }
         }
@@ -40,12 +44,12 @@ const LeftSideBar = ({ className }) => {
     }
   `);
   let sidebarData;
-  if (data.pageFurnitureYaml && data.pageFurnitureYaml.contentArray) {
-    sidebarData = sidebarDataFromPageFurniture(data.pageFurnitureYaml.contentArray);
+  if (data.pageFurnitureYaml && data.pageFurnitureYaml.items) {
+    sidebarData = sidebarDataFromPageFurniture(data.pageFurnitureYaml.items);
   } else {
     sidebarData = sidebarDataFromDocumentPaths(data.allDocumentPath.edges);
   }
-  return <Sidebar className={className} data={sidebarData} />;
+  return <Sidebar className={className} data={sidebarData} expandMenu={EXPAND_MENU.COLLAPSE_NEXT} />;
 };
 
 LeftSideBar.propTypes = {
