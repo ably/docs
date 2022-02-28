74d9fcc60c1dacc9d3ad53a89c3d8dbf90c021a6
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-28 10:46:41 +0000

    Overhaul sidebar

--- gatsby-toolchain/src/components/Sidebar/SidebarLinkMenu.js
+++ gatsby-toolchain/src/components/Sidebar/SidebarLinkMenu.js
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Accordion } from 'react-accessible-accordion';
 import styled from 'styled-components';
-import { ROOT_LEVEL } from './consts';
+import { EXPAND_MENU, ROOT_LEVEL } from './consts';
 import SidebarLabel from './SidebarLabel';
 import SidebarLink from './SidebarLink';
 import SidebarLinkItem from './SidebarLinkItem';
@@ -12,7 +12,7 @@ const OrderedList = styled.ol`
   padding: 0;
 `;
 
-const SidebarLinkMenu = ({ data, interactable = false, indent = 0 }) => {
+const SidebarLinkMenu = ({ data, interactable = false, expandMenu = EXPAND_MENU.EXPANDED, indent = 0 }) => {
   const preExpanded = useMemo(() => [], []);
   const linkMenu = useMemo(
     () =>
@@ -29,7 +29,14 @@ const SidebarLinkMenu = ({ data, interactable = false, indent = 0 }) => {
           </SidebarLabel>
         );
 
-        preExpanded.push(uuid);
+        if ([EXPAND_MENU.EXPANDED, EXPAND_MENU.COLLAPSE_NEXT].includes(expandMenu)) {
+          preExpanded.push(uuid);
+        }
+        const nextExpandMenu = EXPAND_MENU.COLLAPSE_NEXT
+          ? EXPAND_MENU.COLLAPSED
+          : EXPAND_MENU.EXPAND_NEXT
+          ? EXPAND_MENU.EXPANDED
+          : expandMenu;
         return content ? (
           <li key={`${label}-${link}-${level}`}>
             <SidebarLinkItem
@@ -39,18 +46,19 @@ const SidebarLinkMenu = ({ data, interactable = false, indent = 0 }) => {
               level={level}
               content={content}
               interactable={interactable}
+              expandMenu={nextExpandMenu}
               indent={indent}
             />
           </li>
         ) : (
           <li key={`${label}-${link}-${level}`}>
-            <SidebarLink to={link} $leaf={indent > 0}>
+            <SidebarLink to={link} $leaf={indent > 0} indent={indent}>
               {label}
             </SidebarLink>
           </li>
         );
       }),
-    [data, interactable, indent, preExpanded],
+    [data, interactable, indent, expandMenu, preExpanded],
   );
   return (
     <Accordion allowMultipleExpanded={true} allowZeroExpanded={true} preExpanded={preExpanded}>
@@ -62,6 +70,7 @@ const SidebarLinkMenu = ({ data, interactable = false, indent = 0 }) => {
 SidebarLinkMenu.propTypes = {
   data: PropTypes.array,
   interactable: PropTypes.bool,
+  expandMenu: PropTypes.oneOf(Object.values(EXPAND_MENU)),
   indent: PropTypes.number,
 };
 
