73ce49ada0fb0f09b73b51dce077da35d8ff5315
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-02-28 10:46:41 +0000

    Overhaul sidebar

--- gatsby-toolchain/src/components/Sidebar/consts.js
+++ gatsby-toolchain/src/components/Sidebar/consts.js
@@ -1,8 +1,17 @@
 const ROOT_LEVEL = 3;
 const MAX_LEVEL = 6;
 
+const EXPAND_MENU = Object.freeze({
+  EXPANDED: true,
+  COLLAPSED: false,
+  SECTION_MATCH: 'SECTION_MATCH',
+  EXPAND_NEXT: 'EXPAND_NEXT',
+  COLLAPSE_NEXT: 'COLLAPSE_NEXT',
+});
+
 // Consts should also be used by gatsby build scripts
 module.exports = {
   ROOT_LEVEL,
   MAX_LEVEL,
+  EXPAND_MENU,
 };
