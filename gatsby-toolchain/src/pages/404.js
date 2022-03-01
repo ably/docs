d84efdaf352c4669c7850ace064434ea4d42cecf
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-03-01 14:40:35 +0000

    Heroku deployment (#11)
    * Heroku staging deployment
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- gatsby-toolchain/src/pages/404.js
+++ gatsby-toolchain/src/pages/404.js
@@ -1,51 +1,34 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { StyledH1 } from '../components/blocks/headings/H1';
+import { StyledGatsbyLink } from '../components/blocks/external-references/A';
 
-// styles
-const pageStyles = {
-  color: '#232129',
-  padding: '96px',
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: '#8A6534',
-  padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
-  borderRadius: 4,
-};
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <main>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{' '}
-        we couldn&apos;t find what you were looking for.
+      <StyledH1>404: Page not found</StyledH1>
+      <p>
+        Sorry - we couldn&apos;t find what you were looking for.
         <br />
         {process.env.NODE_ENV === 'development' ? (
           <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
+            Try creating a new page in:
+            <ul>
+              <li>
+                <code>src/pages/</code>: for specific pages
+              </li>
+              <li>
+                <code>data/textile/nanoc-compatible</code>: for complete textile pages
+              </li>
+              <li>
+                <code>data/textile/partials</code>: for reusable textile sections &endash; ask if there&apos;s a
+                preferred alternative first.
+              </li>
+            </ul>
           </>
         ) : null}
         <br />
-        <Link to="/">Go home</Link>.
+        <StyledGatsbyLink to="/documentation">Return to documentation home page</StyledGatsbyLink>.
       </p>
     </main>
   );
