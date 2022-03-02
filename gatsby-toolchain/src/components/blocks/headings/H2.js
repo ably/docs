7811548a330431701b987ed02dba0f6906cbab84
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-02 12:05:38 +0000

    Copy link IDs from headings etc.

--- gatsby-toolchain/src/components/blocks/headings/H2.js
+++ gatsby-toolchain/src/components/blocks/headings/H2.js
@@ -4,7 +4,7 @@ import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
 const StyledH2 = styled.h2`
   ${fonts.h2};
-  margin: ${spacing.large} 0 ${spacing.medium};
+  margin: ${spacing.medium} 0 ${spacing.medium};
 `;
 
 const H2 = GenericHtmlBlock(StyledH2);
--- gatsby-toolchain/src/components/blocks/headings/H4.js
+++ gatsby-toolchain/src/components/blocks/headings/H4.js
@@ -4,7 +4,7 @@ import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
 const StyledH4 = styled.h4`
   ${fonts.h4};
-  margin: 40px 0 ${spacing.small};
+  margin: ${spacing.small} 0 ${spacing.small};
 `;
 
 const H4 = GenericHtmlBlock(StyledH4);
