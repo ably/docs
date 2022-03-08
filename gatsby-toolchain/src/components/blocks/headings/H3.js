d99b83f0442f67380778974a0122710fcdab281a
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-08 15:47:22 +0000

    Partial workaround for dl div textile markup

--- gatsby-toolchain/src/components/blocks/headings/H3.js
+++ gatsby-toolchain/src/components/blocks/headings/H3.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import { fonts } from '../../../styles';
-import GenericHtmlBlock from '../Html/GenericHtmlBlock';
+import LinkableHtmlBlock from '../Html/LinkableHtmlBlock';
 
 const StyledH3 = styled.h3`
   ${fonts.h3};
   margin: 24px 0 24px;
 `;
 
-const H3 = GenericHtmlBlock(StyledH3);
+const H3 = LinkableHtmlBlock(StyledH3);
 
 export default H3;
