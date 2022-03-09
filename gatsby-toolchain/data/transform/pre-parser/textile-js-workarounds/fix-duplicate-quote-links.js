92e060731061f2b6bd55c66638e961bb1d8d7e0f
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-09 10:53:08 +0000

    Cleaner solution

--- gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/fix-duplicate-quote-links.js
+++ gatsby-toolchain/data/transform/pre-parser/textile-js-workarounds/fix-duplicate-quote-links.js
@@ -1,6 +1,5 @@
-// Duplicate quote links are gathered GREEDILY, not LAZILY by textile-js
-const fixDuplicateQuoteLinks = (content) =>
-  content.replace(/"([^"]+?)":([/#\w-]+)/g, (_match, text, href) => `<a href="${href}">${text}</a>`);
+// Duplicate quote links are gathered GREEDILY, not LAZILY by textile-js when a code section is nested inside quotes.
+const fixDuplicateQuoteLinks = (content) => content.replace(/"(@[^"]*@)"[^:]/g, '$1');
 
 module.exports = {
   fixDuplicateQuoteLinks,
