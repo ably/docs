6e8f3e521e6c2c19ce542877c11ca965d3a4f04b
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-09 17:50:21 +0000

    Bring more styling in line with existing site

--- gatsby-toolchain/data/transform/pre-parser/language/white-space.js
+++ gatsby-toolchain/data/transform/pre-parser/language/white-space.js
@@ -5,18 +5,16 @@
  * that end during a list correctly, which makes sense as it's technically invalid HTML (it should place the
  * final </div> outside the <dl></dl> but it isn't clever enough to manage that)
  */
-const trimWhitespaceBetweenSpans = content => content.replace(/\<\/span\>\s+\<span\slang=["']([^"']+)["']>/g, '</span><span lang="$1">');
+const trimWhitespaceBetweenSpans = (content) =>
+  content.replace(/<\/span>\s+<span\slang=["']([^"']+)["']>/g, '</span><span lang="$1">');
 
-const ensureNewlineBetweenDivs = content => content.replace(/\<\/div\>\s*\<div\slang=["']([^"']+)["']>/g, '</div>\n<div lang="$1">');
+const ensureNewlineBetweenDivs = (content) =>
+  content.replace(/<\/div>\s*<div\slang=["']([^"']+)["']>/g, '</div>\n<div lang="$1">');
 
-const enforceWhiteSpaceLevelsBetweenLanguageElements = content => {
-    return trimWhitespaceBetweenSpans(
-        ensureNewlineBetweenDivs(
-            content
-        )
-    );
+const enforceWhiteSpaceLevelsBetweenLanguageElements = (content) => {
+  return trimWhitespaceBetweenSpans(ensureNewlineBetweenDivs(content));
 };
 
 module.exports = {
-    enforceWhiteSpaceLevelsBetweenLanguageElements
-}
+  enforceWhiteSpaceLevelsBetweenLanguageElements,
+};
