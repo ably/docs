545aafce1c2264420e981788d8c6a96db91da74d
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-09 12:01:17 +0000

    Fix partials rendering

--- gatsby-toolchain/data/transform/retrieve-partials/index.js
+++ gatsby-toolchain/data/transform/retrieve-partials/index.js
@@ -1,23 +1,27 @@
-const DataTypes = require("../../types");
+const DataTypes = require('../../types');
 const { indent, flattenContentOrderedList } = require('../shared-utilities');
 
-const maybeRetrievePartial = graphql => async ({ data, type }) => {
-    if(type !== DataTypes.Partial) {
-        return { data, type };
+const maybeRetrievePartial =
+  (graphql) =>
+  async ({ data, type }) => {
+    if (type !== DataTypes.Partial) {
+      return { data, type };
     }
-    const fileName = (data.match(/<%=\s+partial\s+partial_version\('([^')]*)'\)[^%>]*%>/) ?? [,''])[1];
+    // eslint-disable-next-line no-sparse-arrays
+    const fileName = (data.match(/<%=\s+partial\s+partial_version\('([^')]*)'\)[^%>]*%>/) ?? [, ''])[1];
 
     const attributes = data.split(',');
 
     let attributeObject = {};
-    if(attributes.length > 1) {
-        const attributePairs = attributes.slice(1)
-        .map(attr => {
-            const pairs = attr.match(/\s+(\w+):\s+(\w+)/);
-            return pairs.length === 3 ? [pairs[1],pairs[2]] : null;
+    if (attributes.length > 1) {
+      const attributePairs = attributes
+        .slice(1)
+        .map((attr) => {
+          const pairs = attr.match(/\s+(\w+):\s+(\w+)/);
+          return pairs.length === 3 ? [pairs[1], pairs[2]] : null;
         })
-        .filter(x => !!x);
-        attributeObject = Object.fromEntries(attributePairs);
+        .filter((x) => !!x);
+      attributeObject = Object.fromEntries(attributePairs);
     }
 
     const result = await graphql(`
@@ -29,47 +33,47 @@ const maybeRetrievePartial = graphql => async ({ data, type }) => {
             }
             }
         }
-        `)
+        `);
     const partial = result.data.fileHtmlPartial;
     const retrievePartialFromGraphQL = maybeRetrievePartial(graphql);
     let contentOrderedList = partial && partial.contentOrderedList;
-    if(partial) {
-        contentOrderedList = await Promise.all(contentOrderedList.map(retrievePartialFromGraphQL));
-        contentOrderedList = flattenContentOrderedList(contentOrderedList);
+    if (partial) {
+      contentOrderedList = await Promise.all(contentOrderedList.map(retrievePartialFromGraphQL));
+      contentOrderedList = flattenContentOrderedList(contentOrderedList);
 
-        if(attributeObject['indent']) {
-        if(attributeObject['skip_first_indent']) {
-            contentOrderedList = contentOrderedList.map((content, i) => {
-            if(i === 0) {
-                return {
+      if (attributeObject['indent']) {
+        if (attributeObject['skip_first_indent']) {
+          contentOrderedList = contentOrderedList.map((content, i) => {
+            if (i === 0) {
+              return {
                 ...content,
                 data: indent(
-                    content.data.substring(content.data.indexOf("\n") + 1),
-                    parseInt(attributeObject['indent'])
-                )
-                };
+                  content.data.substring(content.data.indexOf('\n') + 1),
+                  parseInt(attributeObject['indent']),
+                ),
+              };
             }
 
             return {
-                ...content,
-                data: indent(content.data, parseInt(attributeObject['indent']))
-            }
-            });
+              ...content,
+              data: indent(content.data, parseInt(attributeObject['indent'])),
+            };
+          });
         } else {
-            contentOrderedList = contentOrderedList.map(content => ({
+          contentOrderedList = contentOrderedList.map((content) => ({
             ...content,
-            data: indent(content.data, parseInt(attributeObject['indent']))
-            }));
+            data: indent(content.data, parseInt(attributeObject['indent'])),
+          }));
         }
-        }
+      }
     }
 
     return {
-        data: contentOrderedList,
-        type
+      data: contentOrderedList,
+      type,
     };
-}
+  };
 
 module.exports = {
-    maybeRetrievePartial
+  maybeRetrievePartial,
 };
