04236830798b5e44f8ec032ba2e305adaa606224
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-15 11:46:23 +0000

    Update snapshots and tests, improve code rendering

--- gatsby-toolchain/data/transform/pre-parser/language/support-for-github.test.js
+++ gatsby-toolchain/data/transform/pre-parser/language/support-for-github.test.js
@@ -1,4 +1,4 @@
-import { addLanguageSupportForGithubStyleCode, minIndent, stripHeredoc } from "./support-for-github";
+import { addLanguageSupportForGithubStyleCode, minIndent, stripHeredoc } from './support-for-github';
 
 const minIndentExample = `
             One
@@ -11,31 +11,31 @@ const githubCodeExample = `\`\`\`[javascript]
         Two
 
     Three
-\`\`\``
+\`\`\``;
 
 describe('Minimum indentation', () => {
-    it('Should return the minimum indentation level of a string', () => {
-        expect(minIndent(minIndentExample)).toBe(4);
-    });
+  it('Should return the minimum indentation level of a string', () => {
+    expect(minIndent(minIndentExample)).toBe(4);
+  });
 });
 
 describe('Strip minimum indentation', () => {
-    it('Should return the string with reduced indentation levels', () => {
-        expect(stripHeredoc(minIndentExample)).toEqual(`
+  it('Should return the string with reduced indentation levels', () => {
+    expect(stripHeredoc(minIndentExample)).toBe(`
         One
     Two
-Three`)
-    });
+Three`);
+  });
 });
 
 describe('Add language support for GitHub style code', () => {
-    it('Should replace line breaks with {{github_br}} & backticks with bc. blocks', () => {
-        expect(addLanguageSupportForGithubStyleCode(githubCodeExample)).toEqual(`bc[javascript]. 
+  it('Should replace line breaks with {{github_br}} & backticks with bc. blocks', () => {
+    expect(addLanguageSupportForGithubStyleCode(githubCodeExample)).toBe(`bc[javascript]. 
         One
 {{{github_br}}}
     Two
 {{{github_br}}}
 Three
 {{{github_br}}}`);
-    });
+  });
 });
