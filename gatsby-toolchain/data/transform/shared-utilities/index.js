729ca409647f1dcdb8f0cedcf547976384f05e35
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-15 10:04:21 +0000

    Various fixes and tweaks to allow the new toolchain to work in the 'old' repo

--- gatsby-toolchain/data/transform/shared-utilities/index.js
+++ gatsby-toolchain/data/transform/shared-utilities/index.js
@@ -1,18 +1,18 @@
 const indent = (str, indentAmount, spacer = '\t') => {
-    const indent = new Array(indentAmount + 1).join(spacer);
-    const result = str.replace(/^(?=.)/gm, indent);
-    return result;
-}
+  const indent = new Array(indentAmount + 1).join(spacer);
+  const result = str.replace(/^(?=.)/gm, indent);
+  return result;
+};
 
-const flattenContentOrderedList = contentOrderedList => contentOrderedList.reduce((acc, {data, type}) => {
-    if(Array.isArray(data)) {
-        return acc.concat(flattenContentOrderedList(data));
+const flattenContentOrderedList = (contentOrderedList) =>
+  contentOrderedList.reduce((acc, { data, type }) => {
+    if (Array.isArray(data)) {
+      return acc.concat(flattenContentOrderedList(data));
     }
     return acc.concat([{ data, type }]);
   }, []);
 
 module.exports = {
-    indent,
-    flattenContentOrderedList
-}
-  
+  indent,
+  flattenContentOrderedList,
+};
