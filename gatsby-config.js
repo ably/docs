f2e655498e41947f96488943b80de6e77fc78206
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-29 14:02:13 +0100

    Framework for image rendering

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: './src/images/',
+        path: './app/assets/images/',
       },
       __key: 'images',
     },
