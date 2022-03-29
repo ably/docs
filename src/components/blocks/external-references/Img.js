04c3158dc0d0588dea728d57ffa4723c1cdb7625
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-03-29 14:02:13 +0100

    Framework for image rendering

--- src/components/blocks/external-references/Img.js
+++ src/components/blocks/external-references/Img.js
@@ -1,5 +1,15 @@
-import SelfClosingHtmlBlock from '../Html/SelfClosingHtmlBlock';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Img = SelfClosingHtmlBlock('img');
+const Img = ({ attribs }) => {
+    console.log(attribs && attribs.src && attribs.src.replace(/^\/images(.*)/, '$1'));
+    return (
+  <img src={attribs && attribs.src && attribs.src.replace(/^\/images(.*)/, '$1')} {...attribs} />
+);
+    }
+Img.propTypes = {
+  src: PropTypes.string,
+  attribs: PropTypes.object,
+};
 
 export default Img;
