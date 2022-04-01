934c18652fc9bd82554d82d4af86f8e4dad167d7
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-01 15:40:55 +0100

    remove obsolete comment

--- data/transform/index.js
+++ data/transform/index.js
@@ -147,7 +147,6 @@ const transformNanocTextiles =
 
     createInlineToc(inlineTOCOnly, slug, node, { createContentDigest, createNodeId, updateWithTransform });
     const { data, frontmatterMeta } = splitDataAndMetaData(noInlineTOC);
-    // retrieve images here
     const newNodeData = {
       contentOrderedList: data,
       id,
