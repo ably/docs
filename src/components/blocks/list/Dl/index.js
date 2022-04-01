96aeb7c6126cb2788e6aff988e402e79fac2a061
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-01 15:03:57 +0100

    Update table display

--- src/components/blocks/list/Dl/index.js
+++ src/components/blocks/list/Dl/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Html from '../../Html';
 import styled, { css } from 'styled-components';
+import { borders } from '../../../../styles';
 
-const definitionListBorder = `1px solid #e1e1e1`;
 const definitionListCell = css`
-  border-bottom: ${definitionListBorder};
+  border-bottom: ${borders.defaultBorder};
   height: 100%;
   padding: 15px 0;
 `;
 
 const StyledDl = styled.dl`
-  border-top: ${definitionListBorder};
+  border-top: ${borders.defaultBorder};
   display: grid;
   grid-auto-rows: 1fr;
   grid-auto-flow: column;
@@ -22,7 +22,6 @@ const StyledDl = styled.dl`
     ${definitionListCell}
   }
   dd {
-    border-bottom: 1px solid #e1e1e1;
     ${definitionListCell}
   }
 `;
--- src/components/blocks/table/Table.js
+++ src/components/blocks/table/Table.js
@@ -1,5 +1,13 @@
+import styled from 'styled-components';
+import { borders } from '../../../styles';
 import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
-const Table = GenericHtmlBlock('table');
+const StyledTable = styled.table`
+  border: ${borders.defaultBorder};
+  border-collapse: collapse;
+  margin-bottom: 1em;
+`;
+
+const Table = GenericHtmlBlock(StyledTable);
 
 export default Table;
