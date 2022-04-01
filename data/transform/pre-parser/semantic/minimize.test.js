dcb7ab9568c2b228b6b91a7e337b4d49e8f6ab4e
Duncan Edwards <duncan.edwards@ably.com>
2022-04-01 17:59:02 +0100

    Fixed to use details summary block

--- data/transform/pre-parser/semantic/minimize.test.js
+++ data/transform/pre-parser/semantic/minimize.test.js
@@ -5,51 +5,62 @@ const h3Minimize = `h3(#minimizable-title)(minimize=Click to see how to minimize
 Some content
 
 h3. Next title`;
+const h3MinimizeNoTitle = `h3(#minimizable-title)(minimize). Minimize on the title
+
+Some content no title
+
+h3. Next title`;
 const h3MinimizeExpected = `h3(#minimizable-title). Minimize on the title
 
-{{DIV_collapsible-wrapper}}
-<input id='collapsible-heading1' class='minimize-checkbox toggle' type='checkbox'/>
-<label for='collapsible-heading1' class='label-collapsible'>+ View More</label>
-{{DIV_collapsible-content}}
-{{DIV_collapsible-inner}}
-Some content
+<details><summary>Click to see how to minimize by title</summary><div>Some content</div></details>
 
-{{DIV_collapsible-inner_END}}
-<label for='collapsible-heading1' class='label-collapsible-close'>- View Less</label>{{DIV_collapsible-content_END}}{{DIV_collapsible-wrapper_END}}
 
+h3. Next title`;
+const h3MinimizeExpectedViewMore = `h3(#minimizable-title). Minimize on the title
+
+<details><summary>View More</summary><div>Some content no title</div></details>
 
 
 h3. Next title`;
 
 describe('Adds minimize tokens for headings with (minimize)d content', () => {
+
     it('Successfully adds minimize tokens as wrappers for headings in format h3(minimize).', () => {
         const result = addMinimizeForHeadings(h3Minimize);
         expect(result).toEqual(h3MinimizeExpected);
     });
+
+    it('Successfully adds minimize tokens as wrappers for headings in format h3(minimize) with no title.', () => {
+        const result = addMinimizeForHeadings(h3MinimizeNoTitle);
+        expect(result).toEqual(h3MinimizeExpectedViewMore);
+    });
 });
 
 const sectionMinimize = `minimize. Click to see how to minimize by block
-  It is also possible to create a minimizable section with the *minimize. [Optional minimizable text]* line,
-  with any following indented lines being included in the minimizable section. If no optional minimize button
-  text is typed, it will default to *View More*.
-`;
-const sectionMinimizeExpected = `{{DIV_collapsible-wrapper}}
-<input id='collapsible-indent0' class='minimize-checkbox toggle' type='checkbox'><label for='collapsible-indent0' class='label-collapsible'>+ Click to see how to minimize by block</label>{{DIV_collapsible-content}}
-{{DIV_collapsible-inner}}
-It is also possible to create a minimizable section with the *minimize. [Optional minimizable text]* line,
-with any following indented lines being included in the minimizable section. If no optional minimize button
-text is typed, it will default to *View More*.
+  It is also possible to create a minimizable section with the *minimize. [Optional minimizable text]* line, with any following indented lines being included in the minimizable section. If no optional minimize button text is typed, it will default to *View More*.
+
+h3. Next title`;
+const sectionMinimizeNoTitle = `minimize.
+  It is also possible to create a minimizable section with the *minimize. [Optional minimizable text]* line, with any following indented lines being included in the minimizable section. If no optional minimize button text is typed, it will default to *View More*.
+
+h3. Next title`;
+const sectionMinimizeExpected = `<details><summary>Click to see how to minimize by block</summary><div>It is also possible to create a minimizable section with the *minimize. [Optional minimizable text]* line, with any following indented lines being included in the minimizable section. If no optional minimize button text is typed, it will default to *View More*.</div></details>
 
 
-{{DIV_collapsible-inner_END}}
-<label for='collapsible-indent0' class='label-collapsible-close'>- View Less</label>{{DIV_collapsible-content_END}}{{DIV_collapsible-wrapper_END}}
+h3. Next title`;
+const sectionMinimizeExpectedNoTitle = `<details><summary>View More</summary><div>It is also possible to create a minimizable section with the *minimize. [Optional minimizable text]* line, with any following indented lines being included in the minimizable section. If no optional minimize button text is typed, it will default to *View More*.</div></details>
 
-`;
+
+h3. Next title`;
 
 describe('Adds minimize tokens for sections with minimize.d content', () => {
     it('Successfully adds minimize tokens as wrappers for sections marked with minimize. and indentations', () => {
         const result = addMinimizedIndent(sectionMinimize);
         expect(result).toEqual(sectionMinimizeExpected);
+    });
+    it('Successfully adds minimize tokens as wrappers for sections marked with minimize. (with no title) and indentations', () => {
+        const result = addMinimizedIndent(sectionMinimizeNoTitle);
+        expect(result).toEqual(sectionMinimizeExpectedNoTitle);
     });
 })
 
