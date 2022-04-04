40f2fb2bca783c2f001cc3ee8942402f05edb2ed
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-04 12:36:59 +0100

    Example h1 ably ui stylings

--- src/components/blocks/headings/H1.js
+++ src/components/blocks/headings/H1.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
+import React from 'react';
 import { fonts, spacing } from '../../../styles';
 import LinkableHtmlBlock from '../Html/LinkableHtmlBlock';
+import '@ably/ui/core/styles.css';
 
-const StyledH1 = styled.h1`
-  ${fonts.h1}
-  margin: ${spacing.medium} 0 ${spacing.medium};
-`;
+const StyledH1 = ({ props, children }) => <h1 {...props} className='ui-text-h4'>{children}</h1>;
 
 const H1 = LinkableHtmlBlock(StyledH1);
 
