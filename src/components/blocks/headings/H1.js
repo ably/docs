272eb9d1b8dea5f3eff751ea0c1035473e0764ad
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-04 13:01:02 +0100

    Return to h1 styling

--- src/components/blocks/headings/H1.js
+++ src/components/blocks/headings/H1.js
@@ -1,10 +1,13 @@
-import styled from 'styled-components';
 import React from 'react';
-import { fonts, spacing } from '../../../styles';
 import LinkableHtmlBlock from '../Html/LinkableHtmlBlock';
 import '@ably/ui/core/styles.css';
 
-const StyledH1 = ({ props, children }) => <h1 {...props} className='ui-text-h4'>{children}</h1>;
+// eslint-disable-next-line react/prop-types
+const StyledH1 = ({ props, children }) => (
+  <h1 {...props} className="ui-text-h1">
+    {children}
+  </h1>
+);
 
 const H1 = LinkableHtmlBlock(StyledH1);
 
