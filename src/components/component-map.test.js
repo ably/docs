1efe3cfc7430f75c4cfc32dada16e93e6b8587d8
Arti Mathanda <arti.mathanda@ably.com>
2022-04-05 15:13:08 +0200

    feat: Adds headers (h1 to h4) from Ably UI

--- src/components/component-map.test.js
+++ src/components/component-map.test.js
@@ -9,4 +9,16 @@ describe('Component map retrieves values expected', () => {
   it('paragraph', () => {
     expect(componentMap(HtmlDataTypes.p)).not.toBe(IS_TEXT);
   });
+  it('h1', () => {
+    expect(componentMap(HtmlDataTypes.h1)).not.toBe(IS_TEXT);
+  });
+  it('h2', () => {
+    expect(componentMap(HtmlDataTypes.h2)).not.toBe(IS_TEXT);
+  });
+  it('h3', () => {
+    expect(componentMap(HtmlDataTypes.h3)).not.toBe(IS_TEXT);
+  });
+  it('h4', () => {
+    expect(componentMap(HtmlDataTypes.h4)).not.toBe(IS_TEXT);
+  });
 });
