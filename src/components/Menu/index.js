21eddf5e5c152042f63b3d58c86e5e4b81e53b61
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-06 16:06:35 +0100

    Moving closer to how things (maybe) should look

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { ChildPropTypes } from '../../react-utilities';
 
 const HorizontalMenu = ({ children }) => (
-  <menu className="flex p-0 m-0 overflow-y-hidden overflow-x-auto whitespace-nowrap bg-dark-grey">{children}</menu>
+  <menu className="flex px-8 m-0 overflow-y-hidden overflow-x-auto whitespace-nowrap bg-dark-grey">{children}</menu>
 );
 
 HorizontalMenu.propTypes = {
