ed6ed1b833bbd29f1780983ea1ec924294743f1f
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-06 10:05:26 +0100

    Quick fixes for JS warnings

--- src/components/blocks/headings/H2.js
+++ src/components/blocks/headings/H2.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import LinkableHtmlBlock from '../Html/LinkableHtmlBlock';
 import '@ably/ui/core/styles.css';
-import PropTypes from 'prop-types';
+import { ChildPropTypes } from '../../../react-utilities';
 
 const AblyH2 = ({ children }) => <h2 className="ui-text-h2 mb-32">{children}</h2>;
 
 const H2 = LinkableHtmlBlock(AblyH2);
 
 AblyH2.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  children: ChildPropTypes,
 };
 export default H2;
