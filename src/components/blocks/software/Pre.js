dab1d2edbab3618a89bbba3e002552bcb21e6ee8
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-06 09:30:24 +0100

    Moving new code component into codebase

--- src/components/blocks/software/Pre.js
+++ src/components/blocks/software/Pre.js
@@ -2,18 +2,29 @@ import React from 'react';
 import PropType from 'prop-types';
 import Html from '../Html';
 import LocalLanguageAlternatives from '../wrappers/LocalLanguageAlternatives';
+import HtmlDataTypes from '../../../../data/types/html';
+import Code from '@ably/ui/core/Code/component';
+import { languageSyntaxHighlighterNames } from '../../../maps/language';
 
-const Pre = ({ data, language, languages, altData, attribs }) => (
-  <pre className={`mt-32 mb-0`} {...attribs}>
-    {language ? (
-      <LocalLanguageAlternatives language={language} languages={languages} data={altData}>
-        <Html data={data} />
-      </LocalLanguageAlternatives>
-    ) : (
-      <Html data={data} />
-    )}
-  </pre>
-);
+const multilineRegex = /\r|\n/gm;
+
+const Pre = ({ data, language, languages, altData, attribs }) => {
+  const codeData = data.data;
+  const codeAttribs = data.attribs;
+  const isString = codeData && codeData.length === 1 && codeData[0].type === HtmlDataTypes.text;
+  const hasRenderableLanguages = isString && codeAttribs && codeAttribs.lang;
+  const hasMultilineText = isString && multilineRegex.test(codeData[0].data);
+
+  const displayLanguage =
+    codeAttribs && codeAttribs.lang && languageSyntaxHighlighterNames[codeAttribs.lang]
+      ? languageSyntaxHighlighterNames[codeAttribs.lang]
+      : languageSyntaxHighlighterNames['plaintext'];
+  const PreWrappedCodeBlock =
+    hasRenderableLanguages || hasMultilineText ? (
+      <Code language={displayLanguage} snippet={codeData[0].data}></Code>
+    ) : null;
+  return PreWrappedCodeBlock;
+};
 
 Pre.propTypes = {
   data: PropType.array,
