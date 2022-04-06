edfc1cece7c2c8a71abffff98befc39243bc524e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-06 14:42:51 +0100

    partial design refactor

--- src/components/Menu/Label/index.js
+++ src/components/Menu/Label/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
 import { ChildPropTypes } from '../../../react-utilities';
-import { primary } from '../../../styles/colors';
+import '@ably/ui/core/styles.css';
+import '../styles.css';
 
-/**
- * This can be moved to a styled component if wanted, the CSS class approach is retained in keeping
- * with the previous codebase & website
- */
-const MenuLabel = ({ children }) => <li className="c-menu__item c-menu__label">{children}</li>;
+const MenuLabel = ({ children }) => <li className="docs-menu-item">{children}</li>;
 
 MenuLabel.propTypes = {
   children: ChildPropTypes,
 };
 
-const SmallMenuLabel = styled.span`
-  vertical-align: middle;
-  font-weight: 700;
-  font-size: 12px;
-  margin-right: 0;
-  color: ${primary.charcoalGrey};
-`;
+const SmallMenuLabel = ({ children }) => <span className="align-middle ui-text-menu3">{children}</span>;
+
+SmallMenuLabel.propTypes = {
+  children: ChildPropTypes,
+};
 
 export { SmallMenuLabel };
 export default MenuLabel;
