267beacd8a9ab37258d7bc99750ea05328b59180
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-06 10:05:26 +0100

    Quick fixes for JS warnings

--- src/components/blocks/headings/H3.js
+++ src/components/blocks/headings/H3.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import LinkableHtmlBlock from '../Html/LinkableHtmlBlock';
 import '@ably/ui/core/styles.css';
-import PropTypes from 'prop-types';
+import { ChildPropTypes } from '../../../react-utilities';
 
 const AblyH3 = ({ children }) => <h3 className="ui-text-h3 mb-24">{children}</h3>;
 
 const H3 = LinkableHtmlBlock(AblyH3);
 
 AblyH3.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  children: ChildPropTypes,
 };
 export default H3;
