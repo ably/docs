ed6e7156c50988d237036e16f030b5631b39d122
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-06 10:05:26 +0100

    Quick fixes for JS warnings

--- src/components/blocks/headings/H4.js
+++ src/components/blocks/headings/H4.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import LinkableHtmlBlock from '../Html/LinkableHtmlBlock';
 import '@ably/ui/core/styles.css';
-import PropTypes from 'prop-types';
+import { ChildPropTypes } from '../../../react-utilities';
 
 const AblyH4 = ({ children }) => <h4 className="ui-text-h3 mb-24">{children}</h4>;
 
 const H4 = LinkableHtmlBlock(AblyH4);
 
 AblyH4.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  children: ChildPropTypes,
 };
 export default H4;
