b1b05ae12e38c50aa8c608a6570e8fd2bc86b725
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-08 11:07:52 +0100

    Design improvements from discussion with team

--- src/components/Menu/LanguageNavigation.js
+++ src/components/Menu/LanguageNavigation.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { HorizontalMenu as HM } from '.';
+import { HorizontalMenu } from '.';
 import MenuLabel from './Label';
 import MenuItem from './MenuItem';
 import '@ably/ui/core/styles.css';
 
-const HorizontalMenu = ({ label, items }) => (
-  <HM className="docs-code-explorer-language-navigation">
+const LanguageNavigation = ({ label, items }) => (
+  <HorizontalMenu>
     {label && <MenuLabel>{label}</MenuLabel>}
     {items.map(({ Component, props, content }, index) => (
       <MenuItem key={index}>
         <Component {...props}>{content}</Component>
       </MenuItem>
     ))}
-  </HM>
+  </HorizontalMenu>
 );
 
-HorizontalMenu.propTypes = {
+LanguageNavigation.propTypes = {
   label: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
   items: PropTypes.array,
 };
 
-export default HorizontalMenu;
+export default LanguageNavigation;
