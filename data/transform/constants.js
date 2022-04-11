bacdc3e70d2fe569c764e830ad0c25feaa42c914
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-11 12:21:39 +0100

    Fix merge conflict

--- data/transform/constants.js
+++ data/transform/constants.js
@@ -1,18 +1,11 @@
 const LATEST_ABLY_API_VERSION = 1.2;
 const LATEST_ABLY_API_VERSION_STRING = '1.2';
-<<<<<<< HEAD
-const DOCUMENTATION_PATH = '/documentation/';
-=======
 const DOCUMENTATION_NAME = 'docs';
 const DOCUMENTATION_PATH = `/${DOCUMENTATION_NAME}/`;
->>>>>>> 96984f107 (Support new path)
 
 module.exports = {
   LATEST_ABLY_API_VERSION,
   LATEST_ABLY_API_VERSION_STRING,
-<<<<<<< HEAD
-=======
   DOCUMENTATION_NAME,
->>>>>>> 96984f107 (Support new path)
   DOCUMENTATION_PATH,
 };
