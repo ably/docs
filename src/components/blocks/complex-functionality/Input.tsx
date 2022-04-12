c76eb75acfd4cee99cf36f75a5e8c56ffdbaefa4
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-12 17:47:50 +0100

    Re-add  attribs to Input block component

--- src/components/blocks/complex-functionality/Input.tsx
+++ src/components/blocks/complex-functionality/Input.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import SelfClosingHtmlBlock from '../Html/SelfClosingHtmlBlock';
 import './styles.css';
 
-const Input = (props:React.ComponentProps<"input">) => {
-  switch (props.type) {
+const Input = ({ attribs }: { attribs: React.ComponentProps<'input'> }) => {
+  switch (attribs.type) {
     case 'text':
-      return <InputTextField {...props} />;
+      return <InputTextField {...attribs} />;
     default: {
       const Type = SelfClosingHtmlBlock('input');
-      return <Type {...props} />;
+      return <Type {...attribs} />;
     }
   }
 };
 
-const InputTextField = (props:React.ComponentProps<"input">) => (
+const InputTextField = (props: React.ComponentProps<'input'>) => (
   <input
     {...{
       className: 'input-field',
