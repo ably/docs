582d0d71c1a9bbdc7fdde0a427b9f8e026a9f6cd
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-04-14 16:56:52 +0100

    [EDX-91] version menu styling (#1376)
    * Rework react-select menu to remove unneeded features
    * Bring styles in line with design library
    * Improve spacing on warning sign
    * Rebased (#1386)
    
    Co-authored-by: Duncan Edwards <duncan.edwards@ably.com>

--- src/styles/svg/ai-close-rounded.tsx
+++ src/styles/svg/ai-close-rounded.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { AriaRole } from 'react';
 
-const AICloseRounded = () => (
-  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+const AICloseRounded = ({ role }: { role: AriaRole | undefined }) => (
+  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" role={role} viewBox="0 0 24 24">
     <title>ai-close-rounded</title>
     <path d="M14.546 12l8.327-8.327c0.703-0.703 0.703-1.843 0-2.546s-1.843-0.703-2.546 0l-9.6 9.6h2.546l-9.6-9.6c-0.703-0.703-1.843-0.703-2.546 0s-0.703 1.843 0 2.546l8.327 8.327-8.327 8.327c-0.703 0.703-0.703 1.843 0 2.546s1.843 0.703 2.546 0l9.6-9.6h-2.546l9.6 9.6c0.703 0.703 1.843 0.703 2.546 0s0.703-1.843 0-2.546l-8.327-8.327z"></path>
   </svg>
