1c5872bc8171e74c293e11ac1b6ca99a2fe9d56a
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-04-14 16:56:52 +0100

    [EDX-91] version menu styling (#1376)
    * Rework react-select menu to remove unneeded features
    * Bring styles in line with design library
    * Improve spacing on warning sign
    * Rebased (#1386)
    
    Co-authored-by: Duncan Edwards <duncan.edwards@ably.com>

--- src/components/Menu/VersionMenu.js
+++ src/components/Menu/VersionMenu.js
@@ -17,21 +17,69 @@ const VersionMenu = ({ versions, version, rootVersion }) => {
       value: rootVersion,
     },
   ];
-  const options = latestOption.concat(
-    versions.map((v) => ({
-      label: v.node.version,
-      value: v.node.slug,
-    })),
-  );
-  const currentValue = version ? { label: version, value: version } : latestOption;
+  const options = latestOption
+    .concat(
+      versions.map((v) => ({
+        label: v.node.version,
+        value: v.node.slug,
+      })),
+    )
+    .sort((a, b) => parseFloat(b.label) - parseFloat(a.label));
+  const currentValue = version ? { label: version, value: version } : latestOption[0];
 
   return (
-    <div>
+    <>
       {versions.length > 0 && (
-        <>
+        <div className="flex justify-end items-center col-span-1 mb-40">
           <SmallMenuLabel htmlFor={'version-menu'}>API Version:</SmallMenuLabel>
-          <Select inputId={'version-menu'} value={currentValue} options={options} onChange={routeToPage} />
-        </>
+          <Select
+            components={{ IndicatorSeparator: () => null }}
+            classNamePrefix="react-select"
+            styles={{
+              control: (provided) => ({
+                ...provided,
+                width: '100px',
+                border: 0,
+                boxShadow: 'none',
+                fontFamily: `NEXT Book,Arial,Helvetica,sans-serif`,
+                fontSize: '14px',
+              }),
+              option: (provided) => ({
+                ...provided,
+                cursor: 'pointer',
+                width: '100px',
+                border: 0,
+                boxShadow: 'none',
+                fontFamily: `NEXT Book,Arial,Helvetica,sans-serif`,
+                backgroundColor: 'transparent',
+                fontSize: '14px',
+                '&:hover': {
+                  color: 'var(--color-gui-hover)',
+                  backgroundColor: 'white',
+                },
+                '&:focus': {
+                  color: 'var(--color-gui-hover)',
+                  backgroundColor: 'white',
+                },
+                '&:target': {
+                  color: 'var(--color-gui-hover)',
+                  backgroundColor: 'white',
+                },
+              }),
+              dropdownIndicator: (provided) => ({
+                ...provided,
+                color: 'var(--color-cool-black)',
+                '&:hover': {
+                  color: 'var(--color-gui-hover)',
+                },
+              }),
+            }}
+            inputId={'version-menu'}
+            value={currentValue}
+            options={options}
+            onChange={routeToPage}
+          />
+        </div>
       )}
       {version && version !== LATEST_ABLY_API_VERSION_STRING && (
         <Warning
@@ -40,7 +88,7 @@ const VersionMenu = ({ versions, version, rootVersion }) => {
           href={`${DOCUMENTATION_PATH}${rootVersion}`}
         />
       )}
-    </div>
+    </>
   );
 };
 
