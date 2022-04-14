2cb8daf8fa671e7012017aafe3d314812bcf90b4
DuncanEdwards <duncan.edwards@abide-financial.com>
2022-04-14 15:58:59 +0100

    Rebased (#1386)
    Co-authored-by: Duncan Edwards <duncan.edwards@ably.com>

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,11 +1,6 @@
 import React, { FunctionComponent as FC } from 'react';
 import '@ably/ui/core/styles.css';
-import { ChildPropTypes } from '../react-utilities';
 
 const PageTitle: FC = ({ children }) => <h1 className="ui-text-h1 mb-40 col-span-2">{children}</h1>;
-
-PageTitle.propTypes = {
-  children: ChildPropTypes,
-};
 
 export default PageTitle;
