9ef42d500ddaab4cb7ba53f1eba785cd8c6a8aff
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-04-14 17:14:35 +0100

    [EDX-42] GUI colours (#1369)
    * Apply correct GUI colors to link copying success
    
    * Links using newer Ably GUI colors
    
    * Make docs CSS classname prefix consistently pluralised so we can just use one Tailwind greedy purge pattern
    
    * Add typescript definitions for StyledGatsbyLink
    
    * A.tsx typescript types added
    
    * Update Pre.js to use TypeScript also
    
    * Move TS ESlint validation into its own config section
    
    * Remove console.log
    
    * Thinner, happier, more productive text-decoration :+1:
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- src/components/html-component-props.ts
+++ src/components/html-component-props.ts
@@ -1,7 +1,8 @@
 import { Component, FunctionComponent, JSXElementConstructor } from 'react';
 
-export type Attribs<
+export type HtmlComponentProps<
   T extends keyof JSX.IntrinsicElements | JSXElementConstructor<typeof Component | FunctionComponent>,
 > = {
   attribs: React.ComponentProps<T>;
+  data: HtmlComponentProps<any>[];
 };
