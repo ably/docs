ed96804f7ed6e5a64ca99d67e0cb72b423c85239
DuncanEdwards <duncan.edwards@abide-financial.com>
2022-04-14 16:52:50 +0100

    Converted to TS and refactored (#1387)
    Authored-by: Duncan Edwards <duncan.edwards@ably.com>

--- src/components/Button/LanguageButton.tsx
+++ src/components/Button/LanguageButton.tsx
@@ -1,20 +1,15 @@
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { DEFAULT_LANGUAGE } from '../../../data/createPages/constants';
 import PageLanguageContext from '../../contexts/page-language-context';
 import languageLabels from '../../maps/language';
 import { navigate } from 'gatsby';
 import '@ably/ui/core/styles.css';
+import { getLanguageDefaults } from '../common/language-defaults';
 
-const LanguageButton = ({ language }) => {
+const LanguageButton = ({ language }: { language: string }) => {
   const pageLanguage = useContext(PageLanguageContext);
 
-  const languageIsDefault = language === DEFAULT_LANGUAGE;
-  const pageIsDefault = pageLanguage === DEFAULT_LANGUAGE;
-
-  const href = pageIsDefault ? `./?lang=${language}` : `./${languageIsDefault ? '' : `?lang=${language}`}`;
-
-  const isActiveClassName = language === pageLanguage ? 'active' : 'inactive';
+  const { isLanguageDefault, isPageLanguageDefault, isActiveClassName } = getLanguageDefaults(language, pageLanguage);
+  const href = isPageLanguageDefault ? `./?lang=${language}` : `./${isLanguageDefault ? '' : `?lang=${language}`}`;
 
   return (
     // 'active' className doesn’t need to be in the Tailwind config safe list as it isn’t part of the Tailwind ecosystem.
@@ -22,10 +17,6 @@ const LanguageButton = ({ language }) => {
       {languageLabels[language] ?? language}
     </button>
   );
-};
-
-LanguageButton.propTypes = {
-  language: PropTypes.string,
 };
 
 export default LanguageButton;
