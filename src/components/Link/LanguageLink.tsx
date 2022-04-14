12ba56112b9b6240e55e461d4279b7d5df1d2263
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-04-14 17:00:07 +0100

    [EDX-105] - Safelist classNames that should not be purged by Tailwind (#1382)
    * Safelist classNames that should not be purged by Tailwind
    
    also add TypeScript files to the content that Tailwind should consider relevant
    when preserving classNames
    
    * Update comment to be clearer, more accurate
    
    * Converted to TS and refactored (#1387)
    
    Authored-by: Duncan Edwards <duncan.edwards@ably.com>
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
    Co-authored-by: DuncanEdwards <duncan.edwards@abide-financial.com>

--- src/components/Link/LanguageLink.tsx
+++ src/components/Link/LanguageLink.tsx
@@ -1,11 +1,10 @@
 import { Link } from 'gatsby';
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { DEFAULT_LANGUAGE } from '../../../data/createPages/constants';
 import PageLanguageContext from '../../contexts/page-language-context';
 import languageLabels from '../../maps/language';
 import { primary } from '../../styles/colors';
+import { getLanguageDefaults } from '../common/language-defaults';
 
 const HoverLink = styled(Link)`
   cursor: pointer;
@@ -21,25 +20,20 @@ const HoverLink = styled(Link)`
   }
 `;
 
-const LanguageLink = ({ language }) => {
+const LanguageLink = ({ language }: { language: string }) => {
   const pageLanguage = useContext(PageLanguageContext);
 
-  const languageIsDefault = language === DEFAULT_LANGUAGE;
-  const pageIsDefault = pageLanguage === DEFAULT_LANGUAGE;
-
-  const href = pageIsDefault ? `./language/${language}` : `../../${languageIsDefault ? '' : `language/${language}`}`;
-
-  const isActiveClassName = language === pageLanguage ? 'active' : 'inactive';
+  const { isLanguageDefault, isPageLanguageDefault, isActiveClassName } = getLanguageDefaults(language, pageLanguage);
+  const href = isPageLanguageDefault
+    ? `./language/${language}`
+    : `../../${isLanguageDefault ? '' : `language/${language}`}`;
 
   return (
+    // 'active' className doesn’t need to be in the Tailwind config safe list as it isn’t part of the Tailwind ecosystem.
     <HoverLink className={isActiveClassName} to={href}>
       {languageLabels[language] ?? language}
     </HoverLink>
   );
-};
-
-LanguageLink.propTypes = {
-  language: PropTypes.string,
 };
 
 export default LanguageLink;
