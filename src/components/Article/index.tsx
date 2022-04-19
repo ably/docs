5be979a5726de7da7669fb25a5b885a2abc69d40
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-19 15:14:37 +0100

    Pull from main again

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,11 +1,7 @@
 import React, { FunctionComponent as FC } from 'react';
 
 const Article: FC<{ columns: number }> = ({ columns = 3, children }) => (
-<<<<<<< HEAD
   <article className={`col-span-${columns} grid grid-cols-3 mt-12`}>{children}</article>
-=======
-  <article className={`col-span-${columns} grid grid-cols-3`}>{children}</article>
->>>>>>> 5d8c11bc0 ([EDX-91] version menu styling (#1376))
 );
 
 export default Article;
