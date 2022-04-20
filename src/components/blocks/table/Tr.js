36bbfa70fba7bb354ade894daa06dca60283f50d
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-04-20 10:17:04 +0100

    [EDX-102] correct spacing on code text element (#1385)
    * Fix whitespace appearing in table elements to avoid entering quirks mode
    
    also remove dynamic(ish) className in Code.js
    
    * Update Code text spacing
    
    also, add updates necessary to avoid cascading changes to site appearance.
    
    * [EDX-91] version menu styling (#1376)
    
    * Rework react-select menu to remove unneeded features
    * Bring styles in line with design library
    * Improve spacing on warning sign
    * Rebased (#1386)
    
    Co-authored-by: Duncan Edwards <duncan.edwards@ably.com>
    
    * Refactor Sidebar to use TypeScript
    
    Replace EXPAND_MENU const with true TypeScript enum.
    
    * Add TypeScript to RightSidebar
    
    * Pull from main again
    
    * Move prop types inline in sidebar files
    
    * Made title compulsory (#1394)
    
    * Made title compulsory
    
    * Made title optional
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
    Co-authored-by: Duncan Edwards <duncan.edwards@ably.com>
    Co-authored-by: DuncanEdwards <duncan.edwards@abide-financial.com>

--- src/components/blocks/table/Tr.js
+++ src/components/blocks/table/Tr.js
@@ -1,5 +1,16 @@
-import GenericHtmlBlock from '../Html/GenericHtmlBlock';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Html from '../Html';
+import HtmlDataTypes from '../../../../data/types/html';
 
-const Tr = GenericHtmlBlock('tr');
+const Tr = ({ data, attribs }) => (
+  <tr {...attribs}>
+    <Html data={data.filter((item) => item.type === HtmlDataTypes.tag)} />
+  </tr>
+);
+Tr.propTypes = {
+  data: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  attribs: PropTypes.object,
+};
 
 export default Tr;
