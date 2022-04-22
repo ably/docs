9c57f6b90f54799a268247d051349950d9c425ff
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-04-22 10:08:48 +0100

    Fix annoying List display

--- src/components/blocks/list/Li.js
+++ src/components/blocks/list/Li.js
@@ -1,5 +1,12 @@
+import React from 'react';
 import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
-const Li = GenericHtmlBlock('li');
+const StyledLi = ({ children, ...props }) => (
+  <li {...props} className="ui-text-p1 mb-16">
+    {children}
+  </li>
+);
+
+const Li = GenericHtmlBlock(StyledLi);
 
 export default Li;
