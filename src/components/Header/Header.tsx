a0340d30ea70b41f8b88999cb555573e5d1c14aa
April Smith <aprilroseannkwong@gmail.com>
2022-04-26 15:48:48 +0100

    Convert Layout and Header components to Typescript (#1406)
    * Convert Layout JS to Typscript
    
    * Convert Header component to typescript

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import Meganav from '@ably/ui/core/Meganav';
+import TopCodeMenu from '../Menu/TopCodeMenu';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import ablyStack from '@ably/ui/core/images/ably-stack.svg';
+
+const Header = ({ languages }: { languages: Array<string> }) => {
+  return (
+    <>
+      <Meganav
+        paths={{
+          ablyStack,
+        }}
+      />
+      <TopCodeMenu languages={languages} />
+    </>
+  );
+};
+
+export default Header;
