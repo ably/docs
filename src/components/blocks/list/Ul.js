ea7e8197d8153977fcada845c25d5544b616cd39
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-05-25 20:14:48 +0100

    A nice simple fix for showing unordered lists (#1439)
    * A nice simple fix for showing unordered lists
    
    * Ensure font weight comes through nicely
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- src/components/blocks/list/Ul.js
+++ src/components/blocks/list/Ul.js
@@ -1,5 +1,14 @@
+import styled from 'styled-components';
 import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
-const Ul = GenericHtmlBlock('ul');
+// Unset these values inherited from AblyUI/reset.css
+const StyledUl = styled.ul`
+  list-style: unset;
+  margin: unset;
+  padding: unset;
+  padding-left: 1em;
+`;
+
+const Ul = GenericHtmlBlock(StyledUl);
 
 export default Ul;
