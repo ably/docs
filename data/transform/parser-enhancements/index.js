5e3437227550bb82742fd71b06e80c6bddd027a1
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-05-26 14:48:42 +0100

    Rename 'enhancedParse' function to be more descriptive

--- data/transform/parser-enhancements/index.js
+++ data/transform/parser-enhancements/index.js
@@ -2,7 +2,7 @@ const { addHyphenListSupport } = require('../pre-parser/textile-js-workarounds/a
 const { addDates } = require('./add-dates');
 const { addSpecAnchorLinks } = require('./spec-anchor-links');
 
-const enhancedParse = (content, attributes = {}, path = null) => {
+const processAfterFrontmatterExtracted = (content, attributes = {}, path = null) => {
   let result = addSpecAnchorLinks(content, attributes);
   result = addDates(result, attributes);
   result = addHyphenListSupport(result);
@@ -10,5 +10,5 @@ const enhancedParse = (content, attributes = {}, path = null) => {
 };
 
 module.exports = {
-  enhancedParse,
+  processAfterFrontmatterExtracted,
 };
