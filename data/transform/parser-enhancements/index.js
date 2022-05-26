08eae5686b01215f56164daf8c6da822b2571cf6
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-05-26 14:47:29 +0100

    Move hyphen list support to a safer place

--- data/transform/parser-enhancements/index.js
+++ data/transform/parser-enhancements/index.js
@@ -1,9 +1,11 @@
+const { addHyphenListSupport } = require('../pre-parser/textile-js-workarounds/add-hyphen-list-support');
 const { addDates } = require('./add-dates');
 const { addSpecAnchorLinks } = require('./spec-anchor-links');
 
 const enhancedParse = (content, attributes = {}, path = null) => {
   let result = addSpecAnchorLinks(content, attributes);
   result = addDates(result, attributes);
+  result = addHyphenListSupport(result);
   return result;
 };
 
