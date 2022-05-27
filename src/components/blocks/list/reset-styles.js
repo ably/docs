ef4a9b974313991c34e9117f83f3b2c60341e6d5
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-05-27 09:19:49 +0100

    Edx 57 textile js ordered list workaround (#1441)
    * Support implicit ordered lists
    
    * Even out padding on the bottom of lists
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

---
+++ src/components/blocks/list/reset-styles.js
@@ -0,0 +1,11 @@
+const { css } = require('styled-components');
+
+// Unset these values inherited from AblyUI/reset.css
+const resetStyles = css`
+  margin: unset;
+  padding: unset;
+  padding-left: 1em;
+  padding-bottom: 1em;
+`;
+
+export { resetStyles };
