9454600022765ff06c02e8ed246fe9144642cdab
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-05-27 10:25:21 +0100

    Ensure that a data-contains-key attribute appears in the <pre><code> element if it contains an API key

---
+++ data/html-parser/codeblock-api-key-info.js
@@ -0,0 +1,14 @@
+const cheerio = require('cheerio');
+
+const addAPIKeyInfoToCodeBlock = (cheerioNodes) =>
+  cheerioNodes('pre > code').each((_i, elem) => {
+    const loadedCodeElement = cheerioNodes(elem);
+    const content = loadedCodeElement.text();
+    if (content.includes('{{API_KEY}}')) {
+      cheerioNodes(elem).attr('data-contains-key', 'true');
+    }
+  });
+
+module.exports = {
+  addAPIKeyInfoToCodeBlock,
+};
