5e1e1205db0d4d52f8fae6a8d7fb7f50bea2883a
DuncanEdwards <dun_edwards@yahoo.com>
2022-06-15 17:46:41 +0100

    I feel like the script is running - it aint connecting though

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -11,7 +11,7 @@ import {
   WEB_API_KEYS_DATA_ENDPOINT,
   WEB_API_USER_DATA_ENDPOINT,
 } from '../../redux/api-key/constants';
-import { Script } from 'gatsby';
+import Helmet from 'react-helmet';
 
 const Layout: FC<{ languages: Array<string> }> = ({ languages, children }) => {
   const [sessionState, setSessionState] = useState({});
@@ -33,24 +33,38 @@ const Layout: FC<{ languages: Array<string> }> = ({ languages, children }) => {
       <header>
         <Header languages={languages} />
       </header>
-      <Script src="//cdn.ably.io/lib/ably.min-1.js"></Script>
-      <Script id="example-id" strategy="idle">{`function subscribeToCurlRequest(key) {
-  var ably = new Ably.Realtime(key),
-      channelName = '{{RANDOM_CHANNEL_NAME}}';
-  if (channelName === '{{RANDOM_CHANNEL' + '_NAME}}') { channelName = window.randomChannelName; }
-  ably.channels.get(channelName).subscribe('greeting', function(message) {
-    alert('That was easy, a message was just received from the REST API on channel "' + channelName + '".\n\nGreeting => ' + message.data);
-  });
-}
 
-/* API_KEY variable is replaced inline on https://ably.com so
-  {{API_KEY}} will not equal '{{API_' + 'KEY}}'
-  On docs.ably.com, we rely on application.js to call the onApiKeyRetrieved method */
-if ('Xocypw.AryGDdgU9SDnBf6qocYm7kZzzGTTY5fqzwQMiZOGGW1G79LmGk' !== '{{API_' + 'KEY}}') {
-  subscribeToCurlRequest('Xocypw.AryGDdgU9SDnBf6qocYm7kZzzGTTY5fqzwQMiZOGGW1G79LmGk');
-} else {
-  window.onApiKeyRetrieved = subscribeToCurlRequest;
-}`}</Script>
+      <Helmet>
+        <script src="//cdn.ably.io/lib/ably.min-1.js" type="text/javascript" />
+        <script>
+          {`
+            console.log('before');
+
+            function subscribeToCurlRequest(key) {
+              var ably = new Ably.Realtime(key),
+              channelName = '{{RANDOM_CHANNEL_NAME}}';
+              if (channelName === '{{RANDOM_CHANNEL' + '_NAME}}') { 
+                channelName = window.randomChannelName; 
+              }
+
+              ably.channels.get(channelName).subscribe('greeting', function(message) {
+                console.log('channelName', channelName);
+                alert('That was easy, a message was just received from the REST API on channel "'+ channelName + '".Greeting => ' + message.data);
+              });
+            }
+
+            /* API_KEY variable is replaced inline on https://ably.com so {{API_KEY}} will not equal '{{API_' + 'KEY}}' 
+               On docs.ably.com, we rely on application.js to call the onApiKeyRetrieved method */
+            if ('Xocypw.AryGDdgU9SDnBf6qocYm7kZzzGTTY5fqzwQMiZOGGW1G79LmGk' !== '{{API_' + 'KEY}}') {
+              subscribeToCurlRequest('Xocypw.AryGDdgU9SDnBf6qocYm7kZzzGTTY5fqzwQMiZOGGW1G79LmGk');
+            } else {
+              window.onApiKeyRetrieved = subscribeToCurlRequest;
+            }
+
+            console.log('after');
+          `}
+        </script>
+      </Helmet>
       <main className={`${languages && languages.length > 1 ? 'pt-128' : 'pt-96'} grid grid-cols-5`}>{children}</main>
     </UserContext.Provider>
   );
