e25dd5fa295570f6389882bb14256faaae07bebc
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-06-23 13:52:13 +0100

    Fix README, add small test for bold text

--- data/transform/pre-parser/textile-js-workarounds/add-bold-text.test.js
+++ data/transform/pre-parser/textile-js-workarounds/add-bold-text.test.js
@@ -1,7 +1,15 @@
+const { default: fc } = require('fast-check');
 const { addBoldText } = require('./add-bold-text');
+
+const stringWithoutAsterisks = fc.string().map((s) => s.replace('*', ''));
 
 describe('Textile bold text is replaced correctly with a <strong> element', () => {
   it('Wraps textile bold text in a <strong> element', () => {
     expect(addBoldText(`**Type: String**`)).toBe(`<strong>Type: String</strong>`);
+  });
+  it('Wraps any text that has no asterisks in a <strong> element', () => {
+    fc.assert(
+      fc.property(stringWithoutAsterisks, (a) => expect(addBoldText(`**${a}**`)).toBe(`<strong>${a}</strong>`)),
+    );
   });
 });
