f2f0ad8b006c60ac3bcbc2c5b90cd791d9abce0c
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-06-27 15:17:22 +0100

    Resolve TypeSCript errors in testing

--- src/components/Sidebar/check-section-match.test.ts
+++ src/components/Sidebar/check-section-match.test.ts
@@ -1,4 +1,4 @@
-import { Arbitrary, array, assert, constant, letrec, nat, oneof, option, property, record, string } from 'fast-check';
+import { Arbitrary, array, assert, constant, letrec, nat, oneof, property, record, string } from 'fast-check';
 import { checkSectionMatch } from './check-section-match';
 import { SidebarData } from './sidebar-data';
 
@@ -64,36 +64,44 @@ const sidebarDataArbitraryWithMatchingLink: Arbitrary<SidebarData> = record({
   content: constant(undefined),
 });
 
-const sidebarDataArbitraryTreeWithMatchingLink = letrec((tie) => ({
-  node: record({
-    label: string(),
-    link: string().filter((s) => s !== '/docs/match'),
-    level: nat(),
-    content: oneof(
-      { maxDepth: 5 },
-      array(sidebarDataArbitraryWithMatchingLink, { minLength: 1, maxLength: 5 }),
-      array(tie('node'), { minLength: 1, maxLength: 5 }),
-    ),
-  }),
-})).node;
+const sidebarDataArbitraryTreeWithMatchingLink = letrec((tie) => {
+  const sidebarNode = <Arbitrary<SidebarData>>tie('node');
+  return {
+    node: record({
+      label: string(),
+      link: string().filter((s) => s !== '/docs/match'),
+      level: nat(),
+      content: oneof(
+        { maxDepth: 5 },
+        array(sidebarDataArbitraryWithMatchingLink, { minLength: 1, maxLength: 5 }),
+        array(sidebarNode, { minLength: 1, maxLength: 5 }),
+      ),
+    }),
+  };
+}).node;
 
-const sidebarDataArbitraryTreeWithoutMatchingLink = letrec((tie) => ({
-  node: record({
-    label: string(),
-    link: string().filter((s) => s !== '/docs/match'),
-    level: nat(),
-    content: oneof(
-      { depthIdentifier: 'node', maxDepth: 5 },
-      constant(undefined),
-      array(tie('node'), { minLength: 1, maxLength: 3, depthIdentifier: 'node' }),
-    ),
-  }),
-})).node;
+const sidebarDataArbitraryTreeWithoutMatchingLink = letrec<{
+  node: SidebarData;
+}>((tie) => {
+  const sidebarNode = <Arbitrary<SidebarData>>tie('node');
+
+  return {
+    node: record({
+      label: string(),
+      link: string().filter((s) => s !== '/docs/match'),
+      level: nat(),
+      content: oneof(
+        { depthIdentifier: 'node', maxDepth: 5 },
+        constant(undefined),
+        array(sidebarNode, { minLength: 1, maxLength: 3, depthIdentifier: 'node' }),
+      ),
+    }),
+  };
+}).node;
 
 describe('Property test of Check Section Matches function', () => {
   it('Matches when sidebar data contains a link', () => {
     assert(
-      // @ts-ignore
       property(sidebarDataArbitraryTreeWithMatchingLink, (sidebarData: SidebarData) =>
         expect(simpleSectionMatch(sidebarData)).toBe(true),
       ),
@@ -101,7 +109,6 @@ describe('Property test of Check Section Matches function', () => {
   });
   it('Does not match when sidebar data does not contain a link', () => {
     assert(
-      // @ts-ignore
       property(sidebarDataArbitraryTreeWithoutMatchingLink, (sidebarData: SidebarData) =>
         expect(simpleSectionMatch(sidebarData)).toBe(false),
       ),
