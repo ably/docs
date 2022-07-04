6a358b4bfbb01fcbadadd2d9831ab01a2469c78d
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-07-04 12:02:35 +0100

    Final chat tweaks

--- src/third-party/hubspot/components/open-chat-button.tsx
+++ src/third-party/hubspot/components/open-chat-button.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Icon from '@ably/ui/core/Icon';
+import { identity } from 'lodash';
 
 declare global {
   interface Window {
@@ -23,80 +24,64 @@ type OpenChatButtonProps = {
 const ACTIVE = true;
 const INACTIVE = false;
 
-const MAX_RETRY_COUNT = 20;
-
-// const toggleChatWidget = (params = {}) {
-//   const { dataId, withIcon } = params;
-//   const container = queryId(dataId);
-//   const chatButton = queryId('open-chat-widget', container);
-//   const textEnabled = chatButton.dataset.enabledLabel;
-//   const textDisabled = chatButton.dataset.disabledLabel;
-
-//   if (!dataId || !container) return;
-//   const trigger = queryId('open-chat-widget', container);
-
-//   let clickHandler;
-
-//   const waitForScript = (delay) => {
-//     const widget = window?.HubSpotConversations?.widget;
-
-//     // If the chat is set to be hidden out of hours this will return null
-//     const iframe = document.querySelector('#hubspot-messages-iframe-container');
-
-//     clickHandler = (e) => {
-//       e.preventDefault();
-//       widget.open();
-//     };
-
-//     if (widget && iframe) {
-//       trigger.addEventListener('click', clickHandler);
-//       enableBtn(trigger, textEnabled, withIcon);
-//     } else if (--MAX_RETRY_COUNT > 0) {
-//       setTimeout(() => waitForScript(WAIT_BETWEEN_RETRIES_MS), delay);
-//     }
-//   };
-
-//   disableBtn(trigger, textDisabled, withIcon);
-//   waitForScript(0);
-
-//   return () => {
-//     disableBtn(trigger, textDisabled, withIcon);
-//     trigger.removeEventListener('click', clickHandler);
-//   };
-// }
+const safeWindow = (
+  typeof window === 'undefined' ? { HubSpotConversations: undefined, hsConversationsOnReady: [] } : window
+) as Window;
 
 const OpenChatButton = ({ label, labelWhenInactive, options: { withIcon } }: OpenChatButtonProps) => {
   const [status, setStatus] = useState(INACTIVE);
   const [selectedLabel, setSelectedLabel] = useState(labelWhenInactive);
+  const [clickHandler, setClickHandler] = useState(identity);
 
-  const enableChatButton: () => void = () => {
+  const activateChatButton: () => void = () => {
     setStatus(ACTIVE);
     setSelectedLabel(label);
+    setClickHandler((event) => {
+      const widget = safeWindow.HubSpotConversations?.widget;
+      if (widget) {
+        widget.open();
+      } else {
+        console.warn('HubSpot Conversation widget not found.');
+      }
+      return event;
+    });
+  };
+
+  const deActivateChatButton: () => void = () => {
+    setStatus(INACTIVE);
+    setSelectedLabel(labelWhenInactive);
+    setClickHandler(identity);
   };
 
   useEffect(() => {
-    const safeWindow = (
-      typeof window === 'undefined' ? { HubSpotConversations: undefined, hsConversationsOnReady: [] } : window
-    ) as Window;
-
     const widget = safeWindow.HubSpotConversations?.widget;
 
     // If the chat is set to be hidden out of hours this will return null
     const iframe = document.querySelector('#hubspot-messages-iframe-container');
     if (iframe) {
       if (widget) {
-        enableChatButton();
+        activateChatButton();
       } else {
         if (safeWindow.hsConversationsOnReady) {
-          safeWindow.hsConversationsOnReady.push(enableChatButton);
+          safeWindow.hsConversationsOnReady.push(activateChatButton);
         } else {
-          safeWindow.hsConversationsOnReady = [enableChatButton];
+          safeWindow.hsConversationsOnReady = [activateChatButton];
         }
       }
+    } else {
+      deActivateChatButton();
     }
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <button type="button" className="ui-btn-secondary-invert" disabled={status} data-id="open-chat-widget">
+    <button
+      type="button"
+      className="ui-btn-secondary-invert"
+      disabled={status}
+      data-id="open-chat-widget"
+      onClick={clickHandler}
+    >
       {withIcon && <Icon name="icon-gui-live-chat" size="1.5rem" color="ui-btn-icon" />}
       {selectedLabel}
     </button>
