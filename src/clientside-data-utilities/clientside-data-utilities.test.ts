20d90c5c321ea88da13c7454679bf2e24bf3390e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-07-19 15:41:10 +0100

    Move helper functions and constants to top of file

--- src/clientside-data-utilities/clientside-data-utilities.test.ts
+++ src/clientside-data-utilities/clientside-data-utilities.test.ts
@@ -2,6 +2,29 @@
 import { assert, property, lorem, nat } from 'fast-check';
 import { attribsContainClass } from '.';
 
+const arbitrariesForAttribsContainClassTest = [
+  lorem({ maxCount: 1 }), // The class to find
+  lorem(), // The full className
+  nat(2), // Where to place the class within the className
+] as const;
+
+const checkDesiredClassNamePresence =
+  (shouldBePresent: boolean) => (cssClass: string, className: string, position: number) => {
+    const attribs = {
+      className: shouldBePresent ? className : className.replace(cssClass, `#${cssClass}`),
+    };
+    if (position === 0) {
+      attribs.className = `${cssClass}${shouldBePresent ? ' ' : ''}${attribs.className}`;
+    } else if (position === 2) {
+      attribs.className = `${attribs.className}${shouldBePresent ? ' ' : ''}${cssClass}`;
+    } else {
+      attribs.className = `${attribs.className}${shouldBePresent ? ' ' : ''}${cssClass}${shouldBePresent ? ' ' : ''}${
+        attribs.className
+      }`;
+    }
+    expect(attribsContainClass(cssClass, attribs)).toBe(shouldBePresent);
+  };
+
 describe('Ensures that attribs object contains the expected className', () => {
   it('Ensures that tip is present in a className', () => {
     expect(
@@ -14,28 +37,6 @@ describe('Ensures that attribs object contains the expected className', () => {
     expect(attribsContainClass('tip', { className: 'no class present' })).toBe(false);
   });
 
-  const arbitrariesForAttribsContainClassTest = [
-    lorem({ maxCount: 1 }), // The class to find
-    lorem(), // The full className
-    nat(2), // Where to place the class within the className
-  ] as const;
-
-  const checkDesiredClassNamePresence =
-    (shouldBePresent: boolean) => (cssClass: string, className: string, position: number) => {
-      const attribs = {
-        className: shouldBePresent ? className : className.replace(cssClass, `#${cssClass}`),
-      };
-      if (position === 0) {
-        attribs.className = `${cssClass}${shouldBePresent ? ' ' : ''}${attribs.className}`;
-      } else if (position === 2) {
-        attribs.className = `${attribs.className}${shouldBePresent ? ' ' : ''}${cssClass}`;
-      } else {
-        attribs.className = `${attribs.className}${shouldBePresent ? ' ' : ''}${cssClass}${shouldBePresent ? ' ' : ''}${
-          attribs.className
-        }`;
-      }
-      expect(attribsContainClass(cssClass, attribs)).toBe(shouldBePresent);
-    };
   it('Always returns true if a desired className is in any position in a className, separated with spaces', () => {
     assert(property(...arbitrariesForAttribsContainClassTest, checkDesiredClassNamePresence(true)));
   });
