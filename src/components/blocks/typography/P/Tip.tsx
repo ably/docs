2ffdd57285477d824f3b469f0f25162483097ddc
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-07-20 11:35:26 +0100

    [EDX-133] New tip design (#1490)
    * Refactor of paragraph component and move to TypeScript.
    
    Also, add start of how TailwindCSS solution for Tip element will look
    
    * Complete tip element
    
    * Clean up the border radius
    
    * Extract class containment check to new function and add exhaustive tests
    
    Reduce border thickness of tips by 1px
    Fix edge case in add bold text test
    
    * Fix test that can fail
    
    * Remove unused import in data utilities test
    
    * Refactor test
    
    * Move helper functions and constants to top of file
    
    * Resolve conflicts with new-toolchain branch
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

---
+++ src/components/blocks/typography/P/Tip.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { HtmlComponentProps } from '../../../html-component-props';
+import Html from '../../Html';
+import styled from 'styled-components';
+import { colors } from '../../../../styles';
+
+/**
+ * This is required because Tailwind v2 does not offer a good way to style one side of a border.
+ * The closest to what design requires here is:
+ * rounded-lg bg-containers-three ring-2 ring-event-driven-push-transport ring-inset border-l-8 border-event-driven-software p-4
+ bg-event-driven-software w-8 left-0 cursor-default
+ */
+const LeftsideElement = styled.span`
+  width: 8px;
+  border-radius: 0.5em 0 0 0.5em;
+  background-color: #08ff13;
+  left: 0;
+  cursor: default;
+  grid-row-start: 1;
+  grid-row-end: span 2;
+  grid-column-start: 1;
+  grid-column-end: 1;
+  height: 100%;
+`;
+
+const TipTitleElement = styled.strong`
+  margin-left: var(--spacing-16);
+  padding: var(--spacing-16);
+  margin-top: var(--spacing-8);
+  grid-column-start: 1;
+  grid-row-start: 1;
+  text-transform: uppercase;
+  font-size: 14px;
+  letter-spacing: 0.1em;
+  line-height: 18px;
+`;
+
+const InlineContentContainer = styled.div`
+  grid-row: 2;
+  grid-column: 1;
+  margin-left: var(--spacing-16);
+  padding-left: var(--spacing-16);
+  padding-right: var(--spacing-16);
+  padding-bottom: var(--spacing-16);
+  margin-bottom: var(--spacing-8);
+`;
+
+const InlineGridParagraph = styled.p`
+  display: inline-grid;
+  border-radius: 0.5em;
+  background-color: ${colors.containers.three};
+  border-width: 1px;
+  border-color: #d9d9da;
+  grid-template-rows: auto auto;
+  font-size: 16px;
+  line-height: 23px;
+  padding: 0;
+`;
+
+// This needs to be set to a grid in order to render properly
+export const Tip = ({ data, attribs }: HtmlComponentProps<'p'>) => (
+  <InlineGridParagraph className={`${attribs.className}`}>
+    <LeftsideElement>&nbsp;</LeftsideElement>
+    <TipTitleElement>Tip</TipTitleElement>
+    <InlineContentContainer className={`inline`}>
+      <Html data={data} />
+    </InlineContentContainer>
+  </InlineGridParagraph>
+);
