d6799781e8c1c3ed34b54033853c7b80aea44ffd
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-07-20 11:35:26 +0100

    [EDX-133] New tip design (#1490)
    * Refactor of paragraph component and move to TypeScript.
    
    Also, add start of how TailwindCSS solution for Tip element will look
    
    * Complete tip element
    
    * Clean up the border radius
    
    * Extract class containment check to new function and add exhaustive tests
    
    Reduce border thickness of tips by 1px
    Fix edge case in add bold text test
    
    * Fix test that can fail
    
    * Remove unused import in data utilities test
    
    * Refactor test
    
    * Move helper functions and constants to top of file
    
    * Resolve conflicts with new-toolchain branch
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

---
+++ src/components/blocks/typography/P/index.tsx
@@ -0,0 +1,46 @@
+import GenericHtmlBlock from '../../Html/GenericHtmlBlock';
+import '@ably/ui/core/styles.css';
+import { PendingClassName } from './PendingClassName';
+import { HtmlComponentProps } from '../../../html-component-props';
+import { Tip } from './Tip';
+import { attribsContainClass } from '../../../../clientside-data-utilities';
+
+const Paragraph = ({ data, attribs }: HtmlComponentProps<'p'> & { attribs: { ['data-variant']: string } }) => {
+  const isTip = attribsContainClass('tip', attribs);
+  const BaseComponent = isTip ? Tip : GenericHtmlBlock('p');
+  const ParagraphComponent = PendingClassName({ data, attribs }, BaseComponent);
+  switch (attribs['data-variant']) {
+    case 'p1-strong':
+      return ParagraphComponent('ui-text-p1 font-medium mb-32');
+    case 'p2':
+      return ParagraphComponent('ui-text-p2 mb-24');
+    case 'p2-strong':
+      return ParagraphComponent('ui-text-p2 font-medium mb-24');
+    case 'p3':
+      return ParagraphComponent('ui-text-p3 mb-24');
+    case 'p3-strong':
+      return ParagraphComponent('ui-text-p3 font-medium');
+    case 'standfirst':
+      return ParagraphComponent('ui-text-standfirst mb-48 xl:mb-64');
+    case 'title':
+      return ParagraphComponent('ui-text-title mt-64 mb-40 sm:mt-80 sm:mb-40');
+    case 'quote':
+      return ParagraphComponent('ui-text-quote mb-48');
+    case 'subheader':
+      return ParagraphComponent('ui-text-sub-header mb-40');
+    case 'overline1':
+      return ParagraphComponent('ui-text-overline1 mb-8');
+    case 'overline2':
+      return ParagraphComponent('ui-text-overline2 mb-8');
+    case 'menu-label-1':
+      return ParagraphComponent('ui-text-menu1');
+    case 'menu-label-2':
+      return ParagraphComponent('ui-text-menu2');
+    case 'menu-label-3':
+      return ParagraphComponent('ui-text-menu3 ');
+    default:
+      return ParagraphComponent('ui-text-p1 mb-32');
+  }
+};
+
+export default Paragraph;
