829ee66fefb9e7322b78e9590c7049aa2625780b
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-07-21 14:11:34 +0100

    Fix button styling and display when active

--- src/components/Button/LanguageButton.tsx
+++ src/components/Button/LanguageButton.tsx
@@ -2,18 +2,21 @@ import React, { useContext } from 'react';
 import PageLanguageContext from '../../contexts/page-language-context';
 import languageLabels from '../../maps/language';
 import { navigate } from 'gatsby';
-import '@ably/ui/core/styles.css';
+import '../Menu/styles.css';
 import { getLanguageDefaults } from '../common/language-defaults';
 
 const LanguageButton = ({ language }: { language: string }) => {
   const pageLanguage = useContext(PageLanguageContext);
 
-  const { isLanguageDefault, isPageLanguageDefault, isActiveClassName } = getLanguageDefaults(language, pageLanguage);
+  const { isLanguageDefault, isPageLanguageDefault, maybeActiveButtonClassName } = getLanguageDefaults(
+    language,
+    pageLanguage,
+  );
   const href = isPageLanguageDefault ? `./?lang=${language}` : `./${isLanguageDefault ? '' : `?lang=${language}`}`;
 
   return (
     // 'active' className doesn’t need to be in the Tailwind config safe list as it isn’t part of the Tailwind ecosystem.
-    <button className={`docs-menu-item-button ${isActiveClassName}`} onClick={() => navigate(href)}>
+    <button className={maybeActiveButtonClassName} onClick={() => navigate(href)}>
       {languageLabels[language] ?? language}
     </button>
   );
--- src/components/common/language-defaults.ts
+++ src/components/common/language-defaults.ts
@@ -2,13 +2,13 @@ import { DEFAULT_LANGUAGE } from '../../../data/createPages/constants';
 
 const isLanguageDefault = (language: string) => language === DEFAULT_LANGUAGE;
 const getActiveClassName = (language: string, pageLanguage: string) =>
-  language === pageLanguage ? 'active' : 'inactive';
+  language === pageLanguage ? 'docs-menu-item-button-active' : 'docs-menu-item-button';
 
 export const getLanguageDefaults = (
   language: string,
   pageLanguage: string,
-): { isLanguageDefault: boolean; isPageLanguageDefault: boolean; isActiveClassName: string } => ({
+): { isLanguageDefault: boolean; isPageLanguageDefault: boolean; maybeActiveButtonClassName: string } => ({
   isLanguageDefault: isLanguageDefault(language),
   isPageLanguageDefault: isLanguageDefault(pageLanguage),
-  isActiveClassName: getActiveClassName(language, pageLanguage),
+  maybeActiveButtonClassName: getActiveClassName(language, pageLanguage),
 });
