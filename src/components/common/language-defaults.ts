8c999a0e5735f849a829a7171ae950887e738cd0
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-08-02 10:26:57 +0100

    [EDX-127] Sync global and local programming language selector (#1500)
    * Synchronise global and local language state
    
    * Save and retrieve preferredLanguage from localStorage, and redirect user to preferredLanguage version of page
    
    * Fix: display active language button with highlight
    
    * Document: Behaviour of ConditionalChildLanguageDisplay using pseudocode
    
    Misc/Unrelated Fixes
    
    * Fix: test for bold text could occasionally flake
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
    Co-authored-by: DuncanEdwards <dun_edwards@yahoo.com>

--- src/components/common/language-defaults.ts
+++ src/components/common/language-defaults.ts
@@ -1,14 +1,29 @@
 import { DEFAULT_LANGUAGE } from '../../../data/createPages/constants';
 
+export const ACTIVE_LANGUAGE_BUTTON_CLASS_NAME = 'docs-menu-item-button-active';
+export const INACTIVE_LANGUAGE_BUTTON_CLASS_NAME = 'docs-menu-item-button';
+
+type languageButtonClassName = typeof ACTIVE_LANGUAGE_BUTTON_CLASS_NAME | typeof INACTIVE_LANGUAGE_BUTTON_CLASS_NAME;
+
 const isLanguageDefault = (language: string) => language === DEFAULT_LANGUAGE;
 const getActiveClassName = (language: string, pageLanguage: string) =>
-  language === pageLanguage ? 'active' : 'inactive';
+  language === pageLanguage ? ACTIVE_LANGUAGE_BUTTON_CLASS_NAME : INACTIVE_LANGUAGE_BUTTON_CLASS_NAME;
 
 export const getLanguageDefaults = (
   language: string,
   pageLanguage: string,
-): { isLanguageDefault: boolean; isPageLanguageDefault: boolean; isActiveClassName: string } => ({
+): {
+  isLanguageDefault: boolean;
+  isPageLanguageDefault: boolean;
+  maybeActiveButtonClassName: languageButtonClassName;
+} => ({
   isLanguageDefault: isLanguageDefault(language),
   isPageLanguageDefault: isLanguageDefault(pageLanguage),
-  isActiveClassName: getActiveClassName(language, pageLanguage),
+  maybeActiveButtonClassName: getActiveClassName(language, pageLanguage),
 });
+
+export const createLanguageHrefFromDefaults = (
+  isPageLanguageDefault: boolean,
+  isLanguageDefault: boolean,
+  language: string,
+): string => (isPageLanguageDefault ? `./?lang=${language}` : `./${isLanguageDefault ? '' : `?lang=${language}`}`);
--- src/contexts/page-language-context.ts
+++ src/contexts/page-language-context.ts
@@ -3,4 +3,6 @@ import { DEFAULT_LANGUAGE } from '../../data/createPages/constants';
 
 const PageLanguageContext = createContext(DEFAULT_LANGUAGE);
 
+export const PageLanguagesContext = createContext<string[]>([]);
+
 export default PageLanguageContext;
