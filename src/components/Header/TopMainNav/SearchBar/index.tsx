a457758801d36cbaecc37e057eabf565272f9801
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-08-02 15:22:03 +0100

    Add search bar stylings

--- src/components/Header/TopMainNav/SearchBar/index.tsx
+++ src/components/Header/TopMainNav/SearchBar/index.tsx
@@ -1,3 +1,24 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { SearchIcon } from './SearchIcon';
 
-export const SearchPlaceholder = () => <div className="h-32 w-256 mx-16"></div>;
+export const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const textInput = useRef<null | HTMLInputElement>(null);
+  const handleClick = () => textInput.current && textInput.current.focus && textInput.current.focus();
+  return (
+    <div
+      onClick={handleClick}
+      className="h-48 px-16 mx-16 bg-light-grey border border-mid-grey rounded-md flex flex-row justify-self-start self-center"
+    >
+      <SearchIcon className="place-self-center" />
+      <input
+        type="text"
+        ref={textInput}
+        placeholder="Search"
+        className="h-48 w-256 font-light bg-transparent pl-8 text-base outline-none"
+        value={searchTerm}
+        onChange={({ target: { value } }) => setSearchTerm(value)}
+      />
+    </div>
+  );
+};
--- src/components/Header/TopMainNav/index.tsx
+++ src/components/Header/TopMainNav/index.tsx
@@ -4,7 +4,7 @@ import { DropdownButton } from './Dropdown/Button';
 import { dropdownData } from './Dropdown/Button/dropdown-data';
 import { DropdownMenu } from './Dropdown/dropdown-menu';
 import { Logo } from './TopMainNavIllustration/logo';
-import { SearchPlaceholder } from './SearchBar';
+import { SearchBar } from './SearchBar';
 import { DropdownDataIdentifier } from './Dropdown/types';
 import { TopMainNavUserMenu } from './TopMainNavUser';
 import UserContext from '../../../contexts/user-context';
@@ -15,18 +15,18 @@ export const TopMainNav = () => {
   const clearDropdownData = () => setDropdownDataID(null);
   const menuItems: (keyof typeof dropdownData)[] = ['API References', 'Resources'];
   return (
-    <div className="fixed bg-white h-64 z-50 flex ui-grid-px justify-between w-full" onMouseLeave={clearDropdownData}>
+    <div className="fixed bg-white h-64 z-50 flex w-full" onMouseLeave={clearDropdownData}>
       <TopHorizontalMenuLight>
         <Logo href="/docs" />
-        <SearchPlaceholder />
+        <SearchBar />
         {menuItems.map((dropdownDataID, i) => (
           <DropdownButton key={i} title={dropdownDataID} setDropdownData={setDropdownData(dropdownDataID)} />
         ))}
-        {dropdownDataID && <DropdownMenu {...dropdownData[dropdownDataID]} />}
         <UserContext.Consumer>
           {({ sessionState }) => <TopMainNavUserMenu sessionState={sessionState} setDropdownData={setDropdownDataID} />}
         </UserContext.Consumer>
       </TopHorizontalMenuLight>
+      {dropdownDataID && <DropdownMenu {...dropdownData[dropdownDataID]} />}
     </div>
   );
 };
