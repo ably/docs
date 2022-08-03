83a266ea4326cc44d74717c216eb363708a91965
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-08-03 11:31:29 +0100

    Language dropdown selector shell

---
+++ src/components/Menu/LanguageDropdownSelector.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const LanguageDropdownSelector = ({
+  languages,
+  showCodeMenu,
+  showDefaultLink,
+}: {
+  languages: string[];
+  showCodeMenu: boolean;
+  showDefaultLink: boolean;
+}) => <></>;
