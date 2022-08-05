80ba641b40a967828d08a8470b0bf7f90e0913a7
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-08-05 16:59:37 +0100

    Reuse ReactSelectOption type

--- src/components/Menu/route-to-page.ts
+++ src/components/Menu/route-to-page.ts
@@ -1,13 +1,9 @@
 import { navigate } from 'gatsby';
 import { SingleValue } from 'react-select';
 import { DOCUMENTATION_PATH } from '../../../data/transform/constants';
+import { ReactSelectOption } from './react-select-option-types';
 
-export const routeToPage = (
-  newValue: SingleValue<{
-    label: string;
-    value: string;
-  }>,
-) => {
+export const routeToPage = (newValue: SingleValue<ReactSelectOption>) => {
   if (!newValue) {
     console.warn('No option selected from version menu');
     return;
