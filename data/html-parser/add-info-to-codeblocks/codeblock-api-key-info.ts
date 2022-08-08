c7dc718dd941dd07297b09df9bdd89413c7e8228
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-08-08 12:22:00 +0100

    Edx 124 generate random channel names (#1522)
    * Replaces {{RANDOM_CHANNEL_NAME}} tokens with a random channel name; memoized so it's the same per page loaded.
    
    * Also some small refactors to the Code.js block.
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

---
+++ data/html-parser/add-info-to-codeblocks/codeblock-api-key-info.ts
@@ -0,0 +1,5 @@
+import { addInfoToCodeBlock } from './add-info-to-codeblocks';
+
+export const API_KEY_DATA_ATTRIBUTE = 'key';
+
+export const addAPIKeyInfoToCodeBlock = addInfoToCodeBlock('API_KEY', API_KEY_DATA_ATTRIBUTE);
