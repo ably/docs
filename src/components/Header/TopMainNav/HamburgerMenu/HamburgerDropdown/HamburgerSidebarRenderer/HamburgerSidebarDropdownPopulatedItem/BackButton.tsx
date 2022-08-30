5ee5be32e5a7248652313dc4b227b7e1a2f5a24c
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-08-30 14:56:00 +0100

    Improve back button interactivity

--- src/components/Header/TopMainNav/HamburgerMenu/HamburgerDropdown/HamburgerSidebarRenderer/HamburgerSidebarDropdownPopulatedItem/BackButton.tsx
+++ src/components/Header/TopMainNav/HamburgerMenu/HamburgerDropdown/HamburgerSidebarRenderer/HamburgerSidebarDropdownPopulatedItem/BackButton.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 export const BackButton = ({ onClick }: { onClick: React.MouseEventHandler }) => (
   <div
     className="cursor-pointer flex items-center max-w-full mr-24 pb-16 flex-grow justify-between text-16"
+    role="button"
+    tabIndex={0}
     onClick={onClick}
   >
     <span className="text-gui-default align-middle pl-24">
