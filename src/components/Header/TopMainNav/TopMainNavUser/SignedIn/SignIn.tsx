680ebcea5249eb5ddc2d0a27c756783284d504e5
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-08-31 10:53:27 +0100

    [EDX-241] - Styling fixes, refactor (#1531)
    * Styling fixes, pending further feedback
    
    * Rebase cleanups
    
    * Fixed linting error
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
    Co-authored-by: DuncanEdwards <dun_edwards@yahoo.com>

--- src/components/Header/TopMainNav/TopMainNavUser/SignedIn/SignIn.tsx
+++ src/components/Header/TopMainNav/TopMainNavUser/SignedIn/SignIn.tsx
@@ -1,21 +1,26 @@
 import { truncate } from 'lodash/fp';
 import React from 'react';
 import { SessionState } from '../../../../../contexts/user-context';
-import { DropdownButton } from '../../Dropdown/Button';
-import { DropdownDataIdentifier } from '../../Dropdown/types';
+import { DropdownButtonAndMenu } from '../../Dropdown/Button/DropdownButton';
+import { TopMainNavStateContext } from '../../top-main-nav-state-context';
 
-export const SignedIn = ({
-  sessionState,
-  setDropdownData,
-}: {
-  sessionState: SessionState;
-  setDropdownData: React.Dispatch<React.SetStateAction<DropdownDataIdentifier>>;
-}) => {
+export const SignedIn = ({ sessionState }: { sessionState: SessionState }) => {
   const accountName = truncate({ length: 19 }, sessionState.accountName ?? '');
   return sessionState.account && Object.keys(sessionState.account).length !== 0 ? (
-    <menu className="hidden md:flex items-center list-none">
+    <menu className="hidden md:flex items-center list-none pl-0">
       <li className="ui-meganav-item relative">
-        <DropdownButton title={accountName} setDropdownData={() => setDropdownData('Your Account')} />
+        <TopMainNavStateContext.Consumer>
+          {({ topMainNavState, dispatch }) => (
+            <DropdownButtonAndMenu
+              titleOverride={accountName}
+              dropdownDataID={'Your Account'}
+              isOpen={topMainNavState['Your Account'].isOpen}
+              onActivated={(id) => dispatch({ type: 'activate', dropdownId: id })}
+              onMouseOver={(dataId) => dispatch({ type: 'mouse-over', dropdownId: dataId })}
+              onMouseOut={(dataId) => dispatch({ type: 'mouse-out', dropdownId: dataId })}
+            />
+          )}
+        </TopMainNavStateContext.Consumer>
       </li>
       <li className="ml-16">
         <a href={sessionState.account.links.dashboard.href} className="ui-btn-secondary p-btn-small">
--- src/components/Menu/HorizontalMenuItemGroup.tsx
+++ src/components/Menu/HorizontalMenuItemGroup.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-// Utility purely for ensuring it's easy to cluster horizontal menu items together in a consistent way.
-export const HorizontalMenuItemGroup = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-row">{children}</div>
-);
+export const HorizontalMenuItemGroup = ({
+  additionalStyles,
+  children,
+}: {
+  additionalStyles?: string;
+  children: React.ReactNode;
+}) => <div className={`flex flex-row ${additionalStyles ?? ''}`}>{children}</div>;
