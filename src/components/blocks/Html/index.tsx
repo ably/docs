c302b879d1a343ee05d369037fdbc6b03ac8b74a
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-09-06 15:48:25 +0100

    Narrow it down to the two most troublesome tests

--- src/components/blocks/Html/index.tsx
+++ src/components/blocks/Html/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { defaultBlocksFromData } from '../create-blocks/default-blocks-from-data';
+import { apiReferenceBlocksFromData } from '../create-blocks/api-reference-blocks-from-data';
 import { isArray } from 'lodash';
 import ConditionalChildrenLanguageDisplay from '../wrappers/ConditionalChildrenLanguageDisplay';
 import { HtmlComponentProps, ValidReactElement } from 'src/components/html-component-props';
-import { ArticleTypeContext } from 'src/contexts/article-type-context';
+import { ArticleTypeContext } from '../../../contexts/article-type-context';
 import { ARTICLE_TYPES } from '../../../../data/transform/constants';
-import apiReferenceBlocksFromData from '../create-blocks/api-reference-blocks-from-data';
 
 const Html = ({ data }: HtmlComponentProps<ValidReactElement>) =>
   isArray(data) ? (
--- src/components/blocks/create-blocks/api-reference-blocks-from-data.js
+++ src/components/blocks/create-blocks/api-reference-blocks-from-data.js
@@ -3,11 +3,9 @@ import { filterAttribsForReact } from '../../../react-utilities';
 import { apiReferenceComponentMap } from '../block-component-maps/api-reference-component-map';
 import Html from '../Html';
 
-const apiReferenceBlocksFromData = (data) =>
+export const apiReferenceBlocksFromData = (data) =>
   data.map(({ attribs, data, name }, i) => {
     const Block = apiReferenceComponentMap(name) ?? Html;
     const filteredAttribs = filterAttribsForReact(attribs);
     return <Block key={i} attribs={filteredAttribs} data={data} />;
   });
-
-export default apiReferenceBlocksFromData;
