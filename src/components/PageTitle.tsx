cb4b19aa6d3cde7736af47d4d346e16770bda330
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-09-09 14:45:44 +0100

    [EDX-135] Update UI for API references (#1569)
    * Extract createDocuments into a separate function, add TypeScript types
    
    * Render API References using a different document template
    
    * Use a base template component
    
    * Add API Reference page component map
    
    * Add context provider
    
    * Add API reference-specific h6 styling
    
    * Add Blockquote stylings for API references
    
    * Add Code element for API references
    
    * Tweak page title margin-y
    
    * Simple fixes and refactors from feedback:
    
    * languages: languages => languages
    * 3rd party and built-in dependencies first in createPages/index.js
    * fix comment in createPages
    * refactor LATEST_ABLY_API_VERSION_STRING constant
    * rems over px in ApiReferenceH6
    * optional chaining operator usage in base-template.js
    * remove periodicTableOfRealtime references from codebase
    
    * Remove PropTypes, add TypeScript
    
    * Rebase, reconcile TypeScript differences
    
    * Update localStorage
    
    * Extract Head component
    
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,6 +1,10 @@
 import React, { FunctionComponent as FC } from 'react';
 import '@ably/ui/core/styles.css';
 
-const PageTitle: FC = ({ children }) => <h1 className="ui-text-h1 mb-40 col-span-2">{children}</h1>;
+const PageTitle: FC = ({ children }) => (
+  <h1 id="title" className="ui-text-h1 my-40 col-span-2">
+    {children}
+  </h1>
+);
 
 export default PageTitle;
