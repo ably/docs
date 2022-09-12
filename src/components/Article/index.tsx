35dda819826bd497407b93b93d7ddc642bfff984
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-09-12 16:56:33 +0100

    Add ArticleFooter to display help and support options (#1575)

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent as FC } from 'react';
+import { ArticleFooter } from './ArticleFooter';
 
 const Article: FC = ({ children }) => (
   <article className={`col-span-5 grid grid-cols-3 ml-32 mr-32 md:col-span-3 md:ml-0 md:mr-0 md:mt-12`}>
     {children}
+    <ArticleFooter />
   </article>
 );
 
