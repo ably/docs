f2a948141229dd48fba7615b199227d50c2d7cd5
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-09-13 14:43:25 +0100

    [EDX-139] No code snippet exists for lang selected - design/ implementation update (#1578)
    * Add tooltip for when language is unavailable

--- src/components/blocks/software/Pre.tsx
+++ src/components/blocks/software/Pre.tsx
@@ -1,31 +1,58 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useContext } from 'react';
+import cn from 'classnames';
+import Icon from '@ably/ui/core/Icon';
 import Html from '../Html';
+import { PageLanguageContext } from 'src/contexts';
+import languageLabels from 'src/maps/language';
 import LocalLanguageAlternatives from '../wrappers/LocalLanguageAlternatives';
-import '@ably/ui/core/styles.css';
-
+import { DEFAULT_LANGUAGE } from '../../../../data/createPages/constants';
 import { HtmlComponentProps, ValidReactElement } from '../../html-component-props';
 
 type PreProps = HtmlComponentProps<'pre'> & {
   language: string;
-  languages: string[];
+  languages?: string[];
   altData?: Record<string, string | HtmlComponentProps<ValidReactElement>[] | null>;
 };
 
 const Pre = ({ data, languages, altData, attribs }: PreProps): ReactElement => {
+  const pageLanguage = useContext(PageLanguageContext);
+  const hasCode = languages?.some((lang) => lang === pageLanguage) || pageLanguage === DEFAULT_LANGUAGE;
+  const shouldDisplayTip = !hasCode && languages?.length !== undefined;
+
   const withModifiedClassname = {
     ...attribs,
-    className: `bg-cool-black text-white p-0 rounded-lg mb-32 relative`,
+    className: `bg-cool-black text-white p-0 rounded-lg relative`,
   };
+
   return (
-    <pre {...withModifiedClassname}>
-      {languages ? (
-        <LocalLanguageAlternatives languages={languages} data={altData}>
-          <Html data={data} />
-        </LocalLanguageAlternatives>
-      ) : (
-        <Html data={data} />
+    <div
+      className={cn('my-32', {
+        'p-16 bg-light-grey rounded-lg': shouldDisplayTip,
+      })}
+    >
+      {shouldDisplayTip && (
+        <aside className="mb-16 flex justify-between items-start">
+          <div className="mt-2">
+            <Icon name="icon-gui-info" size="1rem" />
+          </div>
+          <div className="ml-8">
+            You’re currently reading the{' '}
+            <span className="font-semibold">{languageLabels[pageLanguage] ?? pageLanguage}</span> docs but we don’t yet
+            have a relevant code sample. You can explore equivalent samples below. To visit the docs for other
+            languages, select the language using the dropdown at the top of the page.
+          </div>
+        </aside>
       )}
-    </pre>
+      <pre {...withModifiedClassname}>
+        {languages ? (
+          <LocalLanguageAlternatives languages={languages} data={altData}>
+            <Html data={data} />
+          </LocalLanguageAlternatives>
+        ) : (
+          <Html data={data} />
+        )}
+      </pre>
+    </div>
   );
 };
 
