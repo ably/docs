b5dcde9330d6b63d78529bda8ed22a06a74d9fed
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-09-16 16:30:04 +0100

    Chore: add typescript components (#1590)
    * Move all block components to TypeScript

--- src/components/html-component-props.ts
+++ src/components/html-component-props.ts
@@ -4,14 +4,18 @@ export type ValidReactElement =
   | keyof JSX.IntrinsicElements
   | JSXElementConstructor<typeof Component | FunctionComponent>;
 
+type ReactAttributes<T extends ValidReactElement> = React.ComponentProps<T> & { [key: `data-${string}`]: string };
+
+export type HtmlAttributes<T extends ValidReactElement> = Omit<ReactAttributes<T>, 'ref'>;
+
 export type NestedHtmlComponentProps<T extends ValidReactElement> = {
-  attribs?: React.ComponentProps<T> & { [key: `data-${string}`]: string };
+  attribs?: HtmlAttributes<T>;
   data: HtmlComponentProps<ValidReactElement>[];
   type?: string;
 };
 
 export type HtmlComponentProps<T extends ValidReactElement> = {
-  attribs?: React.ComponentProps<T> & { [key: `data-${string}`]: string };
+  attribs?: HtmlAttributes<T>;
   data: HtmlComponentProps<ValidReactElement>[] | string | null;
   type?: string;
 };
