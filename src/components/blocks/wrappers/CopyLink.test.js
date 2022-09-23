bfc835485fd7d241345fa853c9d36942b1913137
snikidev <n.kakuev@gmail.com>
2022-09-23 17:39:51 +0100

    wip: refactor CopyLink

--- src/components/blocks/wrappers/CopyLink.test.js
+++ src/components/blocks/wrappers/CopyLink.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import CopyLink, { LinkHoverPopup } from './CopyLink';
+import CopyLink from './CopyLink';
 
 const rootElement = (
   <CopyLink>
@@ -14,16 +14,8 @@ const rootElementWithId = (
   </CopyLink>
 );
 
-const linkHoverElement = <LinkHoverPopup copySuccess={null}>Hover popup</LinkHoverPopup>;
-const linkHoverElementSuccess = <LinkHoverPopup copySuccess={true}>Success</LinkHoverPopup>;
-const linkHoverElementFailure = <LinkHoverPopup copySuccess={false}>Failure</LinkHoverPopup>;
-
 const copyLinkRenderer = TestRenderer.create(rootElement);
 const copyLinkIdRenderer = TestRenderer.create(rootElementWithId);
-
-const linkHoverRenderer = TestRenderer.create(linkHoverElement);
-const linkHoverSuccessRenderer = TestRenderer.create(linkHoverElementSuccess);
-const linkHoverFailureRenderer = TestRenderer.create(linkHoverElementFailure);
 
 describe('CopyLink displays statically as expected', () => {
   it('Snapshot of CopyLink is the same', () => {
@@ -31,17 +23,5 @@ describe('CopyLink displays statically as expected', () => {
   });
   it('Snapshot of CopyLink with ID is the same', () => {
     expect(copyLinkIdRenderer.toJSON()).toMatchSnapshot();
-  });
-});
-
-describe('LinkHoverPopup displays statically as expected', () => {
-  it('Snapshot of LinkHoverPopup is the same', () => {
-    expect(linkHoverRenderer.toJSON()).toMatchSnapshot();
-  });
-  it('Snapshot of LinkHoverPopup with Success is the same', () => {
-    expect(linkHoverSuccessRenderer.toJSON()).toMatchSnapshot();
-  });
-  it('Snapshot of LinkHoverPopup with Failure is the same', () => {
-    expect(linkHoverFailureRenderer.toJSON()).toMatchSnapshot();
   });
 });
