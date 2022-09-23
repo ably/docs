8e7c80dad8552cfa9148747009ecf86d7311cf08
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-09-23 15:30:25 +0100

    Fix failing test

--- data/transform/pre-parser/erbjs/url-for.test.js
+++ data/transform/pre-parser/erbjs/url-for.test.js
@@ -3,7 +3,7 @@ import { urlforReplacer } from './url-for';
 describe('Replaces JSBin urlfor ERB strings with links', () => {
   it('Replaces a real example of a JSBin urlfor ERB string', () => {
     const sampleText = `bc[text]. "(external) Example":<%= JsBins.url_for('client-lib-development-guide/example') %>`;
-    const expectedResult = `bc[text]. "(external) Example":https://jsbin.ably.com:443/ohidar/latest_?javascript,live`;
+    const expectedResult = `bc[text]. "(external) Example":https://jsbin.ably.com:443/ekedis/latest_?javascript,live`;
     expect(urlforReplacer(sampleText)).toBe(expectedResult);
   });
 });
