e5d069c7c39a5b04f76ea4f46ff5586c1cbb0376
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-09-23 14:57:13 +0100

    Remove unneded PropTypes

--- src/components/blocks/dividers/Div.tsx
+++ src/components/blocks/dividers/Div.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
 import { matchesLanguageOrDefault } from '../wrappers/language-utilities';
 import { PageLanguageContext } from 'src/contexts';
 import Html from '../Html';
