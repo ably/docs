bcb22429339740a1bee7005d80e187346e92c472
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-09-23 12:08:36 +0100

    [EDX-255] Search results UI/UX (#1593)
    * Add SuggestionBox component
    
    * wip: make SearchBar tests work
    
    * Fix SearchBar tests
    
    * Remove use-keyboard-shortcut hook
    
    * Fix SearchBar test
    
    * Improve proprs for SearchDisplay

---
+++ src/components/Header/TopMainNav/SearchBar/SuggestionBox.tsx
@@ -0,0 +1,59 @@
+import React, { useState, MouseEvent } from 'react';
+import { HitType } from 'src/hooks';
+import htmr from 'htmr';
+
+import { container } from './SuggestionBox.module.css';
+
+type Props = {
+  results: HitType[] | null;
+  isActive: boolean;
+};
+
+export const SuggestionBox = ({ results, isActive }: Props) => {
+  const [capture, setCapture] = useState<{ link: string; title: string } | null>(null);
+
+  const handleOptionHover = (event: MouseEvent<HTMLAnchorElement>) => {
+    const link = event.currentTarget.getAttribute('data-capture') as string;
+    const title = event.currentTarget.getAttribute('data-title') as string;
+    if (capture?.link === link) {
+      return;
+    }
+    setCapture({ link, title });
+  };
+
+  const handleOptionHoverOut = () => {
+    setCapture(null);
+  };
+
+  if (!isActive || !results || results.length === 0) {
+    return null;
+  }
+
+  return (
+    <div aria-label="suggestions" className={container}>
+      <div className={`col-span-6 border-r border-mid-grey pr-16 py-16`}>
+        <div className="font-light text-dark-grey uppercase text-menu3 px-16">Results from docs</div>
+        {results &&
+          results.map(({ title, highlight, url, id, images: { capture } }) => (
+            <a
+              key={id}
+              href={url}
+              className="block p-16 hover:bg-light-grey rounded-lg"
+              data-capture={capture}
+              data-title={title}
+              onMouseOver={handleOptionHover}
+              onMouseLeave={handleOptionHoverOut}
+              role="link"
+            >
+              <h4 className="text-menu2 mb-6 font-medium">{title}</h4>
+              <div className="text-menu3 font-light text-charcoal-grey">{htmr(highlight)}</div>
+              {url && <div className="text-dark-grey font-light text-menu3 mt-8">{new URL(url).hostname}</div>}
+            </a>
+          ))}
+      </div>
+      <div className="col-span-4 relative py-16 pl-16">
+        {capture && <img className="sticky top-16" src={capture.link} alt={capture.title} />}
+      </div>
+    </div>
+  );
+};
