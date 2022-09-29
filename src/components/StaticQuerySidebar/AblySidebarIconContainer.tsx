1f7613e50a0d5a3889a4323ae7969803d35f8990
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-09-29 11:25:14 +0100

    Fix css bug (#1601)

--- src/components/StaticQuerySidebar/AblySidebarIconContainer.tsx
+++ src/components/StaticQuerySidebar/AblySidebarIconContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { AblySidebarIcon } from './AblySidebarIcon';
 
 export const AblySidebarIconContainer = () => (
-  <div className="fixed bottom-0 bg-extra-light-grey w-1/5 block pl-40 pt-24 pb-28 h-64 border-t border-mid-grey">
+  <div className="absolute bottom-0 left-0 bg-extra-light-grey w-full block pl-40 pt-24 pb-28 h-64 border-t border-mid-grey">
     <a href="/">
       <AblySidebarIcon />
     </a>
