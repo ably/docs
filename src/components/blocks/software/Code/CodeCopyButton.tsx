ab867a75088f87a4bec989de1d0fb3d482c7230b
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-10-24 10:42:52 +0100

    [EDX-138] GTM UI interaction metrics (#1633)
    * Add Google Tag Manager interaction events

--- src/components/blocks/software/Code/CodeCopyButton.tsx
+++ src/components/blocks/software/Code/CodeCopyButton.tsx
@@ -2,14 +2,23 @@ import React, { FunctionComponent as FC } from 'react';
 import Icon from '@ably/ui/core/Icon';
 import { ButtonWithTooltip } from 'src/components';
 import '@ably/ui/core/styles.css';
+import { safeWindow } from 'src/utilities';
+import { copyCodeBlockContentTracker } from 'src/third-party/gtm/gtm';
+import { DEFAULT_LANGUAGE } from '../../../../../data/createPages/constants';
 
 type Props = {
   content: string;
+  language?: string;
 };
 
-const CodeCopyButton: FC<Props> = ({ content }) => {
+const CodeCopyButton: FC<Props> = ({ content, language }) => {
   const handleCopy = () => {
     navigator.clipboard.writeText(content);
+    const page = safeWindow.location.pathname;
+    const contentIdentifier = content.slice(0, 10);
+    copyCodeBlockContentTracker({
+      copyCodeBlockContent: `docs - ${language ?? DEFAULT_LANGUAGE} ${page} ${contentIdentifier}`,
+    });
   };
 
   return (
