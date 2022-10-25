520a9e14fcfad0a2ccf80a7869405aec55c32d71
snikidev <n.kakuev@gmail.com>
2022-10-25 17:44:36 +0100

    Revert "Add HTML processor for blockquote"
    This reverts commit 97fb873d1b95e4906d0f62b35d07957a777892d8.

--- src/components/blocks/external-references/Blockquote.tsx
+++ src/components/blocks/external-references/Blockquote.tsx
@@ -1,13 +1,5 @@
-import React from 'react';
-import { HtmlComponentProps } from 'src/components/html-component-props';
-import Html from 'src/components/blocks/Html';
+import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
-const Blockquote = ({ data, attribs }: HtmlComponentProps<'blockquote'>) => {
-  return (
-    <blockquote {...attribs}>
-      <Html data={data} />
-    </blockquote>
-  );
-};
+const Blockquote = GenericHtmlBlock('blockquote');
 
 export default Blockquote;
