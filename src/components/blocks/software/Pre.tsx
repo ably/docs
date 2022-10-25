7c094ce4c53f25f1571a741c50a58fe915085cb7
april <aprilroseannkwong@gmail.com>
2022-10-25 15:41:40 +0100

    Preformatted text without a <code> element child should render the same as an inline code element

--- src/components/blocks/software/Pre.tsx
+++ src/components/blocks/software/Pre.tsx
@@ -3,10 +3,13 @@ import cn from 'classnames';
 import Icon from '@ably/ui/core/Icon';
 import Html from '../Html';
 import { PageLanguageContext } from 'src/contexts';
-import languageLabels from 'src/maps/language';
+import languageLabels, { languageSyntaxHighlighterNames } from 'src/maps/language';
 import LocalLanguageAlternatives from '../wrappers/LocalLanguageAlternatives';
 import { DEFAULT_LANGUAGE } from '../../../../data/createPages/constants';
 import { HtmlComponentProps, ValidReactElement } from '../../html-component-props';
+import HtmlDataTypes from '../../../../data/types/html';
+import { isString, every, reduce } from 'lodash/fp';
+import { MultilineCodeContent } from './Code/MultilineCodeContent';
 
 type PreProps = HtmlComponentProps<'pre'> & {
   language: string;
@@ -18,11 +21,30 @@ const Pre = ({ data, languages, altData, attribs }: PreProps): ReactElement => {
   const pageLanguage = useContext(PageLanguageContext);
   const hasCode = languages?.some((lang) => lang === pageLanguage) || pageLanguage === DEFAULT_LANGUAGE;
   const shouldDisplayTip = !hasCode && languages?.length !== undefined;
-
   const withModifiedClassname = {
     ...attribs,
     className: `bg-cool-black text-white p-0 rounded-lg relative`,
   };
+
+  const dataTreatedAsCode = data && !isString(data) && every((element) => element.type === HtmlDataTypes.text, data);
+  if (dataTreatedAsCode) {
+    // We know that the first child's data is a string because we've confirmed the element type in dataTreatedAsCode
+    const stringToRender = reduce((acc, curr) => acc.concat((curr.data as string) ?? ''), '', data);
+
+    return (
+      <pre {...attribs} className="bg-cool-black text-white p-0 rounded-lg relative overflow-hidden">
+        <div className="overflow-auto relative p-16">
+          <MultilineCodeContent
+            dataContainsKey={false}
+            contentWithObfuscatedKey={stringToRender}
+            contentWithKey={stringToRender}
+            content={stringToRender}
+            displayLanguage={'plaintext'}
+          />
+        </div>
+      </pre>
+    );
+  }
   return (
     <div
       className={cn('my-32', {
