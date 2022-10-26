c0c8a335f014c363b13b0ba9a00ea1303f294df1
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-10-26 14:49:06 +0100

    [EDX-406] Partial parsing broken (#1652)
    * Add HTML processor for blockquote
    
    * Revert "Add HTML processor for blockquote"
    
    This reverts commit 2363c1a20075f25e385aa2384b9749b5df590f52.
    
    * Add fix for partial parsing
    
    * Revert some changes designed for another ticket

--- src/components/blocks/api-reference/headings/ApiReferenceH6.tsx
+++ src/components/blocks/api-reference/headings/ApiReferenceH6.tsx
@@ -3,10 +3,10 @@ import GenericHtmlBlock from '../../Html/GenericHtmlBlock';
 
 const StyledApiReferenceH6 = ({ attribs, children }: { attribs: ComponentProps<'h6'>; children: ReactNode }) => (
   <h6
-    {...{ ...attribs }}
-    className={`${attribs?.className ?? ''} font-mono
-    items-center w-min pb-4 px-8 mt-10 mb-24
-    rounded-sm bg-api-reference-attribute-highlight border border-api-reference-attribute-border`}
+    {...attribs}
+    className={`font-mono items-center w-min py-4 px-8 mt-10 mb-24 rounded-sm bg-api-reference-attribute-highlight border border-api-reference-attribute-border ${
+      attribs?.className ?? ''
+    } `}
   >
     {children}
   </h6>
