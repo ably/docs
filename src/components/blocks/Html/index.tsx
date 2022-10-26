1b10ced51c3765e401f3249682480a750056edd0
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-10-26 11:05:25 +0100

    [EDX-377] <div> elements aren't always being process correctly (inside of definition lists) (#1646)
    * Fix DL parsing issue
    
    * Add HTML processor for blockquote
    
    * Fixed test
    
    * Revert "Add HTML processor for blockquote"
    
    This reverts commit 97fb873d1b95e4906d0f62b35d07957a777892d8.

--- src/components/blocks/Html/index.tsx
+++ src/components/blocks/Html/index.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { defaultBlocksFromData } from '../create-blocks/default-blocks-from-data';
-import { apiReferenceBlocksFromData } from '../create-blocks/api-reference-blocks-from-data';
+import React, { FunctionComponent } from 'react';
 import { isArray } from 'lodash';
-import ConditionalChildrenLanguageDisplay from '../wrappers/ConditionalChildrenLanguageDisplay';
 import { HtmlComponentProps, ValidReactElement } from 'src/components/html-component-props';
 import { ArticleTypeContext } from '../../../contexts/article-type-context';
 import { ARTICLE_TYPES } from '../../../../data/transform/constants';
+import { defaultBlocksFromData } from '../create-blocks/default-blocks-from-data';
+import { apiReferenceBlocksFromData } from '../create-blocks/api-reference-blocks-from-data';
+import ConditionalChildrenLanguageDisplay from '../wrappers/ConditionalChildrenLanguageDisplay';
 
-const Html = ({ data }: { data: HtmlComponentProps<ValidReactElement>[] | string | null }) =>
-  isArray(data) ? (
-    <ArticleTypeContext.Consumer>
-      {(articleType) => {
-        const blockCreator =
-          articleType === ARTICLE_TYPES.apiReference ? apiReferenceBlocksFromData : defaultBlocksFromData;
-        return <ConditionalChildrenLanguageDisplay>{blockCreator(data)}</ConditionalChildrenLanguageDisplay>;
-      }}
-    </ArticleTypeContext.Consumer>
-  ) : (
-    <>{data}</>
-  );
+const Html = ({
+  data,
+  BlockWrapper = ConditionalChildrenLanguageDisplay as FunctionComponent,
+}: {
+  data: HtmlComponentProps<ValidReactElement>[] | string | null;
+  BlockWrapper?: React.FunctionComponent;
+}) => {
+  if (isArray(data)) {
+    return (
+      <ArticleTypeContext.Consumer>
+        {(articleType) => {
+          const blockCreator =
+            articleType === ARTICLE_TYPES.apiReference ? apiReferenceBlocksFromData : defaultBlocksFromData;
+          return <BlockWrapper>{blockCreator(data)}</BlockWrapper>;
+        }}
+      </ArticleTypeContext.Consumer>
+    );
+  }
+
+  return <>{data}</>;
+};
 
 export default Html;
---
+++ src/components/blocks/list/Dl/DlWrapper.tsx
@@ -0,0 +1,22 @@
+import React, { Children, FunctionComponent, useContext } from 'react';
+import PageLanguageContext from '../../../../contexts/page-language-context';
+import { DEFAULT_LANGUAGE, DEFAULT_PREFERRED_LANGUAGE } from '../../../../../data/createPages/constants';
+
+export const DlWrapper: FunctionComponent<any> = ({ children }) => {
+  const language = useContext(PageLanguageContext);
+
+  return Children.map(children, (child) => {
+    const { attribs } = child?.props;
+
+    if (attribs?.lang) {
+      if (
+        (language === DEFAULT_LANGUAGE && attribs?.lang === DEFAULT_PREFERRED_LANGUAGE) ||
+        attribs?.lang === language
+      ) {
+        return child;
+      }
+      return null;
+    }
+    return child;
+  });
+};
