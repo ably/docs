30b4f291d656a71f5f36956be165e789f3ca67ec
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-10-26 11:05:25 +0100

    [EDX-377] <div> elements aren't always being process correctly (inside of definition lists) (#1646)
    * Fix DL parsing issue
    
    * Add HTML processor for blockquote
    
    * Fixed test
    
    * Revert "Add HTML processor for blockquote"
    
    This reverts commit 97fb873d1b95e4906d0f62b35d07957a777892d8.

---
+++ src/components/blocks/Html/Html.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { DlWrapper } from 'src/components/blocks/list/Dl/DlWrapper';
+import { PageLanguageContext } from 'src/contexts';
+
+import Html from './';
+import { dtFixture } from './fixtures';
+
+describe('<Html />', () => {
+  it('renders correct Dl based on PageLanguageContext value', () => {
+    const { container } = render(
+      <PageLanguageContext.Provider value="python">
+        <Html data={dtFixture} BlockWrapper={DlWrapper} />
+      </PageLanguageContext.Provider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
