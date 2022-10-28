b94fa63dd18e1d91fe626d34d00648b00a0e89bc
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-10-28 12:01:54 +0100

    [EDX-385 code sample local language select (#1661)
    * Whenever we don't have a language to show, the language should be selectable locally; not globally. Also, the select button should work when selecting languages locally.

--- src/components/Menu/MenuItemButton/MenuItemButton.tsx
+++ src/components/Menu/MenuItemButton/MenuItemButton.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent as FC, HTMLAttributes } from 'react';
 import cn from 'classnames';
-import { button, selected } from './MenuItemButton.module.css';
+import { button, isActive } from './MenuItemButton.module.css';
 
 type Props = {
   isSelected?: boolean;
@@ -9,7 +9,7 @@ type Props = {
 const MenuItemButton: FC<Props> = ({ isSelected = false, ...props }) => (
   <button
     className={cn(button, {
-      [selected]: isSelected,
+      [isActive]: isSelected,
     })}
     {...props}
   />
