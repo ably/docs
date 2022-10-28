af9e2d773eba978f10aeb77a96b69ff2a74cab98
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-10-28 16:22:17 +0100

    Make inline code element look the same everywhere (#1663)

--- src/components/blocks/software/Code/InlineCodeElement.tsx
+++ src/components/blocks/software/Code/InlineCodeElement.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const InlineCodeElement = ({ children, ...props }: { children: React.ReactNode }) => (
-  <code {...props} className="font-mono font-semibold text-code p-4">
+  <code
+    {...props}
+    className="font-mono p-2 text-code text-charcoal-grey font-thin bg-light-grey border border-mid-grey rounded-md"
+  >
     {children}
   </code>
 );
