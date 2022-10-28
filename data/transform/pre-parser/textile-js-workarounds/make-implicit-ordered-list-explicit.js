860cb161ce97e5be9c1be509df89ec8eec262b5e
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-10-28 16:20:51 +0100

    Fix link parsing issue (#1669)

--- data/transform/pre-parser/textile-js-workarounds/make-implicit-ordered-list-explicit.js
+++
@@ -1,7 +0,0 @@
-// Not supporting implicit OLs at different indentation levels because the
-// chance of causing unexpected behaviour is too high.
-const makeImplicitOrderedListExplicit = (content) => content.replace(/^\d+\.(.+)$/gm, '# $1');
-
-module.exports = {
-  makeImplicitOrderedListExplicit,
-};
