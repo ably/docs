bdde90f40d5507668faeccfa601478a46355b7fd
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-10-28 09:35:20 +0100

    Never render the component label server side to avoid double renders (#1664)
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- src/components/Menu/ReactSelectCustomComponents/Formatters/FormatOptionLabelWithLanguageLogo.tsx
+++ src/components/Menu/ReactSelectCustomComponents/Formatters/FormatOptionLabelWithLanguageLogo.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { ReactSelectOption } from 'src/components';
 import { imageMap, isInImageMap } from '../../LanguageDropdownSelector/image-map';
 
-const returnNullIfNoLanguageAvailable = (languageOption: ReactSelectOption) =>
-  isInImageMap(languageOption.value) ? imageMap[languageOption.value] : () => null;
+const returnNullIfNoLanguageAvailable = (languageOptionValue: string) =>
+  isInImageMap(languageOptionValue) && typeof window !== 'undefined' ? imageMap[languageOptionValue] : () => null;
 
 export const FormatOptionLabelWithLanguageLogo = (languageOption: ReactSelectOption) => {
-  const Component = returnNullIfNoLanguageAvailable(languageOption);
+  const Component = returnNullIfNoLanguageAvailable(languageOption.value);
   return (
     <div className="language-option items-center flex gap-8">
       <Component />
