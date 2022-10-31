ef72a40c8e451f843ac2189f907d0b296760f024
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-10-31 09:08:24 +0000

    Remove duplicate CSS imports (#1670)

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent as FC } from 'react';
-import '@ably/ui/core/styles.css';
 
 const PageTitle: FC = ({ children }) => (
   <h1 id="title" className="ui-text-h1 my-40 col-span-2">
