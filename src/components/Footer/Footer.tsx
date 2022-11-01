204beac533d96743f55f4a8af9bcc873cc909e7e
april <aprilroseannkwong@gmail.com>
2022-11-01 14:32:15 +0000

    Refactor Footer into smaller components
    Refactor Footer into smaller components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { FooterMenu, FooterMenuItemType } from './FooterMenu';
 import { graphql, useStaticQuery } from 'gatsby';
-import { FooterSocial } from './FooterSocial';
-import { FooterLogoAndStatus } from './FooterLogoAndStatus';
+import { FooterTopNav } from './FooterTopNav';
+import { FooterBottomNav } from './FooterBottomNav';
 
 export const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -32,42 +31,15 @@ export const Footer = () => {
     }
   `);
 
-  const menuDataItems = data.menuData.items;
-  const ablySocialLinks = data.ablySocial.items;
-  const bottomLinks = data.bottomMenu.items;
+  const menuDataItems = data.menuData.items ?? [];
+  const ablySocialLinks = data.ablySocial.items ?? [];
+  const bottomLinks = data.bottomMenu.items ?? [];
 
   return (
     <>
       <footer className="bg-white antialiased" data-id="footer">
-        <div className="max-w-screen-xl mx-auto py-64 ui-grid-gap grid ui-grid-px grid-cols-6">
-          <div className="col-span-full md:col-span-2 mb-30 md:mb-0">
-            <FooterLogoAndStatus />
-            <p className="mt-24 ui-text-p3">Connect with us on:</p>
-            <div className="pt-16 flex items-center">
-              {ablySocialLinks
-                ? ablySocialLinks.map((social: { label: string; link: string }) => (
-                    <FooterSocial label={social.label} link={social.link} key={social.label} />
-                  ))
-                : null}
-            </div>
-          </div>
-          {menuDataItems
-            ? menuDataItems.map((item: { items: FooterMenuItemType[]; label: string }) => (
-                <FooterMenu label={item.label} items={item.items} key={item.label} />
-              ))
-            : null}
-        </div>
-        <div className="max-w-screen-xl mx-auto py-40 border-t border-mid-grey w-full">
-          <div className="inline-flex  justify-center w-full space-x-24">
-            {bottomLinks
-              ? bottomLinks.map((link: { label: string; link: string }) => (
-                  <a key={link.label} className="ui-footer-link" href={link.link}>
-                    {link.label}
-                  </a>
-                ))
-              : null}
-          </div>
-        </div>
+        <FooterTopNav ablySocialLinks={ablySocialLinks} menuDataItems={menuDataItems} />
+        <FooterBottomNav bottomLinks={bottomLinks} />
       </footer>
     </>
   );
--- src/components/Footer/FooterTopNav/FooterMenu/FooterMenu.tsx
+++ src/components/Footer/FooterTopNav/FooterMenu/FooterMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FooterMenuItem } from './FooterMenuItem';
 
 export type FooterMenuItemType = { label: string; link: string };
 
@@ -7,15 +8,9 @@ export const FooterMenu = ({ items, label }: { items: FooterMenuItemType[]; labe
     <div className="col-span-full sm:col-span-3 md:col-span-1">
       <h2 className="ui-footer-col-title">{label}</h2>
       <ul>
-        {items
-          ? items.map((item: FooterMenuItemType) => (
-              <li className="p-menu-row-snug pb-8" key={item.label}>
-                <a href={item.link} className="ui-footer-menu-row-link">
-                  {item.label}
-                </a>
-              </li>
-            ))
-          : null}
+        {items.map((item: FooterMenuItemType) => (
+          <FooterMenuItem label={item.label} link={item.link} key={item.label} />
+        ))}
       </ul>
     </div>
   );
