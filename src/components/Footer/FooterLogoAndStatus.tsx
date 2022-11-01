66697b7edaa3d2753bcbfb6932f36a4dcdcd6dc5
april <aprilroseannkwong@gmail.com>
2022-11-01 11:40:07 +0000

    Upgrade Ably UI and add icons

---
+++ src/components/Footer/FooterLogoAndStatus.tsx
@@ -0,0 +1,17 @@
+import Logo from '@ably/ui/core/Logo';
+import FeaturedLink from '@ably/ui/core/FeaturedLink';
+import React from 'react';
+import { DOCUMENTATION_PATH } from '../../../data/transform/constants';
+
+export const FooterLogoAndStatus = () => (
+  <>
+    <Logo dataId="footer-logo" href={DOCUMENTATION_PATH} />
+    <div className="flex flex-row mt-24 -mb-6">
+      <iframe className="w-24 h-24 mt-4" src="https://status.ably.com/embed/icon"></iframe>
+      <span className="pl-8 font-medium">System status</span>
+    </div>
+    <FeaturedLink url="https://status.ably.com/" textSize="text-p3">
+      <span className="ml-30">More on our status site</span>
+    </FeaturedLink>
+  </>
+);
