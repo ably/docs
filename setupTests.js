442a488b334cce21af502beb3dba210d3af2844c
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-11-02 09:13:37 +0000

    [EDX-396] adopt ably UI code highlight (#1678)
    * Npm audit fix
    
    * Update Ably UI, replace PrismJS
    
    * Fix DOMPurify issue with SSR

--- setupTests.js
+++ setupTests.js
@@ -3,6 +3,15 @@ import { server } from './mocks/server';
 
 process.env.GATSBY_ADDSEARCH_API_KEY = 'shh-do-not-tell-to-anyone';
 
+jest.mock('@ably/ui/src/core/utils/syntax-highlighter', () => ({
+  highlightSnippet: jest.fn,
+  registerDefaultLanguages: jest.fn,
+}));
+
+jest.mock('@ably/ui/src/core/utils/syntax-highlighter-registry', () => ({
+  default: jest.fn,
+}));
+
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 // Reset any request handlers that we may add during the tests,
