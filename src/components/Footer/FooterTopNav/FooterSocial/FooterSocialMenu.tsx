99f218c837257f9cbcc8f63c49f7a74de1108e70
april <aprilroseannkwong@gmail.com>
2022-11-02 16:22:23 +0000

    removed the  Sidebar Logo and update footer text
    removed the  Sidebar Logo and update footer text

--- src/components/Footer/FooterTopNav/FooterSocial/FooterSocialMenu.tsx
+++ src/components/Footer/FooterTopNav/FooterSocial/FooterSocialMenu.tsx
@@ -4,7 +4,7 @@ import { FooterMenuItemType } from '../FooterMenu/FooterMenu';
 
 export const FooterSocialMenu = ({ ablySocialLinks }: { ablySocialLinks: FooterMenuItemType[] }) => (
   <>
-    <p className="mt-24 ui-text-p3">Connect with us on:</p>
+    <p className="mt-24 ui-text-p3">Find us on:</p>
     <div className="pt-16 flex items-center">
       {ablySocialLinks.map((social: { label: string; link: string }) => (
         <FooterSocialMenuIcons label={social.label} link={social.link} key={social.label} />
