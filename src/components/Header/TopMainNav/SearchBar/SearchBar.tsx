27856109ae8fb00e3705c49029a68a2b39354fbd
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-11-03 17:03:08 +0000

    [EDX-367] Trigger search via cmd+k keyboard shortcut (#1675)
    * Add CMD+K design + mobile
    
    * Fix the tests
    
    * Feedback

--- src/components/Header/TopMainNav/SearchBar/SearchBar.tsx
+++ src/components/Header/TopMainNav/SearchBar/SearchBar.tsx
@@ -1,14 +1,21 @@
 import React, { ChangeEvent, useRef, useState } from 'react';
-
-import { useSearch } from 'src/hooks';
 import useKeyboardShortcut from 'use-keyboard-shortcut';
-import { SearchIcon, DisplayMode, SearchDisplay } from '.';
+import Icon from '@ably/ui/core/Icon';
+
+import { useFunctionOnOutsideClick } from 'src/hooks/useFunctionOnOutsideClick';
+import { useSearch } from 'src/hooks';
+import { isMac } from 'src/utilities';
+
+import { DisplayMode, SearchDisplay } from '.';
 import { SuggestionBox } from './SuggestionBox';
+import { KeyIcon } from './KeyIcon';
+
+import { searchInput } from './SearchBar.module.css';
 
 export const SearchBar = ({ displayMode }: { displayMode: DisplayMode }) => {
   const textInput = useRef<null | HTMLInputElement>(null);
+  const searchDisplayRef = useRef<null | HTMLDivElement>(null);
   const [isInFocus, setIsInFocus] = useState(false);
-  const focusOnSearchInput = () => textInput?.current?.focus();
 
   const {
     state: { query, results },
@@ -26,24 +33,32 @@ export const SearchBar = ({ displayMode }: { displayMode: DisplayMode }) => {
     search({ query: value });
   };
 
-  useKeyboardShortcut(['Meta', 'K'], () => textInput?.current?.focus(), {
+  const focusOnSearchInput = () => {
+    setIsInFocus(true);
+    textInput?.current?.focus();
+  };
+  const handleFocusEvent = () => setIsInFocus(false);
+
+  useKeyboardShortcut(['Meta', 'K'], focusOnSearchInput, {
     overrideSystem: true,
     repeatOnHold: false,
   });
 
+  useFunctionOnOutsideClick(handleFocusEvent, searchDisplayRef);
+
   return (
-    <SearchDisplay onClick={focusOnSearchInput} displayMode={displayMode}>
-      <SearchIcon className="place-self-center" />
+    <SearchDisplay ref={searchDisplayRef} onClick={focusOnSearchInput} displayMode={displayMode}>
+      <Icon name="icon-gui-search" size="1.5rem" />
       <input
         type="text"
         ref={textInput}
         placeholder="Search"
-        className="h-48 font-light bg-transparent pl-8 text-base outline-none w-160 max-w-512"
+        className={searchInput}
         value={query}
         onChange={handleSearch}
-        onFocus={() => setIsInFocus(true)}
-        onBlur={() => setIsInFocus(false)}
       />
+      <KeyIcon className="mr-4">{isMac ? '⌘' : '^'}</KeyIcon>
+      <KeyIcon>K</KeyIcon>
       <SuggestionBox results={results} isActive={isInFocus} />
     </SearchDisplay>
   );
