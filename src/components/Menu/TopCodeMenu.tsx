71946650a3d1885de227326292905cf71cc11bf5
april <aprilroseannkwong@gmail.com>
2022-11-03 14:27:02 +0000

    Show CodeMenu if Lang and version is present
    clear condition

--- src/components/Menu/TopCodeMenu.tsx
+++ src/components/Menu/TopCodeMenu.tsx
@@ -7,13 +7,18 @@ import VersionMenu, { VersionMenuProps } from './VersionMenu';
 
 const TopCodeMenu = ({ languages, versionData }: { languages: string[]; versionData: VersionMenuProps }) => {
   const pageLanguage = useContext(PageLanguageContext);
-  const showCodeMenu = languages && languages.length > 1;
+  const showVersionMenu = versionData && versionData.versions.length > 0;
+  const showLanguageSelector = languages && languages.length > 0;
+  const showCodeMenu = showLanguageSelector || showVersionMenu;
   const showDefaultLink = pageLanguage !== DEFAULT_LANGUAGE;
+
   return showCodeMenu ? (
     <div className="fixed right-0 z-10 mt-64 w-full items-end border-b border-mid-grey bg-white md:col-span-4 md:w-4/5 2xl:w-full">
       <HorizontalMenu variant={HorizontalMenuVariant.end}>
         <VersionMenu {...versionData} />
-        <LanguageDropdownSelector language={pageLanguage} languages={languages} showDefaultLink={showDefaultLink} />
+        {showLanguageSelector ? (
+          <LanguageDropdownSelector language={pageLanguage} languages={languages} showDefaultLink={showDefaultLink} />
+        ) : null}
       </HorizontalMenu>
     </div>
   ) : null;
