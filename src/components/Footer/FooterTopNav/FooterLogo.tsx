07b73e1033d1f16d9cad11b369db59dc01661078
april <aprilroseannkwong@gmail.com>
2022-11-04 15:34:31 +0000

    Logo changes based on feedback

--- src/components/Footer/FooterTopNav/FooterLogo.tsx
+++ src/components/Footer/FooterTopNav/FooterLogo.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import Logo from '@ably/ui/core/Logo';
-import { DOCUMENTATION_PATH } from '../../../../data/transform/constants';
 
 export const FooterLogo = () => (
   <>
-    <Logo dataId="footer-logo" href={DOCUMENTATION_PATH} />
+    <Logo dataId="footer-logo" href="/" />
   </>
 );
