e1dfa41cc360f34ddb3a971cb343d7ceb4e7b167
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-11-07 13:30:34 +0000

    EDX-372: Clicking on images should follow the same UX as the marketing website (voltaire) (#1706)

--- src/components/blocks/external-references/A.tsx
+++ src/components/blocks/external-references/A.tsx
@@ -6,20 +6,32 @@ import { DOCUMENTATION_NAME } from '../../../../data/transform/constants';
 import { HtmlComponentProps } from '../../html-component-props';
 
 import './styles.css';
+import Img from './Img';
 
 const StyledGatsbyLink = ({ to, children, ...props }: Omit<GatsbyLinkProps<Record<string, unknown>>, 'ref'>) => (
-  <Link className="docs-link" to={to} {...props}>
+  <Link className="docs-link" data-testid="gatsby-link" to={to} {...props}>
     {children}
   </Link>
 );
 
 const A = ({ data, attribs }: HtmlComponentProps<'a'>): ReactElement => {
   const rawHref = attribs?.href;
+
+  // If there is an image inside the link with src same as href, then nuke <a> and render <img> only
+  if (Array.isArray(data)) {
+    const imageElement = data.find((item) => item.name === 'img');
+    // @ts-ignore - says src doesn't exist, but it does
+    if (imageElement?.attribs?.src === rawHref && imageElement?.data) {
+      return <Img attribs={imageElement?.attribs} data={imageElement?.data} />;
+    }
+  }
+
   if (rawHref && /^(\/|https?:\/\/(?:www.)?ably.com\/docs).*/.test(rawHref)) {
     let href = rawHref;
     if (/^\/(?!docs\/).*/.test(rawHref)) {
       href = `/${DOCUMENTATION_NAME}${rawHref}`;
     }
+
     return (
       <StyledGatsbyLink to={href} {...{ ...attribs }}>
         <Html data={data} />
