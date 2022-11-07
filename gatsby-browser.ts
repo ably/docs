c7c7ec25cf5d2b3c5ee45cd9d35bbb5fa563f200
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-11-07 17:37:56 +0000

    [EDX-466] Page heading cut off on page load (#1707)

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -22,10 +22,6 @@ const onClientEntry = () => {
   attachStoreToWindow(store);
 };
 
-const versionRegex = /\/versions\/v\d+\.\d+/;
-const languageRegex = /\?lang=\w+/;
-const hashRegex = /(\/?#.*)$/;
-
 /**
  *  The 'shouldUpdateScroll' function will fire when navigating to new pages within Gatsby
  */
@@ -33,14 +29,12 @@ const shouldUpdateScroll = ({ prevRouterProps, routerProps: { location } }) => {
   if (!prevRouterProps || !location) {
     return false;
   }
-  const curr = location.href;
-  const prev = prevRouterProps.location.href;
 
-  const plainCurr = curr.replace(versionRegex, '').replace(languageRegex, '').replace(hashRegex, '');
-  const plainPrev = prev.replace(versionRegex, '').replace(languageRegex, '').replace(hashRegex, '');
+  const curr = location.pathname;
+  const prev = prevRouterProps.location.pathname;
 
   return (
-    plainCurr !== plainPrev && // If the current location is a variant of the previous location, do not update scroll
+    curr !== prev && // If the current location is a variant of the previous location, do not update scroll
     !(curr.indexOf('#') > -1) &&
     !(prev.indexOf('#') > -1) // If we're going to or from any hash link on page B from page A, do not update scroll; we want to visit the specific section
   );
