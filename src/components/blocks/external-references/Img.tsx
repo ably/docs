2aeabc4791a6c892e196d12454c5e67f2d960c4d
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-11-07 13:30:34 +0000

    EDX-372: Clicking on images should follow the same UX as the marketing website (voltaire) (#1706)

--- src/components/blocks/external-references/Img.tsx
+++ src/components/blocks/external-references/Img.tsx
@@ -1,14 +1,24 @@
 import React, { ReactElement } from 'react';
+import Zoom from 'react-medium-image-zoom';
 import { StaticImage } from 'src/components/StaticImage';
 import { HtmlComponentProps } from '../../html-component-props';
 import SelfClosingHtmlBlock from '../Html/SelfClosingHtmlBlock';
 
+import 'react-medium-image-zoom/dist/styles.css';
+import { classDialog } from './Img.module.css';
+
 const Img = ({ attribs }: HtmlComponentProps<'img'>): ReactElement => {
   const rawSrc = attribs?.src;
-  if (rawSrc && /^\/images.*/.test(rawSrc)) {
-    return <StaticImage src={rawSrc} {...{ ...attribs }} />;
-  }
-  return SelfClosingHtmlBlock('img')({ attribs });
+
+  return (
+    <Zoom zoomMargin={32} classDialog={classDialog}>
+      {rawSrc && /^\/images.*/.test(rawSrc) ? (
+        <StaticImage src={rawSrc} {...{ ...attribs }} />
+      ) : (
+        SelfClosingHtmlBlock('img')({ attribs })
+      )}
+    </Zoom>
+  );
 };
 
 export default Img;
