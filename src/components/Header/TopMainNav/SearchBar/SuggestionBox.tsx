4183ca952715b1202df288787a2715fe46b536c7
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-11-07 17:38:34 +0000

    [EDX-485] Search results don't match the specs (#1701)
    * Fix style issues and title and breadcrumbs display
    
    * Add fixes

--- src/components/Header/TopMainNav/SearchBar/SuggestionBox.tsx
+++ src/components/Header/TopMainNav/SearchBar/SuggestionBox.tsx
@@ -5,6 +5,7 @@ import clamp from 'lodash/clamp';
 
 import { HitType, useKeyPress } from 'src/hooks';
 
+import { DOCUMENTATION_PATH } from '../../../../../data/transform/constants';
 import { container, hitItem, titleStyle } from './SuggestionBox.module.css';
 
 type Props = {
@@ -38,10 +39,18 @@ export const SuggestionBox = ({ results, isActive }: Props) => {
 
   return (
     <div aria-label="suggestions" className={container}>
-      <div className="font-light text-dark-grey uppercase text-menu3 px-16">Results from docs</div>
+      <div className="font-light text-dark-grey uppercase text-menu3 px-16 mb-2">Results from docs</div>
       {results &&
         results.map((hit, index) => {
-          const { title, highlight, url, id } = hit;
+          const { title, highlight, meta_description, url, id } = hit;
+          const [pageTitle, ...breadcrumbs] = title.split(' / ').filter(
+            (item) =>
+              // We need to get rid of 'Docs' because it's redundant and
+              // 'Root` because there are no root section it gets redirected to '/docs/'
+              !DOCUMENTATION_PATH.toLowerCase().includes(item.trim().toLowerCase()) ||
+              item.trim().toLowerCase() === 'root',
+          );
+
           return (
             <a
               id={`suggestion-${index}`}
@@ -49,14 +58,18 @@ export const SuggestionBox = ({ results, isActive }: Props) => {
               href={url}
               tabIndex={index}
               className={cn(
-                'block p-16 hover:bg-light-grey focus:bg-light-grey focus:outline-none rounded-lg break-all',
+                'block p-16 hover:bg-light-grey focus:bg-light-grey focus:outline-none rounded-lg break-all mb-4',
                 hitItem,
               )}
               role="link"
             >
-              <h4 className={cn('text-menu2 mb-6 font-medium', titleStyle)}>{title}</h4>
-              <div className="text-menu3 font-light text-charcoal-grey">{htmr(highlight)}</div>
-              {url && <div className="text-dark-grey font-light text-menu3 mt-8">{new URL(url).hostname}</div>}
+              <h4 className={cn('text-menu2 mb-6 font-medium', titleStyle)}>{pageTitle}</h4>
+              <div className="text-menu3 font-light text-charcoal-grey leading-5">
+                {htmr(meta_description ?? highlight)}
+              </div>
+              {breadcrumbs.length > 0 && (
+                <div className="text-dark-grey font-light text-menu3 mt-8">{breadcrumbs.join(' > ').toLowerCase()}</div>
+              )}
             </a>
           );
         })}
