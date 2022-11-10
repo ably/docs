24ffd12822e5290cea73a203c024ebf81de3bd7c
april <aprilroseannkwong@gmail.com>
2022-11-10 16:32:15 +0000

    add space so the h5 text will display also add H5 to be copied link

---
+++ src/components/blocks/api-reference/headings/ApiReferenceH5.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+import LinkableHtmlBlock from 'src/components/blocks/Html/LinkableHtmlBlock';
+import { HtmlAttributes } from 'src/components/html-component-props';
+
+const StyledApiReferenceH5: FC<HtmlAttributes<'h5'>> = ({ children, ...attribs }) => (
+  <h5 {...attribs} className="ui-text-h5 font-sans my-16 font-bold leading-normal">
+    {children}
+  </h5>
+);
+
+export const ApiReferenceH5 = LinkableHtmlBlock(StyledApiReferenceH5, 'mb-24');
