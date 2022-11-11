1d44c76748e30e9b1603eed2617cf8645c189387
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-11-11 10:07:03 +0000

    Its a goddamn bear jamboroo out here

--- data/transform/pre-parser/textile-js-workarounds/fix-textile-definition-lists/index.ts
+++ data/transform/pre-parser/textile-js-workarounds/fix-textile-definition-lists/index.ts
@@ -1,5 +1,4 @@
 import { compose } from 'lodash/fp';
-import { fixDivsInDefinitionLists } from './fix-divs-in-definition-lists';
 import { fixNewlinesInDefinitionLists } from './fix-newlines-in-definition-lists';
 
-export const fixTextileDefinitionLists = compose(fixNewlinesInDefinitionLists, fixDivsInDefinitionLists);
+export const fixTextileDefinitionLists = compose(fixNewlinesInDefinitionLists);
