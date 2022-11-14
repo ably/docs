d6a3dfd29c620d363b20a82c65621e0207217eeb
Nikita Kakuev <10689226+snikidev@users.noreply.github.com>
2022-11-14 10:46:16 +0000

    [EDX-486] Search bar features - 'no results' and text highlighting (#1733)

--- src/components/Header/TopMainNav/SearchBar/SuggestionBox.tsx
+++ src/components/Header/TopMainNav/SearchBar/SuggestionBox.tsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import htmr from 'htmr';
 import cn from 'classnames';
 import clamp from 'lodash/clamp';
+import Icon from '@ably/ui/core/Icon';
 
 import { HitType, useKeyPress } from 'src/hooks';
 
 import { DOCUMENTATION_PATH } from '../../../../../data/transform/constants';
+import { EmptyState } from './EmptyState';
 import { container, hitItem, titleStyle } from './SuggestionBox.module.css';
 
 type Props = {
   results: HitType[] | null;
   isActive: boolean;
+  error?: { message: string } | null;
+  query: string;
 };
 
-export const SuggestionBox = ({ results, isActive }: Props) => {
+export const SuggestionBox = ({ results, isActive, error, query }: Props) => {
   const totalResults = results?.length ?? 0;
   const [selectedItem, setSelectedItem] = useState<number | null>(null);
 
@@ -33,14 +37,14 @@ export const SuggestionBox = ({ results, isActive }: Props) => {
 
   useKeyPress(['ArrowDown', 'ArrowUp'], handleResultItemSelect);
 
-  if (!isActive || !results || totalResults === 0) {
+  if (!isActive || !query) {
     return null;
   }
 
   return (
     <div aria-label="suggestions" className={container}>
       <div className="font-light text-dark-grey uppercase text-menu3 px-16 mb-2">Results from docs</div>
-      {results &&
+      {results && totalResults > 0 ? (
         results.map((hit, index) => {
           const { title, highlight, meta_description, url, id } = hit;
           const [pageTitle, ...breadcrumbs] = title.split(' / ').filter(
@@ -72,7 +76,36 @@ export const SuggestionBox = ({ results, isActive }: Props) => {
               )}
             </a>
           );
-        })}
+        })
+      ) : error ? (
+        <EmptyState>
+          We couldn&apos;t perform the search due to an API error. Please try again or{' '}
+          <a
+            href="https://ably.com/support"
+            target="_blank"
+            rel="noreferrer"
+            className="text-gui-active hover:underline"
+          >
+            raise a support ticket
+          </a>{' '}
+          if the problem persists.
+        </EmptyState>
+      ) : (
+        <EmptyState>
+          We couldn&apos;t find an exact match for <span className="font-medium">‘{query}’</span> in the docs, but you
+          may be able to find the results you&apos;re looking for on{' '}
+          <a
+            href={`https://ably.com/search?q=${query}`}
+            target="_blank"
+            rel="noreferrer"
+            className="text-gui-active hover:underline"
+          >
+            our main site
+            <Icon name="icon-gui-external-link" size="12px" additionalCSS="inline-block ml-4" />
+          </a>
+          .
+        </EmptyState>
+      )}
     </div>
   );
 };
