4fa266065e0016dff5d56bb2eaf4fd15223f8c89
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-11-16 15:58:14 +0000

    Recursively process divs

--- data/transform/pre-parser/index.ts
+++ data/transform/pre-parser/index.ts
@@ -1,4 +1,5 @@
 import { compose } from 'lodash/fp';
+import textile from 'textile-js';
 import { replaceERB } from './erbjs';
 import {
   convertJSAllToNodeAndJavaScript,
@@ -16,9 +17,17 @@ import { textileJSCompatibility } from './textile-js-workarounds';
 // Newlines before closing tags inhibit textile-js' ability to correctly parse HTML
 const removeNewlinesBeforeClosingTags: StringTransformation = (content) => content.replace(/\n+(<\/\w+>)/g, '$1');
 
+// Jest has difficulty with resolving recursively-composed functions from different files
+// so we define this textile-js-workaround here.
+export const recursivelyProcessDivs: StringTransformation = (content) =>
+  content.replace(/\n<div(.*?)>\n((?:.*?\n)+)<\/div>/g, (_match, p1, p2) => {
+    return `\n<div${p1}>${textile(preParser(p2))}</div>`;
+  });
+
 export const preParser = compose(
   // Textile compatibility must follow all other changes
   textileJSCompatibility,
+  recursivelyProcessDivs,
   // Language operations
   addLanguageSupportForHeadings,
   addLanguageSupportForBlockQuotes,
---
+++ data/transform/pre-parser/textile-js-workarounds/recursively-process-divs.test.ts
@@ -0,0 +1,21 @@
+import { recursivelyProcessDivs } from '..';
+
+describe('Divs that contain examples of textile to be processed correctly', () => {
+  it('Renders a known example from api/realtime-sdk/channels.textile correctly', () => {
+    expect(
+      recursivelyProcessDivs(`
+<div lang="jsall">
+bq(definition#on-state-array-listener).
+  jsall: on(String[] events, listener("ChannelStateChange":#channel-state-change stateChange))
+
+Same as above, but registers multiple listeners, one for each event in the array.
+</div>`),
+    ).toMatchInlineSnapshot(`
+      "
+      <div lang=\\"jsall\\"><blockquote class=\\"definition\\" id=\\"on-state-array-listener\\">
+      <p class=\\"definition\\"><span lang=\\"javascript,nodejs\\">on(String[] events, listener(<a href=\\"#channel-state-change\\">ChannelStateChange</a> stateChange))</span></p>
+      </blockquote>
+      <p>Same as above, but registers multiple listeners, one for each event in the array.</p></div>"
+    `);
+  });
+});
