0eaa5e4c87def92187841ed37c9132d8f6064c4e
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-11-17 12:13:28 +0000

    Remove console.log

--- src/templates/base-template.tsx
+++ src/templates/base-template.tsx
@@ -53,7 +53,6 @@ const Template = ({
     [menuLanguages],
   );
   const languagesExist = filteredLanguages.length > 0 || (versionData && versionData.versions.length > 0);
-  console.log(contentOrderedList);
   const elements = useMemo(
     () =>
       contentOrderedList.map(
