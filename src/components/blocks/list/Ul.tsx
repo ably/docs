5003607c58dc730cbb69463543f969108f8cc675
snikidev <n.kakuev@gmail.com>
2022-11-23 11:51:01 +0000

    Change font-size for paragraph

--- src/components/blocks/list/Ul.tsx
+++ src/components/blocks/list/Ul.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import GenericHtmlBlock from '../Html/GenericHtmlBlock';
 
-const Ul: FC = (props) => <ul {...props} className="ui-text-p1 pl-16 pb-32 list-disc -mt-16" />;
+const Ul: FC = (props) => <ul {...props} className="ui-text-p2 pl-16 pb-32 list-disc -mt-16" />;
 
 export default GenericHtmlBlock(Ul);
