1da584dcc783da1774d8267734f0b1742dffcfae
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-11-24 15:20:07 +0000

    fixup: export site metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,6 @@
 const mainWebsite = process.env.GATSBY_ABLY_MAIN_WEBSITE ?? 'http://localhost:3000';
 
-const siteMetadata = {
+export const siteMetadata = {
   siteUrl: `${mainWebsite}/docs`,
   title: 'Documentation | Ably Realtime',
 };
