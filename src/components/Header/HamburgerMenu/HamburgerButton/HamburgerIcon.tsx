b8cf1e6f673e58635c91eef65a79ec939af76715
MymmiJ <32373140+MymmiJ@users.noreply.github.com>
2022-11-30 17:04:02 +0000

    Make adjustments to Hamburger Icon and Ably Logo so they show more easily (#1776)
    Co-authored-by: Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>

--- src/components/Header/HamburgerMenu/HamburgerButton/HamburgerIcon.tsx
+++ src/components/Header/HamburgerMenu/HamburgerButton/HamburgerIcon.tsx
@@ -5,7 +5,7 @@ const MENU_DESCRIPTION = 'Site navigation menu';
 
 export const HamburgerIcon = ({ isOpen }: { isOpen: boolean }) =>
   isOpen ? (
-    <StaticImage src={'/images/icons/hamburger-is-open.svg'} alt={MENU_DESCRIPTION} />
+    <StaticImage className="min-w-24" src={'/images/icons/hamburger-is-open.svg'} alt={MENU_DESCRIPTION} />
   ) : (
-    <StaticImage alt={MENU_DESCRIPTION} src="/images/icons/hamburger-is-closed.svg" />
+    <StaticImage className="min-w-24" alt={MENU_DESCRIPTION} src="/images/icons/hamburger-is-closed.svg" />
   );
