713fdd52a171bbff684bebe5fb59f6c7180d48c5
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-12-06 11:14:57 +0000

    Make sure site name is only derived one time

--- src/utilities/assets/src-from-docs-site.ts
+++ src/utilities/assets/src-from-docs-site.ts
@@ -2,4 +2,6 @@ const herokuAppSite = process.env.HEROKU_APP_NAME
   ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
   : 'http://localhost:9000';
 
-export const srcFromDocsSite = (src: string) => `${process.env.GATSBY_DOCS_SITE_URL ?? herokuAppSite}${src}`;
+const sitePrefix = `${process.env.GATSBY_DOCS_SITE_URL ?? herokuAppSite}`;
+
+export const srcFromDocsSite = (src: string) => `${sitePrefix}${src}`;
