962d5b94f08e244af26a254206e007b62133a6d4
snikidev <n.kakuev@gmail.com>
2022-12-07 22:11:37 +0000

    Add final touches from the feedback

--- src/components/Header/SearchBar/SearchBar.tsx
+++ src/components/Header/SearchBar/SearchBar.tsx
@@ -58,26 +58,28 @@ export const SearchBar = ({ displayMode }: { displayMode: DisplayMode }) => {
     <div
       ref={searchDisplayRef}
       onClick={focusOnSearchInput}
-      className={cn('relative flex items-center justify-start pt-24 pb-16 md:py-0 mx-24', {
-        'md:hidden': displayMode === DisplayMode.MOBILE,
-        'hidden md:block': displayMode === DisplayMode.FULL_SCREEN,
+      className={cn('relative items-center justify-start md:py-0', {
+        'flex md:hidden': displayMode === DisplayMode.MOBILE,
+        'hidden md:flex': displayMode === DisplayMode.FULL_SCREEN,
       })}
     >
-      <input
-        type="text"
-        ref={textInput}
-        placeholder="Search"
-        className={cn(searchInput)}
-        value={query}
-        onChange={handleSearch}
-      />
-      <Icon name="icon-gui-search" size="24px" additionalCSS="absolute left-16 top-36 md:top-12" />
-      {!isInFocus && (
-        <div className="absolute right-16 top-12 hidden lg:flex items-center justify-end">
-          <KeyIcon className="mr-4">{isMac ? '⌘' : '^'}</KeyIcon>
-          <KeyIcon className="pt-2">K</KeyIcon>
-        </div>
-      )}
+      <div className="relative w-full mx-24 mt-24 md:m-0">
+        <input
+          type="text"
+          ref={textInput}
+          placeholder="Search"
+          className={cn(searchInput)}
+          value={query}
+          onChange={handleSearch}
+        />
+        <Icon name="icon-gui-search" size="24px" additionalCSS="absolute left-16 top-12" />
+        {!isInFocus && (
+          <div className="absolute right-16 top-12 hidden lg:flex items-center justify-end">
+            <KeyIcon className="mr-4">{isMac ? '⌘' : '^'}</KeyIcon>
+            <KeyIcon className="pt-2">K</KeyIcon>
+          </div>
+        )}
+      </div>
       <SuggestionBox results={results} error={error} query={query} isActive={isInFocus} />
     </div>
   );
