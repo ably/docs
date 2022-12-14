b4961b4dc8dc84345baccef3268359a4a548b504
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2022-12-14 09:50:59 +0000

    Remove signs of Ruby/Nanoc toolchain from the codebase

--- lib/default.rb
+++
@@ -1,9 +0,0 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
-
-require 'pry'
-require 'pry-byebug'
-
-include Nanoc::Helpers::LinkTo
-include Nanoc::Helpers::HTMLEscape
-include Nanoc::Helpers::Rendering
