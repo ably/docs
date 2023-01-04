fcf06268af2a8d70b5c4a1442088922acc5db725
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2023-01-04 16:25:12 +0000

    Add home page link to API reference pages

--- src/components/Menu/HomePageLink/HomePageLink.test.tsx
+++ src/components/Menu/HomePageLink/HomePageLink.test.tsx
@@ -1,8 +1,8 @@
 import { expectAccessibility } from 'src/utilities/test/expect-accessibility';
-import PageTitle from './PageTitle';
+import { HomePageLink } from './HomePageLink';
 
 describe('Basic accessibility test for PageTitle', () => {
   it('is accessible', () => {
-    expectAccessibility(<PageTitle>Lorem Ipsum</PageTitle>);
+    expectAccessibility(<HomePageLink />);
   });
 });
--- src/components/Menu/TopCodeMenu.tsx
+++ src/components/Menu/TopCodeMenu.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { HorizontalMenu, HorizontalMenuVariant } from 'src/components';
 import { DEFAULT_LANGUAGE } from '../../../data/createPages/constants';
 import PageLanguageContext from '../../contexts/page-language-context';
+import { HomePageLink } from './HomePageLink/HomePageLink';
 import { LanguageDropdownSelector } from './LanguageDropdownSelector/LanguageDropdownSelector';
 import VersionMenu, { VersionMenuProps } from './VersionMenu';
 
@@ -15,9 +16,10 @@ const TopCodeMenu = ({ languages, versionData }: { languages: string[]; versionD
   return showCodeMenu ? (
     <div
       id="top-code-menu"
-      className="fixed right-0 z-10 top-64 w-full items-end border-b border-mid-grey bg-white px-24"
+      className="fixed right-0 z-10 top-64 left-0 md:pl-244 w-full items-end border-b border-mid-grey bg-white px-24"
     >
       <HorizontalMenu variant={HorizontalMenuVariant.end}>
+        <HomePageLink />
         <VersionMenu {...versionData} />
         {showLanguageSelector ? (
           <LanguageDropdownSelector language={pageLanguage} languages={languages} showDefaultLink={showDefaultLink} />
