ada24a47b9c5042f2c9c6428eff9ec49219b2593
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2023-02-28 14:18:08 +0000

    Change API token endpoint to API keys

--- src/redux/api-key/constants.ts
+++ src/redux/api-key/constants.ts
@@ -3,4 +3,4 @@ export const API_KEYS_REDUCER_KEY = 'api-key';
 const WEB_API = process.env.GATSBY_ABLY_MAIN_WEBSITE || 'http://localhost:3000';
 export const WEB_API_USER_DATA_ENDPOINT = `${WEB_API}/api/me`;
 export const WEB_API_KEYS_DATA_ENDPOINT = `${WEB_API}/api/api_keys`;
-export const WEB_API_TEMP_KEY_ENDPOINT = `${WEB_API}/ably-auth/token/docs`;
+export const WEB_API_TEMP_KEY_ENDPOINT = `${WEB_API}/ably-auth/api-key/docs`;
