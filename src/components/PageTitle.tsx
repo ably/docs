e6331af1f45a7ea865943a54f76fb55d476ff27d
Jimmy MacGregor <jimmymacgregor@Jimmys-MacBook-Pro.local>
2023-02-28 17:00:50 +0000

    fix: unclear why PageTitle was ever styled in this way

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent as FC } from 'react';
 
 const PageTitle: FC = ({ children }) => (
-  <h1 id="title" className="ui-text-h1 my-40 col-span-2 whitespace-nowrap overflow-x-scroll">
+  <h1 id="title" className="ui-text-h1 my-40 col-span-2">
     {children}
   </h1>
 );
