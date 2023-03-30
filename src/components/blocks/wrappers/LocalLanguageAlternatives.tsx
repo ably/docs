50419d3ee3b497fb316d1a08e0e9fb17ba77454d
april <aprilroseannkwong@gmail.com>
2023-03-30 14:43:34 +0100

    Code blocks should always change global language
    - Remove the localChange functionality and component parameters so it always reload whenever they select new language
    - In the LocalLanguageAlternatives, remove the MenuItemButton component as it wont be used anymore abd other event handling function for localChange
    - Added a method, helps us to determine if an array of languages has a pageSelected present, a condition added if it is an SDK Interface, so we can compare the values correctly . So if language is present in the languages then return the pageLanguage if not then return the first language of the array
    - Rename the function from getFilteredLanguages to getTrimmedLanguage so its more easier to understand the use of the function
    - In the LanguageButton, separated the isLanguageActive as we will pass a pageLanguage that is not only from the context lang, but a condition
      where the context page lang is not present in the languages we will pass the first language of the languages

--- src/components/blocks/wrappers/LocalLanguageAlternatives.tsx
+++ src/components/blocks/wrappers/LocalLanguageAlternatives.tsx
@@ -1,108 +1,70 @@
-import React, { useState, MouseEvent, useEffect, Dispatch, SetStateAction } from 'react';
+import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
 import languageLabels from '../../../maps/language';
-import { MenuItemButton } from '../../Menu/MenuItemButton';
 import Html from '../Html';
 import { LanguageNavigation } from '../../Menu/LanguageNavigation';
-import { getFilteredLanguages, LanguageButton, ReactSelectOption } from 'src/components';
+import { getTrimmedLanguage, LanguageButton } from 'src/components';
 import { LanguageNavigationProps } from '../../Menu/LanguageNavigation';
 import { HtmlComponentProps, HtmlComponentPropsData, ValidReactElement } from 'src/components/html-component-props';
 import { DEFAULT_LANGUAGE, DEFAULT_PREFERRED_LANGUAGE } from '../../../../data/createPages/constants';
-import { SingleValue } from 'react-select';
-import { isObject } from 'lodash';
-import { cleanIfLanguageHasSDKInterface } from '../software/Pre';
 
 const LocalLanguageAlternatives = ({
   languages,
   data,
   initialData,
-  localChangeOnly,
+  isSDKInterface,
   selectedSDKInterfaceTab,
   setSelectedSDKInterfaceTab,
   setPreviousSDKInterfaceTab,
+  selectedPageLanguage,
 }: {
   languages: string[];
   data?: Record<string, string | HtmlComponentProps<ValidReactElement>[] | null>;
   initialData: HtmlComponentPropsData;
-  localChangeOnly: boolean;
+  isSDKInterface: boolean;
   selectedSDKInterfaceTab: string;
   setSelectedSDKInterfaceTab: Dispatch<SetStateAction<string>>;
   setPreviousSDKInterfaceTab: Dispatch<SetStateAction<string>>;
+  selectedPageLanguage: string;
 }) => {
   const [selected, setSelected] = useState(initialData);
-  const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
+
   useEffect(() => {
     setSelected(initialData);
   }, [initialData]);
 
-  const setLocalSelected = (value: string) => {
-    const selectedLocalData = data ? data[value] : '';
-    if (selectedLocalData !== null && isObject(selectedLocalData?.[0])) {
-      /* when rest/realtime languages are passed from local they are not trimmed yet, so we need to trim here */
-      const selectedLocalDataLang = selectedLocalData[0]?.attribs?.lang;
-      const cleanSelectedLocalData = [
-        {
-          ...selectedLocalData[0],
-          attribs: {
-            lang: selectedLocalDataLang ? cleanIfLanguageHasSDKInterface(selectedLocalDataLang) : '',
-          },
-        },
-      ];
-      setSelected(cleanSelectedLocalData);
-    }
-    setSelectedLanguage(getFilteredLanguages(value));
-  };
-
-  const onClick = ({ currentTarget: { value } }: MouseEvent<HTMLButtonElement>) => {
-    setLocalSelected(value);
-  };
-
-  const onSelect = (newValue: SingleValue<ReactSelectOption>) => {
-    if (newValue) {
-      setLocalSelected(newValue.value);
-    }
-  };
-
-  const languageItems = languages
+  const filteredLanguagesWithoutDefault = languages
     .filter((lang) => lang !== DEFAULT_LANGUAGE)
-    .filter((lang) => lang !== '')
-    .map((lang) => {
-      // Site navigation button
+    .filter((lang) => lang !== '');
 
-      const languageSelected = lang || DEFAULT_PREFERRED_LANGUAGE;
-      const filterLanguageForLangButton = languageSDKInterfaceClean(languageSelected, selectedSDKInterfaceTab);
+  const isPreHasLanguageSelected = checkIfPreHaveLanguageSelected(
+    filteredLanguagesWithoutDefault,
+    selectedPageLanguage,
+    selectedSDKInterfaceTab,
+    isSDKInterface,
+  );
 
-      if (!localChangeOnly && filterLanguageForLangButton != '') {
-        return {
-          Component: LanguageButton,
-          props: { language: filterLanguageForLangButton },
-          content: languageLabels[lang] ?? lang,
-        };
-      }
-      // Local button, if global language option doesn't exist
+  const languageItems = filteredLanguagesWithoutDefault.map((lang) => {
+    // Site navigation button
 
-      const selectedLanguageFiltered = getFilteredLanguages(selectedLanguage);
-      const languageFiltered = getFilteredLanguages(lang);
-      const filterLanguageForMenuButton = languageSDKInterfaceClean(lang, selectedSDKInterfaceTab);
+    const languageSelected = lang || DEFAULT_PREFERRED_LANGUAGE;
+    const filterLanguageForLangButton = languageSDKInterfaceClean(languageSelected, selectedSDKInterfaceTab);
 
+    if (filterLanguageForLangButton != '') {
       return {
-        Component: MenuItemButton,
+        Component: LanguageButton,
         props: {
-          language: filterLanguageForMenuButton != '' ? filterLanguageForMenuButton : lang,
-          onClick,
-          value: lang,
-          isSelected: languageFiltered === selectedLanguageFiltered,
+          language: filterLanguageForLangButton,
+          selectedLanguageForPre: isPreHasLanguageSelected ? selectedPageLanguage : getTrimmedLanguage(languages[0]),
         },
         content: languageLabels[lang] ?? lang,
       };
-    });
+    }
+  });
 
   return (
     <>
       <LanguageNavigation
         items={languageItems as LanguageNavigationProps['items']}
-        localChangeOnly={localChangeOnly}
-        selectedLanguage={selectedLanguage}
-        onSelect={onSelect}
         allListOfLanguages={data ? Object.entries(data).map(([key]) => key) : []}
         selectedSDKInterfaceTab={selectedSDKInterfaceTab}
         setSelectedSDKInterfaceTab={setSelectedSDKInterfaceTab}
@@ -117,3 +79,16 @@ export default LocalLanguageAlternatives;
 
 const languageSDKInterfaceClean = (language: string, selectedTab: string) =>
   language.includes(`_`) ? (language.includes(`${selectedTab}_`) ? language.split('_', 2)[1] : '') : language;
+
+/* this method helps us to determine if an array of languages has a pageSelected present, a condition added if it is an SDK Interface, so we can compare the values correctly */
+const checkIfPreHaveLanguageSelected = (
+  languages: string[],
+  selectedPageLanguage: string,
+  selectedSDKInterfaceTab: string,
+  isSDKInterface: boolean,
+) =>
+  languages
+    .map((lang) =>
+      isSDKInterface ? lang === `${selectedSDKInterfaceTab}_${selectedPageLanguage}` : lang === selectedPageLanguage,
+    )
+    .includes(true);
