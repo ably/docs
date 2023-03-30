4d2fec559f6dcaaf0017c02cb27517e1855d4178
april <aprilroseannkwong@gmail.com>
2023-03-30 14:43:34 +0100

    Code blocks should always change global language
    - Remove the localChange functionality and component parameters so it always reload whenever they select new language
    - In the LocalLanguageAlternatives, remove the MenuItemButton component as it wont be used anymore abd other event handling function for localChange
    - Added a method, helps us to determine if an array of languages has a pageSelected present, a condition added if it is an SDK Interface, so we can compare the values correctly . So if language is present in the languages then return the pageLanguage if not then return the first language of the array
    - Rename the function from getFilteredLanguages to getTrimmedLanguage so its more easier to understand the use of the function
    - In the LanguageButton, separated the isLanguageActive as we will pass a pageLanguage that is not only from the context lang, but a condition
      where the context page lang is not present in the languages we will pass the first language of the languages

--- src/components/LanguageButton/LanguageButton.tsx
+++ src/components/LanguageButton/LanguageButton.tsx
@@ -1,19 +1,19 @@
 import { useContext, FunctionComponent as FC } from 'react';
 import cn from 'classnames';
 import { PageLanguageContext } from 'src/contexts';
-import { createLanguageHrefFromDefaults, getLanguageDefaults, getFilteredLanguages } from 'src/components';
+import { createLanguageHrefFromDefaults, getLanguageDefaults, getTrimmedLanguage } from 'src/components';
 import languageLabels from 'src/maps/language';
 import { cacheVisitPreferredLanguage } from 'src/utilities';
 import { LanguageNavigationComponentProps } from '../Menu/LanguageNavigation';
 import { button, isActive } from '../Menu/MenuItemButton/MenuItemButton.module.css';
 
-const LanguageButton: FC<LanguageNavigationComponentProps> = ({ language }) => {
+const LanguageButton: FC<LanguageNavigationComponentProps> = ({ language, selectedLanguageForPre }) => {
   const pageLanguage = useContext(PageLanguageContext);
-  const selectedLanguage = getFilteredLanguages(language);
-  const { isLanguageDefault, isPageLanguageDefault, isLanguageActive } = getLanguageDefaults(
-    selectedLanguage,
-    pageLanguage,
-  );
+  const selectedLanguage = getTrimmedLanguage(language);
+  const { isLanguageDefault, isPageLanguageDefault } = getLanguageDefaults(selectedLanguage, pageLanguage);
+  /* separated the isLanguageActive as we will pass a pageLanguage that is not only from the context lang, but a condition
+  where the context page lang is not present in the languages we will pass the first language of the languages */
+  const { isLanguageActive } = getLanguageDefaults(selectedLanguage, selectedLanguageForPre);
 
   const handleClick = () => {
     const href = createLanguageHrefFromDefaults(isPageLanguageDefault, isLanguageDefault, selectedLanguage);
