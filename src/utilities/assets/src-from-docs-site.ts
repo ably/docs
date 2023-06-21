3c16715ac048ca8b8c0278a04b20c98770ece8e0
Matthew O'Riordan <matthew.oriordan@gmail.com>
2023-06-21 11:19:39 +0100

    lint: Improve style for linter

--- src/utilities/assets/src-from-docs-site.ts
+++ src/utilities/assets/src-from-docs-site.ts
@@ -1,7 +1,5 @@
-const herokuAppSite = process.env.HEROKU_APP_NAME
-  ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
-  : '';
+const herokuAppSite = process.env.HEROKU_APP_NAME ?? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
 
-const sitePrefix = `${process.env.GATSBY_DOCS_SITE_URL ?? herokuAppSite}`;
+const sitePrefix = process.env.GATSBY_DOCS_SITE_URL ?? herokuAppSite;
 
 export const srcFromDocsSite = (src: string) => `${sitePrefix}${src}`;
