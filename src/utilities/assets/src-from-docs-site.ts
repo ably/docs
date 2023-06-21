6e6d99d9e4a404685c8f16f30f2071a600007dcc
Matthew O'Riordan <matthew.oriordan@gmail.com>
2023-06-21 10:55:06 +0100

    Remove hardcoded localhost URL
    Having a hardcoded URL for assets is unnecessary and makes it more difficult to work locally. Removing this hardcoded default allows us to not specify a URL for GATSBY_DOCS_SITE_URL

--- src/utilities/assets/src-from-docs-site.ts
+++ src/utilities/assets/src-from-docs-site.ts
@@ -1,6 +1,6 @@
 const herokuAppSite = process.env.HEROKU_APP_NAME
   ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
-  : 'http://localhost:9000';
+  : '';
 
 const sitePrefix = `${process.env.GATSBY_DOCS_SITE_URL ?? herokuAppSite}`;
 
