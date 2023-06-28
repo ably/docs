b558551ccfbc5090382f8e7e3c78edbf3005e8a1
Dominik Piatek <dominik.piatek@ably.io>
2023-06-28 13:21:40 +0100

    Revert change to URL prefixing for heroku
    This was changed my mistake - we need the ternary to correctly return the Heroku URL:
    const herokuAppSite = process.env.HEROKU_APP_NAME ?? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
    
    if process.env.HEROKU_APP_NAME is set, it will return ably-docs-rendering-and-pcbscx, a heroku app name is not it's URL

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,6 @@
-const herokuAppSite = process.env.HEROKU_APP_NAME ?? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
+const herokuAppSite = process.env.HEROKU_APP_NAME
+  ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+  : 'http://localhost:9000';
 
 const mainWebsite = process.env.GATSBY_ABLY_MAIN_WEBSITE ?? 'http://localhost:3000';
 
