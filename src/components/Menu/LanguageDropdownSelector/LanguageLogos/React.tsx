9ca74ffa7dea493ad6164a677b385c3e944c595e
Stephane Moreau <st.moreau@outlook.com.gr>
2023-08-23 17:42:32 +0100

    [SDK-3781] Add eslint-plugin-prettier

--- src/components/Menu/LanguageDropdownSelector/LanguageLogos/React.tsx
+++ src/components/Menu/LanguageDropdownSelector/LanguageLogos/React.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const ReactJS = () => (
   <svg width="24" height="24" viewBox="-11.5 -10.23174 23 20.46348" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="0" cy="0" r="2.05" fill="#61dafb" />
-    <g stroke="#61dafb" stroke-width="1" fill="none">
+    <g stroke="#61dafb" strokeWidth="1" fill="none">
       <ellipse rx="11" ry="4.2" />
       <ellipse rx="11" ry="4.2" transform="rotate(60)" />
       <ellipse rx="11" ry="4.2" transform="rotate(120)" />
