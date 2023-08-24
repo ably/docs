57b6ffdff4c22dfad9f22ab1d9c09ea2cb6e845c
Denis Sellu <denis.sellu@ably.com>
2023-08-24 17:02:00 +0300

    feat: add new product component

---
+++ src/components/ProductPage/ProductPageContent.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BodySection } from './BodySection/BodySection';
+
+export type LinkProps = {
+  text: string;
+  href: string;
+};
+
+export type CallToActionProps = {
+  text: string;
+  href: string;
+};
+
+export type CardProps = {
+  title: string;
+  type: string;
+  content: string;
+  image: string;
+  links: LinkProps[];
+};
+
+export type SectionProps = {
+  title: string;
+  level: keyof JSX.IntrinsicElements | null;
+  description: string | null;
+  columns: number;
+  releaseStage: string;
+  callToAction: CallToActionProps;
+  cards: CardProps[];
+};
+
+export const ProductPageContent = ({ sections }: { sections: SectionProps[] }) => (
+  <article className="px-24 md:pl-40 md:pr-48 xl:pr-64">
+    {sections.map((section, index) => (
+      <BodySection key={index} section={section} />
+    ))}
+  </article>
+);
