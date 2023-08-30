64458fff7801ac4190e2370c47cac28ede997726
Denis Sellu <denis.sellu@ably.com>
2023-08-30 14:33:12 +0300

    feat: add SVG object component
    this renders in safari and doesn't make it blurry

--- src/components/ProductPage/BodySection/Card/FeatureCard.tsx
+++ src/components/ProductPage/BodySection/Card/FeatureCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StaticImage } from '../../../StaticImage';
 import { CardProps } from '../../ProductPageContent';
 import { Links } from './Links';
+import { SvgObject } from '../../../SvgObject';
 
 export const FeatureCard = ({ title, content, image, links }: CardProps) => (
   <div className="border border-extra-light-grey rounded-lg bg-extra-light-grey flex justify-between block hover:border-mid-grey hover:text-cool-black group">
@@ -16,6 +16,6 @@ export const FeatureCard = ({ title, content, image, links }: CardProps) => (
         <Links links={links}></Links>
       </div>
     </div>
-    <StaticImage src={`/images/product/${image}`} alt={`${title}`} />
+    <SvgObject src={`/images/products/${image}`}></SvgObject>
   </div>
 );
