37715b9984be1d3114267f663c8560fb6c06dc70
Denis Sellu <denis.sellu@ably.com>
2023-09-04 18:44:36 +0300

    feat: add homepage sections with the hero component

--- src/components/Homepage/BodySection/BodySection.tsx
+++ src/components/Homepage/BodySection/BodySection.tsx
@@ -1,37 +1,48 @@
 import React from 'react';
-import { Section } from '../HomepageContent';
+import cn from 'classnames';
+import { SectionProps } from '../HomepageContent';
 import { BodySectionDescription } from './BodySectionDescription';
-import { BodySectionDescriptionContainer } from './BodySectionDescriptionContainer';
-import { BodySectionCard } from './Card/Card';
+import { HeroCard } from './Card/HeroCard';
+import { CallToAction } from './CallToAction';
 
-export const BodySection = ({ section }: { section: Section }) => {
-  const cardsExist = section.cards.length > 0;
+const cardTypes = {
+  hero: HeroCard,
+};
+
+export const BodySection = ({ section }: { section: SectionProps }) => {
+  const cards = section.cards ?? [];
+  const cardsExist = cards.length > 0;
+  const columns = section.columns;
+  const singleColumn = columns == 1;
+
+  const gridColVariants = {
+    1: 'lg:grid-cols-1',
+    2: 'sm:grid-cols-2 lg:grid-cols-2',
+    4: 'sm:grid-cols-4 lg:grid-cols-4',
+  };
+
+  const sectionBottomMarginVariants = {
+    48: 'mb-48',
+    72: 'mb-72',
+  };
+
   return (
-    <section className="grid grid-cols-6">
-      {section.level ? (
-        <section.level className={`ui-text-${section.level} mt-40 col-span-6`}>{section.title}</section.level>
-      ) : (
-        <h2 className="ui-text-h2 col-span-6">{section.title}</h2>
-      )}
-      {section.description ? (
-        <BodySectionDescription description={section.description} />
-      ) : (
-        <BodySectionDescriptionContainer />
-      )}
+    <section className={`${sectionBottomMarginVariants[section.bottomMargin]} w-full`} style={{ maxWidth: '960px' }}>
+      {section.title && <h2 className="ui-text-h2">{section.title}</h2>}
+      {section.description && <BodySectionDescription description={section.description} />}
       {cardsExist && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 col-span-6 mb-96 gap-24 mr-32">
-          {section.cards.map((card, index) => (
-            <BodySectionCard
-              key={index}
-              title={card.title}
-              content={card.content}
-              link={card.link}
-              flag={card.flag}
-              callToAction={card.callToAction ?? section.defaultCallToAction}
-            />
-          ))}
+        <div
+          className={cn({
+            [`grid grid-cols-1 sm:grid-cols-2 ${gridColVariants[columns]}  gap-24`]: !singleColumn,
+          })}
+        >
+          {cards.map((card, index) => {
+            const Card = cardTypes[card.type];
+            return <Card key={index} {...card} />;
+          })}
         </div>
       )}
+      {section.callToAction && <CallToAction {...section.callToAction}></CallToAction>}
     </section>
   );
 };
