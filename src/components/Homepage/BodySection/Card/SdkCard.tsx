afbcaa232b6f76a8a2036716ca369d8ce628192e
Denis Sellu <denis.sellu@ably.com>
2023-09-05 17:35:35 +0300

    feat: add sdk component for the homepage

---
+++ src/components/Homepage/BodySection/Card/SdkCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { CardProps } from '../../ProductPageContent';
+import { SvgObject } from '../../../SvgObject';
+
+const sdkLanguageStyle = {
+  fontFamily: 'Jetbrains mono',
+  fontWeight: '500',
+  color: 'var(--color-charcoal-grey)',
+  fontSize: 'var(--fs-p3)',
+  lineHeight: 'var(--lh-relaxed)',
+};
+
+export const SdkCard = ({ title, image }: CardProps) => (
+  <div className="items-center border border-extra-light-grey rounded-lg bg-extra-light-grey flex flex-col h-full p-16 pb-0">
+    <p className="mr-auto pb-16" style={sdkLanguageStyle}>
+      {title}
+    </p>
+    <SvgObject src={`/images/homepage/${image}`}></SvgObject>
+  </div>
+);
