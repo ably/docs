908c669161583ea9e1d287eed74a50eab11f33f8
Denis Sellu <denis.sellu@ably.com>
2023-09-08 13:43:55 +0300

    chore: refactor BodySection component variables

--- src/components/Homepage/BodySection/BodySection.tsx
+++ src/components/Homepage/BodySection/BodySection.tsx
@@ -13,30 +13,31 @@ const cardTypes = {
   sdk: SdkCard,
 };
 
+const gridColVariants = {
+  1: 'lg:grid-cols-1',
+  2: 'sm:grid-cols-2 lg:grid-cols-2',
+  4: 'sm:grid-cols-4 lg:grid-cols-4',
+};
+
+const sectionBottomMarginVariants = {
+  48: 'mb-48',
+  72: 'mb-72',
+};
+
+const gridGapVariants = {
+  2: 'gap-32',
+  4: 'gap-24',
+};
+
 export const BodySection = ({ section }: { section: SectionProps }) => {
   const cards = section.cards ?? [];
   const cardsExist = cards.length > 0;
   const columns = section.columns;
   const singleColumn = columns == 1;
-
-  const gridColVariants = {
-    1: 'lg:grid-cols-1',
-    2: 'sm:grid-cols-2 lg:grid-cols-2',
-    4: 'sm:grid-cols-4 lg:grid-cols-4',
-  };
-
-  const sectionBottomMarginVariants = {
-    48: 'mb-48',
-    72: 'mb-72',
-  };
-
-  const gridGapVariants = {
-    2: 'gap-32',
-    4: 'gap-24',
-  };
+  const bottomMargin = sectionBottomMarginVariants[section.bottomMargin];
 
   return (
-    <section className={`${sectionBottomMarginVariants[section.bottomMargin]} w-full`} style={{ maxWidth: '960px' }}>
+    <section className={bottomMargin}>
       {section.title && <h2 className="ui-text-h2">{section.title}</h2>}
       {section.description && <BodySectionDescription description={section.description} />}
       {cardsExist && (
