89d843fd98f535c625317a50a0e97203d65e5375
Peter Jones <hi@peterjones.io>
2023-10-02 13:16:40 +0100

    Make the sidebar collapsible

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,17 @@
-import React, { ReactElement } from 'react';
 import cn from 'classnames';
+import { ReactElement } from 'react';
 
 import { HighlightedMenuContext } from 'src/contexts/highlighted-menu-context';
 
-import { SidebarLinkMenu, SectionTitle } from './';
-import { SidebarData, EXPAND_MENU } from './types';
+import { SectionTitle, SidebarLinkMenu } from './';
+import { EXPAND_MENU, SidebarData } from './types';
 
-type SidebarProps = {
+import { useSidebar } from 'src/contexts/SidebarContext';
+
+import ExpandLeftIcon from './icons/ExpandLeftIcon';
+import ExpandRightIcon from './icons/ExpandRightIcon';
+
+export type SidebarProps = {
   data: SidebarData[];
   className?: string;
   indentOffset?: number;
@@ -23,32 +28,46 @@ export const Sidebar = ({
   expandableLinkMenu = true,
   expandMenu = EXPAND_MENU.EXPANDED,
 }: SidebarProps): ReactElement => {
+  const { collapsed, setCollapsed } = useSidebar();
+
+  const handleToggleSidebar = () => {
+    setCollapsed((prev) => !prev);
+  };
+
   return (
     <aside
       className={cn(
-        'transition-all fixed hidden h-screen md:block overflow-y-auto bg-extra-light-grey z-20 pt-24 pb-128 left-0 w-244',
+        'transition-all fixed hidden h-screen md:block overflow-y-auto bg-extra-light-grey z-20 left-0 w-244 pb-128',
+        { '-left-200': collapsed },
+
         className,
       )}
       data-languages={languages}
     >
-      {data.map(({ label, content }) => (
-        <div key={label} className="mb-32 px-24">
-          {label && <SectionTitle className="mb-8">{label}</SectionTitle>}
-          <HighlightedMenuContext.Consumer>
-            {(highlightedMenuId) =>
-              content && (
-                <SidebarLinkMenu
-                  data={content}
-                  expandable={expandableLinkMenu}
-                  expandMenu={expandMenu}
-                  indentOffset={indentOffset}
-                  highlightedMenuId={highlightedMenuId}
-                />
-              )
-            }
-          </HighlightedMenuContext.Consumer>
-        </div>
-      ))}
+      <div className="pt-12 pr-8 text-right bg-extra-light-grey w-244">
+        <button className="inline-block" onClick={handleToggleSidebar}>
+          {collapsed ? <ExpandRightIcon /> : <ExpandLeftIcon />}
+        </button>
+      </div>
+      {!collapsed &&
+        data.map(({ label, content }) => (
+          <div key={label} className="px-24 mb-32">
+            {label && <SectionTitle className="mb-8">{label}</SectionTitle>}
+            <HighlightedMenuContext.Consumer>
+              {(highlightedMenuId) =>
+                content && (
+                  <SidebarLinkMenu
+                    data={content}
+                    expandable={expandableLinkMenu}
+                    expandMenu={expandMenu}
+                    indentOffset={indentOffset}
+                    highlightedMenuId={highlightedMenuId}
+                  />
+                )
+              }
+            </HighlightedMenuContext.Consumer>
+          </div>
+        ))}
     </aside>
   );
 };
