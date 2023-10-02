389395a6a364577e45ad66d809902164a28b950f
Peter Jones <hi@peterjones.io>
2023-10-02 13:16:40 +0100

    Make the sidebar collapsible

---
+++ src/components/Sidebar/icons/ExpandRightIcon.tsx
@@ -0,0 +1,13 @@
+const ExpandRightIcon = () => (
+  <svg width="20" height="20" fill="none">
+    <path
+      fill="#39414E"
+      fillRule="evenodd"
+      d="M13.431 13.695a.668.668 0 0 0 .993.893l3.728-4.142a.665.665 0 0 0 .008-.907l-3.736-4.151a.668.668 0 0 0-.993.893l2.736 3.04H5.205a.668.668 0 0 0 0 1.335h10.962l-2.736 3.04Z"
+      clipRule="evenodd"
+    />
+    <path stroke="#39414E" strokeLinecap="round" strokeWidth="1.25" d="M1.667 15.583V4.353" />
+  </svg>
+);
+
+export default ExpandRightIcon;
---
+++ src/contexts/SidebarContext.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode, createContext, useContext, useState } from 'react';
+
+interface SidebarContextProps {
+  collapsed: boolean;
+  setCollapsed: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const SidebarContext = createContext<SidebarContextProps | undefined>(undefined);
+
+interface SidebarProviderProps {
+  children: ReactNode;
+}
+
+export const useSidebar = (): SidebarContextProps => {
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error('useSidebar must be used within a SidebarProvider');
+  }
+  return context;
+};
+
+export const SidebarProvider: React.FC<SidebarProviderProps> = ({ children }) => {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
+  return <SidebarContext.Provider value={{ collapsed, setCollapsed }}>{children}</SidebarContext.Provider>;
+};
