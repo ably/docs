ff54b8dc389655a474ec492402a8db7e9a00dbea
Peter Jones <hi@peterjones.io>
2023-10-03 21:57:31 +0100

    Add gatsby typing

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,6 +1,7 @@
+import type { GatsbySSR } from 'gatsby';
 import React from 'react';
 import { SidebarProvider } from './src/contexts/SidebarContext';
 
-export const wrapRootElement = ({ element }) => {
+export const wrapRootElement: GatsbySSR['wrapRootElement'] = ({ element }) => {
   return <SidebarProvider>{element}</SidebarProvider>;
 };
