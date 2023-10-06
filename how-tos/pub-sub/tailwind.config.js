5948c28549ba9002e1f0aa25e292bd3153a3ae7f
Kenneth Kalmer <kenneth.kalmer@ably.com>
2023-10-06 15:40:45 +0100

    feat: stand alone vite app for our pub-sub how to
    Borrowed heavily from ably-labs/realtime-examples as they also prepare examples
    to be shown using Sandpack. This mechanism not only helps that but improves the
    local development experience as well

---
+++ how-tos/pub-sub/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
