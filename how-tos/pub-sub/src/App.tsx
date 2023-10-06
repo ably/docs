d059bf06cd1409ba30426981e85707899f05e492
Kenneth Kalmer <kenneth.kalmer@ably.com>
2023-10-06 15:40:45 +0100

    feat: stand alone vite app for our pub-sub how to
    Borrowed heavily from ably-labs/realtime-examples as they also prepare examples
    to be shown using Sandpack. This mechanism not only helps that but improves the
    local development experience as well

---
+++ how-tos/pub-sub/src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
