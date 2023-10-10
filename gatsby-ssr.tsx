bfdab80349f6ebd02589ebcd2d8ff4c7e3ed52b3
Peter Jones <hi@peterjones.io>
2023-10-10 09:23:11 +0100

    Slightly re-work collapsible sidebar

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,12 +1,1 @@
-import { GatsbySSR } from 'gatsby';
-import React from 'react';
-import { SidebarProvider, urlsForCollapsedSidebar } from './src/contexts/SidebarContext';
 
-export const wrapPageElement: GatsbySSR['wrapPageElement'] = ({ element, props }) => {
-  const { location } = props;
-  const currentUrl = location ? location.pathname : '';
-  const shouldCollapse = urlsForCollapsedSidebar.some((url) => currentUrl.includes(url));
-  console.log(shouldCollapse);
-
-  return <SidebarProvider initialCollapsedState={shouldCollapse}>{element}</SidebarProvider>;
-};
