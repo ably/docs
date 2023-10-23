dea5720e79c2d31fa88ca4fed8f02eb263549983
Denis Sellu <denis.sellu@ably.com>
2023-10-23 18:09:16 +0100

    feat: add pub-sub for football how-to

---
+++ how-tos/pub-sub/src/footballPublisher.js
@@ -0,0 +1,32 @@
+// Import Ably
+import Ably from 'ably';
+
+// Import the function responsible for publishing football scores.
+import { publishFootballScores } from './footballGame';
+
+// Get the Ably API key from the environment variables.
+const ABLY_API_KEY = import.meta.env.VITE_ABLY_KEY;
+
+// Define the name of the Ably channel used for football scores.
+export const FOOTBALL_CHANNEL_NAME = import.meta.env.VITE_ABLY_FOOTBALL_CHANNEL_NAME;
+
+// Create an Ably client instance with the provided API key.
+const client = new Ably.Rest.Promise({
+  key: ABLY_API_KEY,
+});
+
+// Get the Ably channel associated with football scores.
+const footballChannel = client.channels.get(FOOTBALL_CHANNEL_NAME);
+
+// Async function to publish football scores to Ably.
+async function publishToAbly(footballLeague, payload) {
+  // Use Ably's publish method to send the score data to the specified league.
+  await footballChannel.publish(footballLeague, payload);
+}
+
+// Export the function for publishing football scores to Ably.
+export { publishToAbly };
+
+// Start the process of publishing football scores.
+publishFootballScores();
+
