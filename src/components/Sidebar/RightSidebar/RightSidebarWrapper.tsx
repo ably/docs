7cf812ad7160eadf648ec376955c358a9a5302db
Peter Jones <hi@peterjones.io>
2023-10-24 18:57:56 +0100

    increase top to prevent unwanted scrolling

--- src/components/Sidebar/RightSidebar/RightSidebarWrapper.tsx
+++ src/components/Sidebar/RightSidebar/RightSidebarWrapper.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import cn from 'classnames';
 import TopCodeMenu from 'src/components/Menu/TopCodeMenu';
 import { RightSidebar } from './RightSidebar';
+import { rightSidebarWrapperDesktop } from './RightSidebar.module.css';
 import { RightSidebarMobile } from './RightSidebarMobile';
 import { MenuData } from './menu-data';
 import { VersionMenuProps } from '../../Menu/VersionMenu';
-import { rightSidebarWrapper, rightSidebarWrapperDesktop } from './RightSidebar.module.css';
 import { useMediaQuery } from '@react-hook/media-query';
 
 const useScreenSize = () => {
@@ -25,7 +24,7 @@ export const RightSidebarWrapper = ({ menuData, languages, versionData }: RightS
   return (
     <div
       className={cn(
-        'fixed md:sticky top-64 left-0 right-0 bg-white flex flex-row md:flex-col justify-between md:justify-start pt-4 z-1 h-72 md:h-screen',
+        'fixed md:sticky top-112 left-0 right-0 bg-white flex flex-row md:flex-col justify-between md:justify-start z-1 h-72 md:h-screen',
         {
           [rightSidebarWrapperDesktop]: isDesktop,
         },
