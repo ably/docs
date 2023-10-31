d7795b28f190fa4caa6a5efc4c7386b11274a57f
Kenneth Kalmer <kenneth.kalmer@ably.com>
2023-10-31 16:12:39 +0000

    feat: rewrite the how-to code before passing to sandpack
    This allows us to get working API keys and channel names into the demo, as well
    as working with different realtime environments too

--- src/pages/docs/how-to/pub-sub.tsx
+++ src/pages/docs/how-to/pub-sub.tsx
@@ -5,8 +5,55 @@ import HowTo from 'HowTos/pub-sub/how-to.mdx';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Head } from 'src/components/Head';
 import Layout from 'src/components/Layout';
-import PageTitle from 'src/components/PageTitle';
+import { MarkdownProvider } from 'src/components/Markdown';
 import { SidebarProvider } from 'src/contexts/SidebarContext';
+import UserContext, { UserApiKey } from 'src/contexts/user-context';
+import { DOCUMENTATION_NAME } from '../../../../data/transform/constants';
+import { useContext } from 'react';
+import { getRandomChannelName } from 'src/components/blocks/software/Code/get-random-channel-name';
+
+const getApiKey = (userApiKeys: UserApiKey[]) => {
+  const app = userApiKeys[0];
+  if (!app) {
+    return 'Waiting...';
+  }
+
+  return app.apiKeys[0].whole_key;
+};
+
+const ablyEnvironment = process.env.GATSBY_ABLY_ENVIRONMENT;
+const basketballChannelName = getRandomChannelName();
+const footballChannelName = getRandomChannelName();
+
+const updateAblyConnectionKey = (files: Record<string, string>, userApiKeys: UserApiKey[]) => {
+  const names = Object.keys(files);
+  const apiKey = getApiKey(userApiKeys);
+
+  return names.reduce((acc, name: string) => {
+    let content = files[name];
+
+    // Environment
+    if (ablyEnvironment !== 'production') {
+      content = content.replaceAll(/new Ably\.(Realtime|Rest)\.Promise\(\{/g, (_match, type) => {
+        return `new Ably.${type}.Promise({\n  environment: '${ablyEnvironment}',`;
+      });
+    }
+
+    // API Key
+    content = content.replaceAll('import.meta.env.VITE_ABLY_KEY', `"${apiKey}"`);
+
+    // Channel names
+    content = content.replaceAll('import.meta.env.VITE_BASKETBALL_CHANNEL_NAME', `"${basketballChannelName}"`);
+    content = content.replaceAll('import.meta.env.VITE_FOOTBALL_CHANNEL_NAME', `"${footballChannelName}"`);
+
+    return { ...acc, [name]: content };
+  }, {});
+};
+
+interface HowToFile {
+  srcPath: string;
+  content: string;
+}
 
 const PubSubHowTo = () => {
   const meta_description = `A fine interactve how to`;
@@ -22,15 +69,18 @@ const PubSubHowTo = () => {
     }
   `);
 
-  const files = {};
-  data.files.nodes.forEach((fileNode) => {
-    const { srcPath, content } = fileNode;
-    files[srcPath] = content;
-  });
+  const files = data.files.nodes.reduce((acc: object, file: HowToFile) => {
+    const { srcPath, content } = file;
+    return { ...acc, [srcPath]: content };
+  }, {});
+
+  const userData = useContext(UserContext);
+  const apiKeys = userData.apiKeys.data;
+  const hasApiKeys = apiKeys.length > 0;
 
   return (
     <>
-      <Head title="Pub/Sub How To" description={meta_description} />
+      <Head title="Pub/Sub How To" canonical={`/${DOCUMENTATION_NAME}/how-to/pub-sub`} description={meta_description} />
 
       <SidebarProvider initialCollapsedState={true}>
         <Layout showProductNavigation={true} currentProduct="channels" collapsibleSidebar={true} isExtraWide>
@@ -42,32 +92,36 @@ const PubSubHowTo = () => {
               </div>
             </div>
             <aside className="pt-24 pl-24">
-              <SandpackProvider
-                files={files}
-                customSetup={{
-                  dependencies: {
-                    ably: 'latest',
-                  },
-                }}
-                options={{
-                  visibleFiles: ['/App.tsx', '/index.css', '/index.tsx', '/components/Card.tsx'],
-                  autorun: true,
-                  autoReload: false,
-                }}
-                theme={sandpackTheme}
-                template="react-ts"
-              >
-                <CodeEditor
-                  editor={{
-                    showLineNumbers: true,
+              {hasApiKeys ? (
+                <SandpackProvider
+                  files={updateAblyConnectionKey(files, apiKeys)}
+                  customSetup={{
+                    dependencies: {
+                      ably: 'latest',
+                    },
                   }}
-                />
+                  options={{
+                    visibleFiles: ['/App.tsx', '/index.css', '/index.tsx', '/components/Scoreboard.tsx'],
+                    autorun: true,
+                    autoReload: false,
+                  }}
+                  theme={sandpackTheme}
+                  template="react-ts"
+                >
+                  <CodeEditor
+                    editor={{
+                      showLineNumbers: true,
+                    }}
+                  />
 
-                <div className="flex gap-16 my-16">
-                  <SandpackPreview style={{ height: '420px' }} />
-                  <SandpackPreview style={{ height: '420px' }} />
-                </div>
-              </SandpackProvider>
+                  <div className="flex gap-16 my-16">
+                    <SandpackPreview style={{ height: '420px' }} />
+                    <SandpackPreview style={{ height: '420px' }} />
+                  </div>
+                </SandpackProvider>
+              ) : (
+                <b>Loading...</b>
+              )}
             </aside>
           </article>
         </Layout>
