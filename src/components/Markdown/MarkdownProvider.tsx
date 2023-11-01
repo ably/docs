e4f7c53f7f639e1161a27087c4cc4573ba8a9431
Kenneth Kalmer <kenneth.kalmer@ably.com>
2023-11-01 15:01:52 +0000

    chore: use MarkdownProvider when rendering how-to code

--- src/components/Markdown/MarkdownProvider.tsx
+++ src/components/Markdown/MarkdownProvider.tsx
@@ -1,38 +1,37 @@
 import React, { FC } from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { HtmlAttributes } from 'src/components/html-component-props';
 
-const H1: FC<HtmlAttributes<'h1'>> = ({ children, ...props }) => (
+const H1: FC<JSX.IntrinsicElements['h1']> = ({ children, ...props }) => (
   <h1 className="ui-text-h1 my-40" {...props}>
     {children}
   </h1>
 );
 
-const H2: FC<HtmlAttributes<'h2'>> = ({ children, ...props }) => (
+const H2: FC<JSX.IntrinsicElements['h2']> = ({ children, ...props }) => (
   <h2 className="ui-text-h2 my-32" {...props}>
     {children}
   </h2>
 );
 
-const H3: FC<HtmlAttributes<'h3'>> = ({ children, ...props }) => (
+const H3: FC<JSX.IntrinsicElements['h3']> = ({ children, ...props }) => (
   <h3 className="ui-text-h3 my-24" {...props}>
     {children}
   </h3>
 );
 
-const Paragraph: FC<HtmlAttributes<'p'>> = ({ children, ...props }) => (
+const Paragraph: FC<JSX.IntrinsicElements['p']> = ({ children, ...props }) => (
   <p className="text-p2 mb-24 leading-relaxed" {...props}>
     {children}
   </p>
 );
 
-const Ul: FC<HtmlAttributes<'ul'>> = ({ children, ...props }) => (
+const Ul: FC<JSX.IntrinsicElements['ul']> = ({ children, ...props }) => (
   <ul className="ui-text-p2 pl-16 pb-32 list-disc" {...props}>
     {children}
   </ul>
 );
 
-const Li: FC<HtmlAttributes<'li'>> = ({ children, ...props }) => (
+const Li: FC<JSX.IntrinsicElements['li']> = ({ children, ...props }) => (
   <li className="ui-text-p2" {...props}>
     {children}
   </li>
@@ -47,4 +46,6 @@ const components = {
   li: Li,
 };
 
-export const MarkdownProvider = ({ children }) => <MDXProvider components={components}>{children}</MDXProvider>;
+export const MarkdownProvider = ({ children }: { children: React.ReactNode }) => (
+  <MDXProvider components={components}>{children}</MDXProvider>
+);
