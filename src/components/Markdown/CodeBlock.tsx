2c3a89e879a8e596e320d51f4a1f8c113f28eac2
Kenneth Kalmer <kenneth.kalmer@ably.com>
2023-11-02 17:19:14 +0000

    feat: copy code button for markdown CodeBlock component

--- src/components/Markdown/CodeBlock.tsx
+++ src/components/Markdown/CodeBlock.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useMemo } from 'react';
 import DOMPurify from 'dompurify';
+import Icon from '@ably/ui/core/Icon';
 import { highlightSnippet, registerDefaultLanguages } from '@ably/ui/src/core/utils/syntax-highlighter';
 import languagesRegistry from '@ably/ui/src/core/utils/syntax-highlighter-registry';
 
@@ -7,18 +8,31 @@ import '@ably/ui/src/core/utils/syntax-highlighter.css';
 
 registerDefaultLanguages(languagesRegistry);
 
+import { ButtonWithTooltip } from 'src/components';
+import { safeWindow } from 'src/utilities';
+import { copyCodeBlockContentTracker } from 'src/third-party/gtm/gtm';
+
 export const CodeBlock: FC<{ children: React.ReactNode; language: string }> = ({
   children,
   language = 'javascript',
 }) => {
+  const content = children.props.children; // hack-ish, but we get the content
   const highlightedContent = useMemo(() => {
-    const content = children.props.children; // hack-ish, but we get the content
     return highlightSnippet(language, content);
-  }, [language, children]);
+  }, [language, content]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(content);
+    const page = safeWindow.location.pathname;
+    const contentIdentifier = content.slice(0, 10);
+    copyCodeBlockContentTracker({
+      copyCodeBlockContent: `docs - ${language} ${page} ${contentIdentifier}`,
+    });
+  };
 
   return (
-    <pre className="bg-cool-black text-white p-0 rounded-lg relative overflow-hidden">
-      <div className="overflow-auto relative p-16">
+    <pre className="bg-cool-black text-white p-0 rounded-lg relative">
+      <div className="overflow-auto relative p-16 pr-32">
         <code
           className="ui-text-code"
           style={{ whiteSpace: 'pre-wrap' }}
@@ -33,6 +47,11 @@ export const CodeBlock: FC<{ children: React.ReactNode; language: string }> = ({
               : highlightedContent,
           }}
         />
+      </div>
+      <div className="absolute top-16 right-8">
+        <ButtonWithTooltip tooltip="Copy" notification="Copied!" onClick={handleCopy} className="text-white">
+          <Icon name="icon-gui-copy" size="1rem" color="white" />
+        </ButtonWithTooltip>
       </div>
     </pre>
   );
