59586e081d987010a4bf3ca29c5e2068091a092b
Kenneth Kalmer <kenneth.kalmer@ably.com>
2023-11-07 16:53:14 +0000

    chore: add sandpack CSS to Gatsby

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,7 +1,21 @@
+import { getSandpackCssText } from '@codesandbox/sandpack-react';
+
+const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <style
+      id="sandpack"
+      key="sandpack-css"
+      dangerouslySetInnerHTML={{
+        __html: getSandpackCssText(),
+      }}
+    />,
+  ]);
+};
+
 /**
  * Load our user state
  */
 import UserContextWrapper from 'src/contexts/user-context/wrap-with-provider';
 const wrapRootElement = UserContextWrapper;
 
-export { wrapRootElement };
+export { onRenderBody, wrapRootElement };
