1d1b69543508d1318b5bc097185b985a6eba2f02
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-01-10 10:57:52 +0000

    fix: update the footer links to work with `pathPrefix`
    Docs links have been updated to use Gatsby's `withPrefix` function and external
    links in the page furniture have been updated as well. Internal links in the
    footer now uses Gatsby's `Link` component for improved navigation speed as well.

--- src/components/Footer/FooterBottomNav/FooterBottomNavItem.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FooterMenuItemType } from '../FooterTopNav/FooterMenu/FooterMenu';
-
-export const FooterBottomNavItem = ({ label, link }: FooterMenuItemType) => (
-  <a key={label} className="ui-footer-link" href={link}>
-    {label}
-  </a>
-);
