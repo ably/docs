7512f887cb6bd3cfabaaed1aa17fc664623b6747
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-01-16 15:00:58 +0000

    cleanup: remove some conflicting CSS classes

--- src/components/ProductPage/BodySection/Card/QuickstartCard.tsx
+++ src/components/ProductPage/BodySection/Card/QuickstartCard.tsx
@@ -4,14 +4,14 @@ import { StaticImage } from '../../../StaticImage';
 import { Links } from './Links';
 
 export const QuickstartCard = ({ title, content, image, links }: CardProps) => (
-  <div className="p-24 border border-extra-light-grey rounded-lg bg-extra-light-grey flex flex-col h-full justify-between block hover:border-mid-grey hover:text-cool-black group">
+  <div className="p-24 border border-extra-light-grey rounded-lg bg-extra-light-grey flex flex-col h-full justify-between hover:border-mid-grey hover:text-cool-black group">
     <div className="flex items-center">
       <StaticImage src={`/images/products/${image}`} alt={`${title}`} width="32px" />
       <h2 className="ml-8" style={{ fontSize: '1.25rem' }}>
         {title}
       </h2>
     </div>
-    <p style={{ fontSize: '1rem', width: '60%' }} className="text-lg text-dark-grey mt-8 leading-6 mb-40 mt-12">
+    <p style={{ fontSize: '1rem', width: '60%' }} className="text-lg text-dark-grey leading-6 mb-40 mt-12">
       {content}
     </p>
 
