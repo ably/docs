932a13399030f9fd3b2d2545ab9146449e846e44
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-01-17 23:46:22 +0000

    fix: handle broken links from gatsby-plugin-mdx
    Inspired by a solution posted on gatsbyjs/gatsby#21462, we fix broken links from
    gatsby-plugin-mdx when building with a `assetPrefix`, by removing those broken
    prefixes from the URL before passing it on to our `Link` component.

--- src/components/Markdown/MarkdownProvider.tsx
+++ src/components/Markdown/MarkdownProvider.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import { MDXProvider } from '@mdx-js/react';
 import Link from 'src/components/Link';
 import { CodeBlock } from './CodeBlock';
@@ -27,11 +28,38 @@ const Paragraph: FC<JSX.IntrinsicElements['p']> = ({ children, ...props }) => (
   </p>
 );
 
-const Anchor: FC<JSX.IntrinsicElements['a']> = ({ children, href, ...props }) => (
-  <Link to={href!} className="docs-link" {...props}>
-    {children}
-  </Link>
-);
+export const Anchor: FC<JSX.IntrinsicElements['a']> = ({ children, href, ...props }) => {
+  /**
+   * Inspired by https://github.com/gatsbyjs/gatsby/issues/21462#issuecomment-605606702
+   * to work around the issues with broken links being emitted by gatsby-plugin-mdx when
+   * specifying an assetPrefix (like we do in production). So what we do is we "break"
+   * the asset prefix pretty much like gatsby-plugin-mdx does [1], and if we find the
+   * broken prefix on the URL we strip it off again...
+   *
+   * 1. https://github.com/gatsbyjs/gatsby/blob/3d4d6a6e222cf3bff3f2c2cdfb0cc539bad2403a/packages/gatsby-plugin-mdx/src/remark-path-prefix-plugin.ts#L18-L28
+   */
+  const { site } = useStaticQuery(graphql`
+    {
+      site {
+        assetPrefix
+      }
+    }
+  `);
+
+  let cleanHref = href;
+  const assetPrefix = site.assetPrefix ?? '';
+  const brokenAssetPrefix = assetPrefix.replace('://', ':/');
+
+  if (href.startsWith(brokenAssetPrefix)) {
+    cleanHref = href.slice(brokenAssetPrefix.length);
+  }
+
+  return (
+    <Link to={cleanHref} className="docs-link" {...props}>
+      {children}
+    </Link>
+  );
+};
 
 const Ul: FC<JSX.IntrinsicElements['ul']> = ({ children, ...props }) => (
   <ul className="ui-unordered-list" {...props}>
