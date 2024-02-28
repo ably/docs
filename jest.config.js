c1de462014cd05fe88792af9a9b1421da7b6bd38
Aleksandar Ristic <aristic665@gmail.com>
2024-02-28 14:32:01 +0100

    fix: add @reach/router path in jest and ts

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
     '^src/(.*)$': '<rootDir>/src/$1',
+    '^@reach/router(.*)': `<rootDir>/node_modules/@gatsbyjs/reach-router$1`,
   },
   coveragePathIgnorePatterns: ['src/styles/svg'],
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
