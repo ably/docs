5d50e024685d88466da3498136facdf74536714f
Jamie Henson <jamie.henson@ably.com>
2024-03-05 14:58:28 +0000

    fix: make StaticImage assetPrefix & pathPrefix aware

--- src/components/StaticImage.tsx
+++ src/components/StaticImage.tsx
@@ -1,7 +1,17 @@
-import React, { ComponentProps } from 'react';
+import React from 'react';
+import { graphql, useStaticQuery, withPrefix } from 'gatsby';
+import { ComponentProps } from 'react';
 
-type StaticImageProps = ComponentProps<'img'> & {
-  src: string;
+export const StaticImage = ({ src, ...attribs }: ComponentProps<'img'>) => {
+  const result = useStaticQuery(graphql`
+    query AssetPrefixQuery {
+      site {
+        assetPrefix
+      }
+    }
+  `);
+  const assetPrefix = result?.site.assetPrefix ?? '';
+  const srcUrl = `${assetPrefix}${withPrefix(src)}`;
+
+  return <img src={srcUrl} {...attribs} data-testid="static-image" />;
 };
-
-export const StaticImage = ({ src, ...attribs }: StaticImageProps) => <img src={src} {...attribs} />;
