2fd247f81800607dfd188a4ceeb4c505a0970075
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-03-07 09:34:55 +0000

    chore: rework HamburderMenu to import images

--- src/components/Header/HamburgerMenu/HamburgerButton/HamburgerIcon.tsx
+++ src/components/Header/HamburgerMenu/HamburgerButton/HamburgerIcon.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StaticImage } from 'src/components/StaticImage';
+
+import hamburgerOpen from 'src/images/icons/hamburger-is-open.svg';
+import hamburgerClosed from 'src/images/icons/hamburger-is-closed.svg';
 
 const MENU_DESCRIPTION = 'Site navigation menu';
 
-export const HamburgerIcon = ({ isOpen }: { isOpen: boolean }) =>
-  isOpen ? (
-    <StaticImage className="min-w-24" src={'/images/icons/hamburger-is-open.svg'} alt={MENU_DESCRIPTION} />
-  ) : (
-    <StaticImage className="min-w-24" alt={MENU_DESCRIPTION} src="/images/icons/hamburger-is-closed.svg" />
-  );
+export const HamburgerIcon = ({ isOpen }: { isOpen: boolean }) => (
+  <img className="min-w-24" src={isOpen ? hamburgerOpen : hamburgerClosed} alt={MENU_DESCRIPTION} />
+);
