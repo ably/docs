68eecd1fd6bd1b8cb4c07ed73e337b27300caeb8
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-03-12 15:29:44 +0000

    feat: update useSiteMetadata hook to return a canonicalUrl function

--- src/hooks/use-site-metadata.ts
+++ src/hooks/use-site-metadata.ts
@@ -1,4 +1,10 @@
-import { graphql, useStaticQuery } from 'gatsby';
+import { graphql, withPrefix, useStaticQuery } from 'gatsby';
+
+const stripTrailingSlash = (str: string) => (str.endsWith('/') ? str.slice(0, -1) : str);
+
+const canonicalUrl = (siteUrl: string): ((path: string) => string) => {
+  return (path: string): string => stripTrailingSlash(`${siteUrl}${withPrefix(path)}`);
+};
 
 export const useSiteMetadata = () => {
   const data = useStaticQuery(graphql`
@@ -12,5 +18,10 @@ export const useSiteMetadata = () => {
     }
   `);
 
-  return data.site.siteMetadata;
+  const siteMetadata = data.site.siteMetadata;
+
+  return {
+    ...siteMetadata,
+    canonicalUrl: canonicalUrl(siteMetadata.siteUrl),
+  };
 };
