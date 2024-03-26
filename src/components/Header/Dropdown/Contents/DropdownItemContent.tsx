a25985be2720666ebcfe44fb41da99f74b18891a
Kenneth Kalmer <kenneth.kalmer@ably.com>
2024-03-26 14:52:34 +0000

    fix: use our own `navigate` in top dropdown menu

--- src/components/Header/Dropdown/Contents/DropdownItemContent.tsx
+++ src/components/Header/Dropdown/Contents/DropdownItemContent.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { navigate, withPrefix } from 'gatsby';
-import { checkLinkIsInternal } from 'src/utilities/link-checks';
+import { navigate } from 'src/components/Link';
 import { ContentDescription } from './ContentDescription';
 import { ContentLink } from './ContentLink';
 import { Content } from './types';
 
 export const DropdownItemContent = ({ content: { link, description } }: { content: Content }) => {
-  const isInternal = checkLinkIsInternal(link.href);
-  const href = isInternal && link.external ? withPrefix(link.href) : link.href;
-
   return (
     <li
       className="flex flex-col hover:bg-light-grey p-8 mr-16 -ml-8 rounded-md cursor-pointer group"
-      onClick={() => navigate(href)}
-      onKeyDown={(event) => event.key === 'Enter' && navigate(href)}
+      onClick={() => navigate(link.href, { external: link.external })}
+      onKeyDown={(event) => event.key === 'Enter' && navigate(link.href, { external: link.external })}
       tabIndex={0}
     >
       <ContentLink href={link.href} rel={link.rel} text={link.text} external={link.external} />
