8859ad6e989e1c32b0d7354072d194a693f31bdf
Aleksandar Ristic <aristic665@gmail.com>
2024-05-28 17:00:55 +0200

    chore: add correct font weights

--- src/components/ProductPage/BodySection/Card/TutorialCard.tsx
+++ src/components/ProductPage/BodySection/Card/TutorialCard.tsx
@@ -6,7 +6,7 @@ export const TutorialCard = ({ title, image, links }: CardProps) => (
   <div className="p-24 border border-extra-light-grey rounded-lg bg-extra-light-grey flex flex-col h-full justify-between hover:border-mid-grey hover:text-cool-black group">
     <div className="flex mb-20">
       <Image image={image} alt={title} width="32px" />
-      <h2 className="ml-8" style={{ fontSize: '1rem' }}>
+      <h2 className="ml-8 font-extrabold" style={{ fontSize: '1rem' }}>
         {title}
       </h2>
     </div>
