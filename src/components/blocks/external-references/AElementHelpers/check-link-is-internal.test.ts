83bf38a866f4cc8a03afe197941555a66f768a9a
matt423 <matthew.a423@gmail.com>
2024-08-27 10:11:26 +0100

    fix: Remove control-api from being an external link
    Since PR #2247, we now host the control-api page directly within the docs repo.
    Marking the link as internal will now let the page render without the need for redirect

--- src/components/blocks/external-references/AElementHelpers/check-link-is-internal.test.ts
+++ src/components/blocks/external-references/AElementHelpers/check-link-is-internal.test.ts
@@ -5,29 +5,12 @@ describe('Check link is internal: unit tests', () => {
   it('Asserts that an internal link is internal', () => {
     expect(checkLinkIsInternal('/docs/api/sse')).toBe(true);
   });
-  it('Asserts that an control API link is not internal', () => {
-    expect(checkLinkIsInternal('/docs/api/control-api')).toBe(false);
-  });
 });
 
-const passingRelativeLinks = webPath()
-  .filter((webPath) => !!webPath)
-  .filter((webPath) => !webPath.includes('/api/control-api'));
-
+const passingRelativeLinks = webPath().filter((webPath) => !!webPath);
 const nonAblyAbsoluteLinks = webUrl()
   .filter((webUrl) => !webUrl.startsWith('https://ably.com'))
   .filter((webUrl) => !webUrl.startsWith('https://www.ably.com'));
-
-const controlAPIPath = tuple(
-  webPath(),
-  constantFrom(
-    'https://ably.com/docs/',
-    'http://ably.com/docs/',
-    'https://www.ably.com/docs/',
-    'http://www.ably.com/docs/',
-    '/',
-  ),
-).map(([webPath, validInternalPrefix]) => `${validInternalPrefix}api/control-api${webPath}`);
 
 describe('Check link is internal: Property tests', () => {
   it('Always identifies web paths as internal', () => {
@@ -47,12 +30,5 @@ describe('Check link is internal: Property tests', () => {
   it('Always identifies an empty link as not being internal', () => {
     expect(checkLinkIsInternal('')).toBe(false);
     expect(checkLinkIsInternal()).toBe(false);
-  });
-  it('Always identifies the control API link and children as not being internal', () => {
-    assert(
-      property(controlAPIPath, (link) => {
-        expect(checkLinkIsInternal(link)).toBe(false);
-      }),
-    );
   });
 });
