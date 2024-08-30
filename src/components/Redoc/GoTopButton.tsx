16f0a5730eda8cbe4de9c6889adce530eae8ef1a
April Smith <april.smith@ably.com>
2024-08-30 13:13:00 +0100

    Refactor Scroll event inside UseEffect

--- src/components/Redoc/GoTopButton.tsx
+++ src/components/Redoc/GoTopButton.tsx
@@ -3,22 +3,24 @@ import Icon from '@ably/ui/core/Icon';
 
 export const GoTopButton = () => {
   const [showGoTop, setShowGoTop] = useState(false);
-
-  const handleVisibleButton = () => {
-    setShowGoTop(window.scrollY > 20);
-  };
+  const scrollOffset = 20;
 
   const handleScrollUp = () => {
-    window.scrollTo({ left: 0, top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
   useEffect(() => {
+    const handleVisibleButton = () => {
+      setShowGoTop(window.scrollY > scrollOffset);
+    };
+
     window.addEventListener('scroll', handleVisibleButton);
+    return () => window.removeEventListener('scroll', handleVisibleButton);
   }, []);
 
   return (
     <div className={showGoTop ? 'block' : 'hidden'} onClick={handleScrollUp}>
-      <div className="rounded text-white py-8 px-8 bg-cool-black bottom-0 left-0 fixed m-16 z-20 cursor-pointer">
+      <div className="rounded text-white py-8 px-8 bg-cool-black fixed bottom-0 left-0 m-16 z-20 cursor-pointer">
         <Icon
           name="icon-gui-link-arrow"
           size="1.5rem"
