4181a4f1a1f1cac992ce74cb02de2f9e0532622f
Greg Holmes <iam@gregholmes.co.uk>
2024-10-03 09:50:43 +0100

    Add typing indicator with chat example

---
+++ examples/chat-typing-indicator/react/README.md
@@ -0,0 +1,22 @@
+# Typing indicator example with Ably Chat in React
+
+This folder contains the code for the Chat typing indicator (React) - a demo of how you can leverage [Ably Chat](https://ably.com/docs/chat) to track people typing in an input.
+
+## Getting started
+
+First, in `/examples/chat-typing-indicator/react/` rename `.env.example` to `.env.local` and enter your Ably API KEY in the `NEXT_PUBLIC_ABLY_KEY` environment variable.
+
+Change directory to `/examples/chat-typing-indicator/react` and install dependencies:
+
+```bash
+yarn install
+```
+
+Run the development server:
+
+```bash
+yarn dev
+```
+
+Open two tabs to [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
---
+++ examples/chat-typing-indicator/react/src/app/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useChatClient, useRoom, useTyping } from '@ably/chat/react';
+import '../../styles/styles.css'
+
+const Loading = () => <div>Loading...</div>;
+
+const ChatInput = () => {
+  const {start, currentlyTyping } = useTyping();
+  const { clientId } = useChatClient();
+
+  const typingClientIds = Array.from(currentlyTyping).filter((id) => id !== clientId);
+  const clientsTyping = typingClientIds.join(' and ');
+  const typingIndicatorText = clientsTyping ? `${clientsTyping} ${typingClientIds.length === 1 ? 'is' : 'are'} typing` : '';
+
+  return (
+    <div id="typing-indicator" className="container">
+      <div className="inner">
+        <input
+          type="text"
+          id="user-input"
+          placeholder="Start typing..."
+          onKeyDown={start}
+        />
+        <label id="user-input-label" htmlFor="user-input">
+          {typingIndicatorText}
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default function ChatApp() {
+  const { roomStatus, connectionStatus } = useRoom();
+
+  if (roomStatus !== 'attached' || connectionStatus !== 'connected') {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <ChatInput />
+    </div>
+  );
+}
