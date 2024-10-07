f66c03db631a54506d0f240a2a07cd67baf7493c
Greg Holmes <iam@gregholmes.co.uk>
2024-10-07 11:59:33 +0100

    fixup! Add typing indicator with chat example

--- examples/chat-typing-indicator/react/src/components/ChatInput.tsx
+++ examples/chat-typing-indicator/react/src/components/ChatInput.tsx
@@ -1,35 +1,8 @@
-import { useState, useCallback, useEffect } from 'react';
-import { useTyping } from '@ably/chat/react';
+import { useTyping, } from '@ably/chat/react';
 import { TypingEvent } from '@ably/chat';
 
 export const ChatInput = () => {
-  const [isTyping, setIsTyping] = useState(false);
-
-  const {start, stop, currentlyTyping } = useTyping({
-    listener: (typingEvent: TypingEvent) => {
-      console.log('Typing event received: ', typingEvent);
-    },
-  });
-
-  const handleTyping = useCallback(() => {
-    if (!isTyping) {
-      setIsTyping(true);
-      start();
-    }
-
-    const timer = setTimeout(() => {
-      setIsTyping(false);
-      stop();
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, [isTyping, start, stop]);
-
-  useEffect(() => {
-    if (!isTyping) {
-      stop();
-    }
-  }, [isTyping, stop]);
+  const {start, currentlyTyping } = useTyping();
 
   let clientsTyping = '';
   const clientIdsArray = Array.from(currentlyTyping);
@@ -52,7 +25,7 @@ export const ChatInput = () => {
           type="text"
           id="user-input"
           placeholder="Start typing..."
-          onKeyDown={handleTyping}
+          onKeyDown={start}
         />
         <label id="user-input-label" htmlFor="user-input">
           {typingIndicatorText}
