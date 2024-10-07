d129351d6dfb8b7bd9315a03c3f8c132a29ccf83
Greg Holmes <iam@gregholmes.co.uk>
2024-10-07 11:24:56 +0100

    fixup! Add typing indicator with chat example

--- examples/chat-typing-indicator/react/src/app/page.tsx
+++ examples/chat-typing-indicator/react/src/app/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState, useCallback, useEffect } from 'react';
-import { useTyping, useRoom } from '@ably/chat/react';
-import { TypingEvent } from '@ably/chat';
+import { useRoom } from '@ably/chat/react';
+import { ChatInput } from '../components/ChatInput';
 import '../../public/styles/styles.css'
 
 export default function Home() {
@@ -14,67 +13,7 @@ export default function Home() {
 
   return (
     <div>
-      <Chat />
-    </div>
-  );
-}
-
-const Chat = () => {
-  const [isTyping, setIsTyping] = useState(false);
-
-  const {start, stop, currentlyTyping } = useTyping({
-    listener: (typingEvent: TypingEvent) => {
-      console.log('Typing event received: ', typingEvent);
-    },
-  });
-
-  const handleTyping = useCallback(() => {
-    if (!isTyping) {
-      setIsTyping(true);
-      start();
-    }
-
-    const timer = setTimeout(() => {
-      setIsTyping(false);
-      stop();
-    }, 5000);
-
-    return () => clearTimeout(timer);
-  }, [isTyping, start, stop]);
-
-  useEffect(() => {
-    if (!isTyping) {
-      stop();
-    }
-  }, [isTyping, stop]);
-
-  let clientsTyping = '';
-  const clientIdsArray = Array.from(currentlyTyping);
-
-  for (let i = 0; i < clientIdsArray.length; i++) {
-    clientsTyping += clientIdsArray[i];
-    if (i !== clientIdsArray.length - 1) {
-      clientsTyping += ' and ';
-    }
-  }
-
-  const typingIndicatorText = clientIdsArray.length > 0
-    ? `${clientsTyping} ${clientIdsArray.length === 1 ? 'is' : 'are'} typing`
-    : '';
-
-  return (
-    <div id="typing-indicator" className="container">
-      <div className="inner">
-        <input
-          type="text"
-          id="user-input"
-          placeholder="Start typing..."
-          onKeyDown={handleTyping}
-        />
-        <label id="user-input-label" htmlFor="user-input">
-          {typingIndicatorText}
-        </label>
-      </div>
+      <ChatInput />
     </div>
   );
 }
