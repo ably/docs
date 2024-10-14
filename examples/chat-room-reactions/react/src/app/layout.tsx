e348792391a5a945f00e1508f3f181a981f34a21
Greg Holmes <iam@gregholmes.co.uk>
2024-10-14 15:47:30 +0100

    Add chat-room-reactions both React and Typescript to examples

---
+++ examples/chat-room-reactions/react/src/app/layout.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import { ChatClientProvider, ChatRoomProvider } from '@ably/chat/react';
+import { Realtime } from 'ably';
+import { ChatClient, RoomOptionsDefaults } from '@ably/chat';
+import '../../styles/global.css'
+
+const inter = Inter({ subsets: ["latin"] });
+
+const mockNames = ['Bob', 'Jane', 'John', 'Sammy'];
+const mockName = () => mockNames[Math.floor(Math.random() * mockNames.length)];
+
+const realtimeClient = new Realtime({key: process.env.NEXT_PUBLIC_ABLY_KEY, clientId: mockName()});
+const chatClient = new ChatClient(realtimeClient);
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ChatClientProvider client={chatClient}>
+          <ChatRoomProvider
+            id="chat-room-reactions"
+            options={RoomOptionsDefaults}
+          >
+            {children}
+          </ChatRoomProvider>
+        </ChatClientProvider>
+      </body>
+    </html>
+  );
+}
