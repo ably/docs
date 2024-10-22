8c4d5ef6c9d8a6a929c79c1367525c091b8520ee
Greg Holmes <iam@gregholmes.co.uk>
2024-10-22 16:02:35 +0100

    Add chat online status example

---
+++ examples/chat-online-status/react/src/app/layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import { ChatClientProvider, ChatRoomProvider } from '@ably/chat/react';
+import { Realtime } from 'ably';
+import { ChatClient, RoomOptionsDefaults } from '@ably/chat';
+import { faker } from '@faker-js/faker';
+import '../../styles/styles.css';
+
+const inter = Inter({ subsets: ["latin"] });
+const roomName = 'chat-online-status';
+
+const realtimeClient = new Realtime({key: process.env.NEXT_PUBLIC_ABLY_KEY, clientId: faker.person.firstName()});
+const chatClient = new ChatClient(realtimeClient);
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ChatClientProvider client={chatClient}>
+          <ChatRoomProvider
+            id={roomName}
+            options={RoomOptionsDefaults}
+          >
+            {children}
+          </ChatRoomProvider>
+        </ChatClientProvider>
+      </body>
+    </html>
+  );
+}
