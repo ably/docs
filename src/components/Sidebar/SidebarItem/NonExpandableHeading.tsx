6bd175de8d1f4211118069448e440f909190d82e
Jamie Henson <jamie.henson@ably.com>
2024-11-04 17:07:53 +0000

    feat: remove old sidebar logic and introduce early simplified successor

--- src/components/Sidebar/SidebarItem/NonExpandableHeading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ROOT_LEVEL } from '../consts';
-import { AccordionHeadingProps } from './ExpandableAccordionHeading';
-
-export const NonExpandableHeading = ({ label, level }: AccordionHeadingProps) => (
-  <div aria-level={level || ROOT_LEVEL}>{label}</div>
-);
--- src/components/Sidebar/consts.ts
+++
@@ -1,2 +0,0 @@
-export const ROOT_LEVEL = 3;
-export const MAX_LEVEL = 6;
