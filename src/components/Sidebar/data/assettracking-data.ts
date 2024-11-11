182d20e08b14b16824e404b3d55890a8161c5d33
Jamie Henson <jamie.henson@ably.com>
2024-11-11 14:22:23 +0000

    chore: migrate sidebar YAML data to JSON

---
+++ src/components/Sidebar/data/assettracking-data.ts
@@ -0,0 +1,28 @@
+import { NavProduct } from '../types';
+
+export default {
+  name: 'Asset Tracking',
+  icon: {
+    closed: 'icon-product-asset-tracking-mono',
+    open: 'icon-product-asset-tracking',
+  },
+  content: [
+    {
+      name: 'Asset Tracking',
+      pages: [
+        {
+          name: 'What is Asset Tracking?',
+          link: '/asset-tracking',
+        },
+        {
+          name: 'Using the example apps',
+          link: '/asset-tracking/example-apps',
+        },
+        {
+          name: 'Using the SDKs',
+          link: '/asset-tracking/using-the-sdks',
+        },
+      ],
+    },
+  ],
+} satisfies NavProduct;
