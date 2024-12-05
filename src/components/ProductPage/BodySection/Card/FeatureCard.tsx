911f7100feeea81588f1e9caa545cf9d41415b5f
Jamie Henson <jamie.henson@ably.com>
2024-12-05 10:58:24 +0000

    chore: refactor FeatureCard image heights and alignment

--- src/components/ProductPage/BodySection/Card/FeatureCard.tsx
+++ src/components/ProductPage/BodySection/Card/FeatureCard.tsx
@@ -6,17 +6,15 @@ import { navigate } from '../../../Link';
 export const FeatureCard = ({ title, content, image, links }: CardProps) => (
   <div
     onClick={() => navigate(links?.[0]?.href || '#')}
-    className="cursor-pointer border border-extra-light-grey rounded-lg bg-extra-light-grey flex sm:flex-col lg:flex-row hover:border-mid-grey hover:text-cool-black group"
+    className="cursor-pointer border border-extra-light-grey rounded-lg bg-extra-light-grey flex flex-col lg:flex-row hover:border-mid-grey hover:text-cool-black group p-24 gap-24"
   >
-    <div className="p-24 flex">
-      <div className="flex flex-col">
-        <div>
-          <h3 className="ui-text-h3">{title}</h3>
-          <p className="ui-text-p2 text-neutral-800 mt-8 mb-40">{content}</p>
-        </div>
-        {links?.length ? <Links links={links} /> : null}
+    <div className="flex flex-col flex-1 gap-16 lg:gap-40">
+      <div className="flex flex-col gap-8 flex-1">
+        <h3 className="ui-text-h3">{title}</h3>
+        <p className="ui-text-p2 text-neutral-800">{content}</p>
       </div>
+      {links?.length ? <Links links={links} /> : null}
     </div>
-    {image && <Image image={image} className="max-w-[250px] mx-auto" />}
+    <div className="flex items-center justify-center">{image ? <Image image={image} className="h-128" /> : null} </div>
   </div>
 );
