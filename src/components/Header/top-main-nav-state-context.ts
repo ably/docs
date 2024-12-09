986d550e610f69a99bab009da48cf21325a90bfd
Jamie Henson <jamie.henson@ably.com>
2024-12-09 14:23:31 +0000

    chore: remove old Header code and add in stub for new Header

--- src/components/Header/top-main-nav-state-context.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-import { initialState, TopMainNavAction } from './main-nav-reducer';
-
-const identityTopMainNavDispatcher: React.Dispatch<TopMainNavAction> = (value) => value;
-
-export const TopMainNavStateContext = createContext({
-  topMainNavState: initialState,
-  dispatch: identityTopMainNavDispatcher,
-});
