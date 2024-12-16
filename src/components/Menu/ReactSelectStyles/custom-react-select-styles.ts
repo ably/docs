394d101833cfe1971d4cf0077f791b48f0bf56d1
Jamie Henson <jamie.henson@ably.com>
2024-12-16 19:09:13 +0000

    chore: remove unneeded Menu components

--- src/components/Menu/ReactSelectStyles/custom-react-select-styles.ts
+++
@@ -1,9 +0,0 @@
-export type CustomReactSelectStyles = {
-  width?: string;
-  border?: string;
-  boxShadow?: string;
-  activeText?: string;
-  right?: number;
-  marginRight?: string;
-  marginLeft?: number;
-};
