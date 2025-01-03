fa5b05566508ad2756b8a02f1f5bb41c4d874e21
Greg Holmes <iam@gregholmes.co.uk>
2025-01-03 14:59:05 +0000

    Add pub/sub message encryption example

---
+++ examples/pub-sub-message-encryption/javascript/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    }
+  }
+})
