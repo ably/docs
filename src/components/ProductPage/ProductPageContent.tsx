4b027577c3e56c2e293d9e0fa3332c1b5a20e612
Jamie Henson <jamie.henson@ably.com>
2025-01-06 14:58:46 +0000

    chore: remove additional horizontal padding on Product/Homepage templates

--- src/components/ProductPage/ProductPageContent.tsx
+++ src/components/ProductPage/ProductPageContent.tsx
@@ -36,7 +36,7 @@ export type SectionProps = {
 };
 
 export const ProductPageContent = ({ sections, images }: { sections: SectionProps[]; images: ImageProps[] }) => (
-  <article className="px-24 md:px-0">
+  <article>
     {sections.map((section, index) => (
       <BodySection key={index} section={section} images={images} />
     ))}
