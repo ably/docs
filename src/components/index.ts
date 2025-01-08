d41ec1a8abd35f698221de9a9b5d48423f1e8fa2
Jamie Henson <jamie.henson@ably.com>
2025-01-08 10:52:31 +0000

    feat: add new Footer component, remove old logic and assets

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export * from './LanguageButton';
 export * from './HorizontalMenu';
 export * from './Select';
 export * from './common';
-export * from './Footer';
 export * from './Container';
 
 export { type HamburgerMenuProps } from './Header';
