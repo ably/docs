3c42cd686a422e948d81a71803fd0b517835fec9
Jamie Henson <jamie.henson@ably.com>
2025-01-08 10:52:31 +0000

    feat: add new Footer component, remove old logic and assets

--- src/components/Footer/FooterTopNav/FooterLogo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Link from 'src/components/Link';
-import logo from '../../Layout/images/ably-logo.png';
-
-export const FooterLogo = () => (
-  <Link to="/" data-id="footer-logo" className="block" style={{ height: '2.125rem' }}>
-    <img src={logo} width="108px" alt="Ably logo" />
-  </Link>
-);
