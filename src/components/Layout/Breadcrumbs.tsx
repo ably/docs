ff17f63420717960b1d3df6b765a8b980b791ddb
Jamie Henson <jamie.henson@ably.com>
2025-01-13 16:13:09 +0000

    fix: optimise breadcrumbs for mobile

--- src/components/Layout/Breadcrumbs.tsx
+++ src/components/Layout/Breadcrumbs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useLayoutContext } from 'src/contexts/layout-context';
 import Link from '../Link';
 import Icon from '@ably/ui/core/Icon';
@@ -8,7 +8,11 @@ import { hierarchicalKey } from './utils';
 const Breadcrumbs: React.FC = () => {
   const { activePage } = useLayoutContext();
 
-  if (activePage?.tree.length === 0) {
+  const breadcrumbNodes = useMemo(() => {
+    return activePage?.tree.filter((node) => node.page.link !== '#') ?? [];
+  }, [activePage.tree]);
+
+  if (breadcrumbNodes.length === 0) {
     return null;
   }
 
@@ -16,18 +20,20 @@ const Breadcrumbs: React.FC = () => {
     'ui-text-menu4 font-semibold text-neutral-900 hover:text-neutral-1300 active:text-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-000 dark:active:text-neutral-500';
 
   return (
-    <nav aria-label="breadcrumb" className="hidden mt-32 sm:flex items-center gap-4">
-      <Link to="/" className={linkStyles}>
+    <nav aria-label="breadcrumb" className="flex mt-32 items-center gap-4">
+      <Link to="/" className={cn(linkStyles, 'hidden sm:block')}>
         Home
       </Link>
-      <Icon name="icon-gui-disclosure-arrow" size="16px" />
-      {activePage?.tree.map((node, index) => (
+      <Icon name="icon-gui-disclosure-arrow" size="16px" additionalCSS="rotate-180 sm:rotate-0" />
+      {breadcrumbNodes.map((node, index) => (
         <React.Fragment key={hierarchicalKey(node.page.link, index, activePage.tree)}>
-          {index > 0 ? <Icon name="icon-gui-disclosure-arrow" size="16px" /> : null}
+          {index > 0 ? <Icon name="icon-gui-disclosure-arrow" size="16px" additionalCSS="hidden sm:flex" /> : null}
           <Link
             to={node.page.link}
             className={cn(linkStyles, {
-              'text-gui-unavailable dark:text-gui-unavailable-dark pointer-events-none': node.page.link === '#',
+              'text-gui-unavailable dark:text-gui-unavailable-dark pointer-events-none':
+                index === breadcrumbNodes.length - 1,
+              'hidden sm:flex': index !== breadcrumbNodes.length - 2,
             })}
           >
             {node.page.name}
