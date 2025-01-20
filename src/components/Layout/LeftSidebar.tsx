e3d669e02d1c2c568ab7bc53367b32fbbaeb3cd9
Jamie Henson <jamie.henson@ably.com>
2025-01-20 11:35:04 +0000

    fix: show LeftSidebar selected icon state only for active page

--- src/components/Layout/LeftSidebar.tsx
+++ src/components/Layout/LeftSidebar.tsx
@@ -30,7 +30,6 @@ const NavPage = ({
   index,
   type,
   indentLinks,
-  activePageTree,
   inHeader,
 }: {
   depth: number;
@@ -39,17 +38,20 @@ const NavPage = ({
   type: ContentType;
   inHeader: boolean;
   indentLinks?: boolean;
-  activePageTree?: PageTreeNode[];
 }) => {
   const location = useLocation();
   const pageActive = 'link' in page && formatNavLink(page.link) === formatNavLink(location.pathname);
   const linkId = 'link' in page ? composeNavLinkId(page.link) : undefined;
+  const { activePage } = useLayoutContext();
+
+  console.log(activePage.tree);
+
   if ('link' in page) {
     const language = new URLSearchParams(location.search).get('lang');
 
     return (
       <Link
-        key={hierarchicalKey(page.link, depth, activePageTree)}
+        key={hierarchicalKey(page.link, depth, activePage.tree)}
         id={linkId}
         className={cn({
           'block ui-text-menu2 leading-relaxed md:leading-snug md:ui-text-menu4 text-neutral-1000 dark:text-neutral-300 md:text-neutral-900 dark:md:text-neutral-400 transition-colors hover:text-neutral-1300 active:text-neutral-800 focus-base':
@@ -57,7 +59,7 @@ const NavPage = ({
           '!font-semibold': !pageActive,
           'text-neutral-900': !pageActive && type === 'content',
           'text-neutral-1000': !pageActive && type === 'api',
-          '!font-bold text-neutral-1300': pageActive,
+          '!font-bold !text-neutral-1300': pageActive,
           'pl-12': indentLinks,
         })}
         target={page.external ? '_blank' : undefined}
@@ -71,38 +73,25 @@ const NavPage = ({
   } else {
     return (
       <Accordion
-        key={hierarchicalKey(page.name, depth, activePageTree)}
+        key={hierarchicalKey(page.name, depth, activePage.tree)}
         data={[
           {
             name: page.name,
             content: page.pages.map((subPage) => (
               <div className="mb-8 first:mt-8" key={subPage.name}>
-                <NavPage
-                  page={subPage}
-                  indentLinks
-                  index={index}
-                  activePageTree={activePageTree?.slice(1)}
-                  type={type}
-                  depth={depth + 1}
-                  inHeader={inHeader}
-                />
+                <NavPage page={subPage} indentLinks index={index} type={type} depth={depth + 1} inHeader={inHeader} />
               </div>
             )),
           },
         ]}
-        {...commonAccordionOptions(page, activePageTree?.[0]?.index === index ? 0 : undefined, false, inHeader)}
+        {...commonAccordionOptions(page, activePage.tree?.[0]?.index === index ? 0 : undefined, false, inHeader)}
       />
     );
   }
 };
 
-const renderProductContent = (
-  content: NavProductContent[],
-  activePageTree: PageTreeNode[] | undefined,
-  type: ContentType,
-  inHeader: boolean,
-) =>
-  content.map((productContent, contentIndex) => (
+const renderProductContent = (content: NavProductContent[], type: ContentType, inHeader: boolean) =>
+  content.map((productContent) => (
     <div className="flex flex-col gap-[10px] md:gap-8" key={productContent.name}>
       <div className="ui-text-overline2 text-neutral-700">{productContent.name}</div>
       {productContent.pages.map((page, pageIndex) => (
@@ -110,7 +99,6 @@ const renderProductContent = (
           key={'name' in page ? page.name : `page-group-${pageIndex}`}
           page={page}
           index={pageIndex}
-          activePageTree={contentIndex === activePageTree?.[0]?.index ? activePageTree.slice(1) : undefined}
           type={type}
           depth={0}
           inHeader={inHeader}
@@ -120,20 +108,16 @@ const renderProductContent = (
   ));
 
 const constructProductNavData = (
-  location: string,
-  products: [ProductKey, NavProduct][],
   activePageTree: PageTreeNode[],
-  selectedProduct: string | undefined,
-  setSelectedProduct: React.Dispatch<React.SetStateAction<ProductKey | undefined>>,
+  products: [ProductKey, NavProduct][],
   inHeader: boolean,
-) => {
+): AccordionData[] => {
   const navData: AccordionData[] = products.map(([productKey, product]) => {
     const apiReferencesId = `${productKey}-api-references`;
 
     return {
       name: product.name,
-      icon: selectedProduct === productKey ? product.icon.open : product.icon.closed,
-      onClick: () => setSelectedProduct(productKey),
+      icon: activePageTree[0].page.name === product.name ? product.icon.open : product.icon.closed,
       content: (
         <div key={product.name} className="flex flex-col gap-20 px-16">
           <div className="flex flex-col gap-[10px] md:gap-8 mt-12">
@@ -156,13 +140,13 @@ const constructProductNavData = (
               </a>
             ) : null}
           </div>
-          {renderProductContent(product.content, activePageTree, 'content', inHeader)}
+          {renderProductContent(product.content, 'content', inHeader)}
           {product.api.length > 0 ? (
             <div
               id={apiReferencesId}
               className="flex flex-col gap-[10px] md:gap-8 rounded-lg bg-neutral-100 border border-neutral-300 p-16 mb-24 md:-mx-16"
             >
-              {renderProductContent(product.api, activePageTree, 'api', inHeader)}
+              {renderProductContent(product.api, 'api', inHeader)}
             </div>
           ) : null}
         </div>
@@ -174,20 +158,11 @@ const constructProductNavData = (
 };
 
 const LeftSidebar = ({ inHeader = false }: LeftSidebarProps) => {
-  const { selectedProduct, setSelectedProduct, activePage, products } = useLayoutContext();
-  const location = useLocation();
+  const { activePage, products } = useLayoutContext();
 
   const productNavData = useMemo(
-    () =>
-      constructProductNavData(
-        location.pathname,
-        products,
-        activePage.tree.slice(1),
-        selectedProduct,
-        setSelectedProduct,
-        inHeader,
-      ),
-    [location.pathname, products, activePage.tree, selectedProduct, setSelectedProduct, inHeader],
+    () => constructProductNavData(activePage.tree, products, inHeader),
+    [activePage.tree, products, inHeader],
   );
 
   return (
