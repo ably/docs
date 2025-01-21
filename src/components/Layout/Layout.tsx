fde2322a8c92295785120034fa4f3d30002f8bdb
Jamie Henson <jamie.henson@ably.com>
2025-01-21 14:00:34 +0000

    temp: offer containerised UI via localStorage param

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
-import { PropsWithChildren } from 'react';
+import React, { PropsWithChildren } from 'react';
+import cn from '@ably/ui/core/utils/cn';
 
 import '../../styles/global.css';
-
 import GlobalLoading from '../GlobalLoading/GlobalLoading';
 import { Container } from 'src/components';
 import Header from './Header';
@@ -19,10 +19,22 @@ interface LayoutProps {
 const Layout: React.FC<PropsWithChildren<LayoutProps>> = ({ children, noSidebar = false, hideSearchBar }) => {
   const showSidebar = !noSidebar;
 
+  // a temporary thing to test out containerised layout
+  const [withContainer, setWithContainer] = React.useState(false);
+  React.useEffect(() => {
+    setWithContainer(localStorage.getItem('ably-docs-with-container') === 'true');
+  }, []);
+
   return (
     <GlobalLoading>
       <Header hideSearchBar={hideSearchBar} />
-      <div className="flex mx-24 sm:mx-32 md:mx-40 lg:mx-64 pt-64 gap-80 justify-center">
+      <div
+        className={cn(
+          'flex pt-64 gap-80 justify-center',
+          { 'ui-standard-container mx-auto': withContainer },
+          { 'mx-24 sm:mx-32 md:mx-40 lg:mx-64': !withContainer },
+        )}
+      >
         {showSidebar ? <LeftSidebar /> : null}
         <Container as="main" className="flex-1">
           <Breadcrumbs />
