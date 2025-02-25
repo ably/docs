8802dd49295214b710fd4f2e6ab5520d8f7d281b
Jamie Henson <jamie.henson@ably.com>
2025-02-25 17:09:30 +0000

    temp: language button update

--- src/components/LanguageButton/LanguageButton.tsx
+++ src/components/LanguageButton/LanguageButton.tsx
@@ -5,17 +5,18 @@ import { LanguageNavigationComponentProps } from '../Menu/LanguageNavigation';
 import { button, isActive } from '../Menu/MenuItemButton/MenuItemButton.module.css';
 import { languageInfo } from 'src/data/languages';
 import { LanguageKey } from 'src/data/languages/types';
+import { useLayoutContext } from 'src/contexts/layout-context';
 
 const LanguageButton: FC<LanguageNavigationComponentProps> = ({ language }) => {
+  const { activePage } = useLayoutContext();
   const handleClick = () => {
-    const href = '#';
-    navigate(href);
+    navigate(`${location.pathname}?lang=${language}`, { replace: true });
   };
 
   return (
     <button
       className={cn(button, 'ui-text-menu3', {
-        [isActive]: true,
+        [isActive]: language === activePage.language,
       })}
       onClick={handleClick}
     >
