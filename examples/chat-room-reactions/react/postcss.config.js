c6ad24c93a4b872503634e891cddfdc2fca9346c
Greg Holmes <iam@gregholmes.co.uk>
2025-03-04 09:42:46 +0000

    Add franken-ui implementation to chat-room-reactions

--- examples/chat-room-reactions/react/postcss.config.js
+++ examples/chat-room-reactions/react/postcss.config.js
@@ -2,5 +2,8 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'franken-ui/postcss/combine-duplicated-selectors': {
+      removeDuplicatedProperties: true,
+    },
   },
-}
+};
