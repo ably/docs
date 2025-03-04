df28ada5cf0a1e73eb9bdbcf4d5aa502875768f1
Greg Holmes <iam@gregholmes.co.uk>
2025-03-04 10:16:51 +0000

    Add franken-ui implementation to pub-sub-channel-messages

--- examples/pub-sub-channel-messages/react/src/app/page.tsx
+++ examples/pub-sub-channel-messages/react/src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
       <button
         id="publish-button"
         onClick={publishHeadline}
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        className="uk-btn uk-btn-md uk-btn-primary mb-4 rounded"
         disabled={isButtonDisabled}
       >
         Publish Random Headline
