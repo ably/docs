228148285d43eb93ee8ada304fa1a57a3fc46a69
Kenneth Kalmer <kenneth.kalmer@ably.com>
2025-03-10 22:50:44 +0000

    feat: compile-time feature flags like ably-ui
    Local implementation of compile-time feature flags like we added in
    ably/ably-ui#650

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,4 +2,4 @@ export { createPages } from './data/createPages';
 export { onCreateNode } from './data/onCreateNode';
 export { onCreatePage } from './data/onCreatePage';
 export { createSchemaCustomization } from './data/onCreateNode/create-graphql-schema-customization';
-export { onCreateWebpackConfig } from './gatsby-overwrite-config';
+export { onCreateWebpackConfig, onCreateBabelConfig } from './gatsby-overwrite-config';
