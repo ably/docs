cb1147ccddc21521a07c907aa14ae6518a3a7a7d
Kenneth Kalmer <kenneth.kalmer@ably.com>
2025-04-22 22:04:01 +0100

    feat: generate llms.txt from document & apiReference articles

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,3 +3,4 @@ export { onCreateNode } from './data/onCreateNode';
 export { onCreatePage } from './data/onCreatePage';
 export { createSchemaCustomization } from './data/onCreateNode/create-graphql-schema-customization';
 export { onCreateWebpackConfig, onCreateBabelConfig } from './gatsby-overwrite-config';
+export { onPostBuild } from './data/onPostBuild';
