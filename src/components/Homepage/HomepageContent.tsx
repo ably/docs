6b967cf0e6e2402036af38b65efdf63ce554318b
Denis Sellu <denis.sellu@ably.com>
2025-04-23 21:01:19 +0200

    feat: redesign the homepage with the new component and data

--- src/components/Homepage/HomepageContent.tsx
+++ src/components/Homepage/HomepageContent.tsx
@@ -1,6 +1,7 @@
-import { ContentSection } from 'src/data/content/types';
-import { BodySection } from './BodySection/BodySection';
-import { ImageProps } from 'src/components/Image';
+import type { ContentData } from 'src/data/content/types';
+import { Image, ImageProps, getImageFromList } from 'src/components/Image';
+import { HeroSection } from './HeroSection';
+import { PlatformAndProducts } from './PlatformAndProducts';
 
 export const HomepageContent = ({
   homepageContent,
@@ -45,24 +46,3 @@ export const HomepageContent = ({
     </div>
   );
 };
-
-export type LinkProps = {
-  text: string;
-  href: string;
-  external: boolean;
-};
-
-export type CallToActionProps = {
-  text: string;
-  href: string;
-  external: boolean;
-  type: string;
-};
-
-export const HomepageContent = ({ sections, images }: { sections: ContentSection[]; images: ImageProps[] }) => (
-  <article className="mx-auto max-w-[960px]">
-    {sections.map((section, index) => (
-      <BodySection key={index} section={section} images={images} />
-    ))}
-  </article>
-);
