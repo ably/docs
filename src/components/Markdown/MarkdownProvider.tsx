5b00efbc5df408bcd897c59e2274b94e0ccb4239
Mark Hulbert <39801222+m-hulbert@users.noreply.github.com>
2025-05-12 19:42:51 +0200

    chore: add MDX support and styling for h4

--- src/components/Markdown/MarkdownProvider.tsx
+++ src/components/Markdown/MarkdownProvider.tsx
@@ -22,6 +22,12 @@ const H3: FC<JSX.IntrinsicElements['h3']> = ({ children, ...props }) => (
   </h3>
 );
 
+const H4: FC<JSX.IntrinsicElements['h4']> = ({ children, ...props }) => (
+  <h4 className="ui-text-h4 my-16" {...props}>
+    {children}
+  </h4>
+);
+
 const Paragraph: FC<JSX.IntrinsicElements['p']> = ({ children, ...props }) => (
   <p className="ui-text-p2 mb-20" {...props}>
     {children}
@@ -99,6 +105,7 @@ const defaultComponents = {
   h1: H1,
   h2: H2,
   h3: H3,
+  h4: H4,
   p: Paragraph,
   a: Anchor,
   ol: Ol,
