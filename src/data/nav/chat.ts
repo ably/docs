bcdeae7b978e7e355960c3a9ae370352313b5718
Andy Ford <andy.ford@ably.com>
2025-05-22 10:18:34 +0100

    chat: add kotlin getting started to nav

--- src/data/nav/chat.ts
+++ src/data/nav/chat.ts
@@ -31,6 +31,10 @@ export default {
               name: 'React',
               link: '/docs/chat/getting-started/react',
             },
+            {
+              name: 'Kotlin',
+              link: '/docs/chat/getting-started/kotlin',
+            },
           ],
         },
         {
