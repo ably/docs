764ae1f9eb0d7adf3f51e4185e846985ee83dbf9
Mike Christensen <mike.christensen@ably.com>
2025-05-23 11:00:16 +0100

    examples/annotations: use client ID in Ably client

--- examples/pub-sub-message-annotations/javascript/src/script.ts
+++ examples/pub-sub-message-annotations/javascript/src/script.ts
@@ -1,20 +1,17 @@
 import * as Ably from 'ably';
 import type { RealtimeChannel, Message } from 'ably';
-import { nanoid } from 'nanoid';
 import './styles.css';
 
+const urlParams = new URLSearchParams(window.location.search);
+const clientId = urlParams.get('clientId') || 'user2';
+const channelName = urlParams.get('name') || 'pub-sub-message-annotations';
+
 const client = new Ably.Realtime({
-  clientId: nanoid(),
+  clientId,
   key: import.meta.env.VITE_ABLY_KEY as string,
 });
 
-const urlParams = new URLSearchParams(window.location.search);
-
-const channelName = urlParams.get('name') || 'pub-sub-message-annotations';
 const channel = client.channels.get(channelName);
-
-const clientId = urlParams.get('clientId') || 'user2';
-console.log(`Client ID: ${clientId}`);
 
 let selectedMessageId: string | undefined;
 
