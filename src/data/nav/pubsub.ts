2fcff9fe694b372d9c8798345f9b8813aefef882
Greg Holmes <iam@gregholmes.co.uk>
2025-06-30 18:01:45 +0100

    Add PHP Getting started guide

--- src/data/nav/pubsub.ts
+++ src/data/nav/pubsub.ts
@@ -42,6 +42,10 @@ export default {
           link: '/docs/getting-started/swift',
         },
         {
+          name: 'PHP',
+          link: '/docs/getting-started/php',
+        },
+        {
           name: 'Quickstart guide',
           link: '/docs/getting-started/quickstart',
         },
