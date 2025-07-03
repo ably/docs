73e98c1be15e808ad83b5f1f9e5a3d2ee434011d
Greg Holmes <iam@gregholmes.co.uk>
2025-07-03 10:36:51 +0100

    Add Laravel getting started guide

--- src/data/nav/pubsub.ts
+++ src/data/nav/pubsub.ts
@@ -65,6 +65,10 @@ export default {
               name: 'PHP',
               link: '/docs/getting-started/php',
             },
+            {
+              name: 'Laravel',
+              link: '/docs/getting-started/laravel',
+            },
           ],
         },
         {
