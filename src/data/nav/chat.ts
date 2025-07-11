004e0d691fba9da4d87867a68dc5d2e93133b6f6
Steven Lindsay <steven.lindsay@ably.com>
2025-07-11 10:19:10 +0100

    Chat: Add guide for implementing replies and quotes in TypeScript

--- src/data/nav/chat.ts
+++ src/data/nav/chat.ts
@@ -189,6 +189,10 @@ export default {
           name: 'Livestream chat',
           link: '/docs/guides/chat/build-livestream',
         },
+        {
+          name: 'Replies',
+          link: '/docs/guides/chat/replies',
+        }
       ],
     },
   ],
