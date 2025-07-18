49b5c624e95b1358eddbe8a4f0ba919adc336ca6
Steven Lindsay <steven.lindsay@ably.com>
2025-07-18 12:00:16 +0100

    Moved replies to chat rooms section.

--- src/data/nav/chat.ts
+++ src/data/nav/chat.ts
@@ -84,6 +84,10 @@ export default {
           name: 'Room reactions',
           link: '/docs/chat/rooms/reactions',
         },
+        {
+          name: 'Replies and quotes',
+          link: '/docs/chat/rooms/replies',
+        },
       ],
     },
     {
@@ -128,11 +132,6 @@ export default {
         {
           name: 'Livestream chat',
           link: '/docs/guides/chat/build-livestream',
-        },
-        {
-          name: 'Replies',
-          link: '/docs/guides/chat/replies',
-          languages: ['react', 'javascript'],
         },
       ],
     },
