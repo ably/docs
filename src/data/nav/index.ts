a863e095fb47a43cb336bb1041ef94e1e91730ad
Greg Holmes <iam@gregholmes.co.uk>
2025-12-02 09:55:52 +0000

    Add AI transport as a product within docs

--- src/data/nav/index.ts
+++ src/data/nav/index.ts
@@ -1,8 +1,17 @@
 import platformNavData from './platform';
 import pubsubNavData from './pubsub';
 import chatNavData from './chat';
+import aiTransportNavData from './aitransport';
 import liveObjectsNavData from './liveobjects';
 import spacesNavData from './spaces';
 import liveSyncNavData from './livesync';
 
-export { platformNavData, pubsubNavData, chatNavData, liveObjectsNavData, spacesNavData, liveSyncNavData };
+export {
+  platformNavData,
+  pubsubNavData,
+  chatNavData,
+  aiTransportNavData,
+  liveObjectsNavData,
+  spacesNavData,
+  liveSyncNavData,
+};
