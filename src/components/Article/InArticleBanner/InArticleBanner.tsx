0ca70d0252f5f6f56209adce43910192f437e4d3
Jamie Henson <jamie.henson@ably.com>
2025-12-03 18:04:32 +0000

    chore: remove article footer unit

--- src/components/Article/InArticleBanner/InArticleBanner.tsx
+++
@@ -1,5 +0,0 @@
-import React, { FC } from 'react';
-
-export const InArticleBanner: FC<{ children: React.ReactNode }> = ({ children }) => (
-  <aside className="bg-extra-light-grey border border-mid-grey rounded-lg p-10 col-span-full my-16">{children}</aside>
-);
