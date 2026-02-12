cf8aba6ec1f6c9ccab7f9c141fadc12992195a65
Marat Alekperov <m.alekperov@gmail.com>
2026-02-12 16:44:25 +0100

    Web push notifications tutorial.

--- src/data/nav/pubsub.ts
+++ src/data/nav/pubsub.ts
@@ -243,6 +243,10 @@ export default {
           name: 'Getting started',
           pages: [
             {
+              name: 'Web Push',
+              link: '/docs/push/getting-started/web',
+            },
+            {
               name: 'APNs',
               link: '/docs/push/getting-started/apns',
             },
